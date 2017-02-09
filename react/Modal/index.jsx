af6a07c35673424214311e6e2a7a1bb8345cf357
Enguerran <enguerran@ticabri.com>
2017-02-09 10:57:07 +0100

    refactor: to functional component

--- react/Modal/index.jsx
+++ react/Modal/index.jsx
@@ -1,42 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 
 import styles from './index.styl'
 
-class Modal extends Component {
-  render () {
-    const {
-      title, description,
-      cancelType, cancelText, cancelAction,
-      validateType, validateText, validateAction
-    } = this.props
+export default (props) => {
+  const {
+    title, description,
+    cancelType, cancelText, cancelAction,
+    validateType, validateText, validateAction
+  } = props
 
-    const cancelTypeDefault = cancelType || 'secondary'
-    const validateTypeDefault = validateType || 'primary'
+  const cancelTypeDefault = cancelType || 'secondary'
+  const validateTypeDefault = validateType || 'primary'
 
-    return (<div className={styles['coz-modal-container']}>
-      <div className={styles['coz-overlay']}>
-        <div className={styles['coz-modal']}>
-          <h2 className={styles['coz-modal-title']}>{title}</h2>
-          <button
-            className={classNames('coz-btn', 'coz-btn--close', styles['coz-modal-close'])}
-            onClick={cancelAction}
-            >
-            <span className='coz-hidden'>{cancelText}</span>
+  return (<div className={styles['coz-modal-container']}>
+    <div className={styles['coz-overlay']}>
+      <div className={styles['coz-modal']}>
+        <h2 className={styles['coz-modal-title']}>{title}</h2>
+        <button
+          className={classNames('coz-btn', 'coz-btn--close', styles['coz-modal-close'])}
+          onClick={cancelAction}
+          >
+          <span className='coz-hidden'>{cancelText}</span>
+        </button>
+        {description}
+        <div className={styles['coz-modal-buttons']}>
+          <button className={styles[cancelTypeDefault]} onClick={cancelAction}>
+            {cancelText}
           </button>
-          {description}
-          <div className={styles['coz-modal-buttons']}>
-            <button className={styles[cancelTypeDefault]} onClick={cancelAction}>
-              {cancelText}
-            </button>
-            <button className={styles[validateTypeDefault]} onClick={validateAction}>
-              {validateText}
-            </button>
-          </div>
+          <button className={styles[validateTypeDefault]} onClick={validateAction}>
+            {validateText}
+          </button>
         </div>
       </div>
-    </div>)
-  }
+    </div>
+  </div>)
 }
-
-export default Modal
