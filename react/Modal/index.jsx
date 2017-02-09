6484e28df9f5b45a02f88849a35f92ed058cc1d1
Enguerran <enguerran@ticabri.com>
2017-02-09 11:18:03 +0100

    feature: add propTypes and defaultProps

--- react/Modal/index.jsx
+++ react/Modal/index.jsx
@@ -3,13 +3,12 @@ import classNames from 'classnames'
 
 import styles from './index.styl'
 
-export default ({ title, description,
-                  cancelType, cancelText, cancelAction,
-                  validateType, validateText, validateAction }) => {
-  const cancelTypeDefault = cancelType || 'secondary'
-  const validateTypeDefault = validateType || 'primary'
-
-  return (<div className={styles['coz-modal-container']}>
+const Modal = ({
+  title, description,
+  cancelType, cancelText, cancelAction,
+  validateType, validateText, validateAction
+}) => (
+  <div className={styles['coz-modal-container']}>
     <div className={styles['coz-overlay']}>
       <div className={styles['coz-modal']}>
         <h2 className={styles['coz-modal-title']}>{title}</h2>
@@ -21,14 +20,32 @@ export default ({ title, description,
         </button>
         {description}
         <div className={styles['coz-modal-buttons']}>
-          <button className={styles[cancelTypeDefault]} onClick={cancelAction}>
+          <button className={styles[cancelType]} onClick={cancelAction}>
             {cancelText}
           </button>
-          <button className={styles[validateTypeDefault]} onClick={validateAction}>
+          <button className={styles[validateType]} onClick={validateAction}>
             {validateText}
           </button>
         </div>
       </div>
     </div>
-  </div>)
+  </div>
+)
+
+Modal.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string.isRequired,
+  cancelType: React.PropsTypes.string,
+  cancelText: React.PropsTypes.string.isRequired,
+  cancelAction: React.PropsTypes.func.isRequired,
+  validateType: React.PropsTypes.string,
+  validateText: React.PropsTypes.string.isRequired,
+  validateAction: React.PropsTypes.func.isRequired
 }
+
+Modal.defaultProps = {
+  cancelType: 'secondary',
+  validateType: 'primary'
+}
+
+export default Modal
