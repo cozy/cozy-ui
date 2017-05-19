edf7889075fb5090f7ba22e64c7de07eeef947ae
Simon Constans <simonc@linux.com>
2017-05-19 16:46:22 +0200

    feat: Add spinner on react Button :art:

--- react/Button.jsx
+++ react/Button.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import cx from 'classnames'
 
 export default function (props) {
-  const { theme, className, children, onClick } = props
+  const { theme, busy, className, children, onClick } = props
   return <button
+    aria-busy={busy}
     role='button'
     className={cx('coz-btn', theme ? `coz-btn--${theme}` : null, className)}
     onClick={onClick}>
