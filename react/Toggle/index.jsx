c14263684bf2e39b026b2e91551c0055dff0c931
Simon Constans <simonc@linux.com>
2017-05-24 11:58:04 +0200

    fix: Remove deprecated msg, it's always display :fire:

--- react/Toggle/index.jsx
+++ react/Toggle/index.jsx
@@ -9,24 +9,17 @@ class Toggle extends Component {
     }
   }
   render (props, state) {
-    props.id = props.id || props.name
-
     return (
       <span className={styles['toggle']}>
-        <input type="checkbox" name={props.id} id={props.id} className={styles['checkbox']} checked={props.checked} onChange={this.onChange.bind(this)} />
+        <input type="checkbox" id={props.id} className={styles['checkbox']} checked={props.checked} onChange={this.onChange.bind(this)} />
         <label for={props.id} className={styles['label']} />
       </span>
     )
   }
 }
 
-function deprecated (arg) {
-  console.warn(`[Toggle] ${arg} is deprecated, please upgrade your Toggle component.`)
-}
-
 Toggle.propTypes = {
   id: React.PropTypes.string.isRequired,  // A unique id for the toggle, used internally.
-  name: deprecated('name') || React.PropTypes.string, // Previous incarnation of `id`
   checked: React.PropTypes.bool,          // The state of the toggle
   onToggle: React.PropTypes.func          // A callback when the state of the toggle changes. Called with the new state as argument.
 }
