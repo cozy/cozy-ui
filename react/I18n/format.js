f32f8fbd58e0a1338b7a878416d49cd3cb8688d0
Simon Constans <simonc@linux.com>
2017-06-07 10:31:32 +0200

    feat: Add I18n component :construction_worker:

---
+++ react/I18n/format.js
@@ -0,0 +1,16 @@
+import format from 'date-fns/format'
+import { DEFAULT_LANG } from '.'
+
+export const initFormat = (lang, defaultLang = DEFAULT_LANG) => {
+  const locales = {
+    [defaultLang]: require(`date-fns/locale/${defaultLang}/index`)
+  }
+  if (lang && lang !== defaultLang) {
+    try {
+        locales[lang] = require(`date-fns/locale/${lang}/index`)
+    } catch (e) {
+      console.warn(`The "${lang}" locale isn't supported by date-fns`)
+    }
+  }
+  return (date, formatStr) => format(date, formatStr, { locale: locales[lang] })
+}
