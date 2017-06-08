86b41f369788907b1792021830827a97f8699023
Patrick Browne <pt.browne@gmail.com>
2017-06-08 15:14:15 +0200

    feat: add properties to customize icon

--- react/Icon/index.jsx
+++ react/Icon/index.jsx
@@ -3,15 +3,24 @@ import styles from './styles.styl'
 import icons from '../../src/icons'
 
 
-const Icon = function ({ icon }) {
-  const hash = allIcons[icon]
+const Icon = function ({ icon, width, height, style, color, className }) {
+  const hash = icons[icon]
 
   if (!hash) {
-    throw new Error('Icon not found ' + icon)
+    throw new Error('Icon not found ' + icon + '\nAvailable icons :' + Object.keys(icons))
   }
 
-  return <svg className={ styles['icon'] } width='1em' height='1em'>
-    <use xlinkHref={allIcons[icon]} />
+  style = Object.assign({}, style)
+  if (color) {
+    style['fill'] = color
+  }
+
+  return <svg
+      className={ className ? `${styles['icon']} ${className}` : styles['icon'] }
+      style={style}
+      width={width || '1em'}
+      height={height || '1em'}>
+    <use xlinkHref={icons[icon]} />
   </svg>
 }
 
