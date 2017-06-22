20ea49689086fd72d937fe251a8c15b2e00f6593
GoOz <gooz@foojin.com>
2017-06-22 11:24:31 +0200

    chore: removed useless comments

--- react/Spinner/index.jsx
+++ react/Spinner/index.jsx
@@ -4,13 +4,6 @@ import classNames from 'classnames'
 
 import styles from './styles'
 
-// === Component Options ===
-// loadingType = message type from locale [string] (default: '')
-// middle      = Component is centered both vertically & horizontally [bool] (default: false)
-// noMargin    = Component has no margin [bool] (default: false)
-// color       = Color of the spinner (available colors: blue, grey, white, red) [string] (default: 'blue')
-// size        = Size of the spinner (available sizes: tiny, small, medium, large, xlarge, xxlarge) [string] (default: 'medium')
-
 export const Spinner = ({ t, loadingType, middle, noMargin, color, size }) => {
   return (
     <div
