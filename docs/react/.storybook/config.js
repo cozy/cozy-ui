755e8bb0c2fd98a590d9e80480882b58f653ea7f
Enguerran <enguerran@ticabri.com>
2017-09-06 17:11:31 +0200

    doc: add toggle examples

--- docs/react/.storybook/config.js
+++ docs/react/.storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/react'
+import { setDefaults } from '@storybook/addon-info';
+
+// addon-info
+setDefaults({
+  header: false,
+  inline: true,
+  source: true
+});
 
 function loadStories() {
   require('../stories/Toggle.js')
