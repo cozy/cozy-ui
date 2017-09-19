9c001684067f2559a78a093064dbdf8bc7c1f1fe
CPatchane <code@patchane.com>
2017-09-19 18:22:01 +0200

    fix: remove require.context usage (webpack) in icons.jsx :art:

--- src/icons.jsx
+++ src/icons.jsx
@@ -3,15 +3,30 @@ const normalize = function (k) {
 }
 
 const importIcons = function () {
-  const ctx = require.context('!!svg-sprite-loader?symbolId=cozyu[name]!../loaders/strip-fill-loader!../assets/icons/ui', true, /icon-/)
-  const keys = ctx.keys()
-    .filter(x => x.indexOf('./icon-') === 0)
-    .filter(x => x.indexOf('-white') === -1)
-  const values = keys.map(ctx)
-  const icons = keys.reduce((o, k, i) => {
-    o[normalize(k)] = values[i].default || values[i]
-    return o
-  }, {})
+  const names = [
+    'icon-album-add',
+    'icon-album-remove',
+    'icon-check',
+    'icon-cozy',
+    'icon-cross',
+    'icon-delete',
+    'icon-device-laptop',
+    'icon-dots',
+    'icon-download',
+    'icon-moveto',
+    'icon-openwith',
+    'icon-paperplane',
+    'icon-rename',
+    'icon-share',
+    'icon-upload',
+    'icon-warn',
+    'icon-warning'
+  ]
+  let icons = {}
+  names.map(n => {
+    const icon = require('!!svg-sprite-loader?symbolId=cozyu[name]!../loaders/strip-fill-loader!../assets/icons/ui/' + n + '.svg')
+    icons[normalize(n)] = icon.default || icon
+  })
   return icons
 }
 
