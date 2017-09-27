84b1e639bc10dcab12a590096aad9741767e4d2d
Patrick Browne <pt.browne@gmail.com>
2017-09-27 19:48:07 +0200

    🔧 chore: use glob for components

--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  components: path.resolve(__dirname, '../react/{Button,Toggle}/*.{js,jsx}'),
+  components: '../react/**/*.jsx',
   webpackConfig: require('./webpack.config.js'),
   serverPort: 6161,
+  skipComponentsWithoutExample: true,
   styleguideDir: path.resolve(__dirname, '../build/react'),
   require: [
     path.join(__dirname, './style.styl'),
