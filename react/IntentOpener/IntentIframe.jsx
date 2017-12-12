da725fb2078984c10ddba388804238722edf733e
Patrick Browne <pt.browne@gmail.com>
2017-12-12 10:37:22 +0100

    ✨ feat: add loading indicator

--- react/IntentOpener/IntentIframe.jsx
+++ react/IntentOpener/IntentIframe.jsx
@@ -1,18 +1,23 @@
 /* global cozy */
 import React from 'react'
 import PropTypes from 'prop-types'
+import Spinner from '../Spinner'
+import styles from './styles.styl'
 
 class IntentIframe extends React.Component {
+  state = { loading: false }
+
   componentDidMount () {
     const { action, doctype, options, onComplete } = this.props
 
     const create = this.props.create || cozy.client.intents.create
 
+    this.setState({ loading: true })
     create(action, doctype, {
         exposeIntentFrameRemoval: true,
         ...options
       })
-      .start(this.intentViewer)
+      .start(this.intentViewer, this.onFrameLoaded)
       .then(result => {
         if (this.props.onComplete) {
           this.props.onComplete(result)
@@ -20,9 +25,17 @@ class IntentIframe extends React.Component {
       })
   }
 
-  render () {
+  onFrameLoaded = () => {
+    this.setState({ loading: false })
+  }
+
+  render (props, { loading }) {
     return (
-      <div ref={intentViewer => (this.intentViewer = intentViewer)} />
+      <div ref={intentViewer => (this.intentViewer = intentViewer)}>
+        { loading ? <div className={styles.intentModal__loading}>
+          <Spinner size='xxlarge' />
+        </div> : null }
+      </div>
     )
   }
 }
