11594b26b06ef8021ae8dc37d3128cb0db22513a
Patrick Browne <pt.browne@gmail.com>
2017-12-17 20:43:04 +0100

    ✨ feat: deprecation warning for font-size() and em()

---
+++ stylus/scripts/deprecate.js
@@ -0,0 +1,19 @@
+const nodes = require('stylus/lib/nodes')
+const chalk = require('chalk')
+
+const last = function (arr) {
+  return arr[arr.length - 1]
+}
+const plugin = function () {
+  return function (style) {
+    style.define('deprecate', function (msg) {
+      const parentSel = last(this.selectorStack)[0]
+      if (process.env.SHOW_STYLUS_DEPRECATION) {
+        console.log(chalk.yellow('\nDeprecation: ' + parentSel.filename + ':' + parentSel.lineno + '\n' + msg.val))
+      }
+      return nodes.null
+    })
+  }
+}
+
+module.exports = plugin
