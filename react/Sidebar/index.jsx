5933b0c9f3204c1be87fda1fed77cb63cd294843
GoOz <gooz@foojin.com>
2017-12-21 16:02:24 +0100

    feat: Add className props to Sidebar

--- react/Sidebar/index.jsx
+++ react/Sidebar/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import styles from './styles.styl'
 
-const Sidebar = ({ children }) => (
-  <aside className={styles['o-sidebar']}>
+const Sidebar = ({ children, className }) => (
+  <aside className={classNames(styles['o-sidebar'], className)}>
     { children }
   </aside>
 )
+
+Sidebar.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string
+}
 
 export default Sidebar
