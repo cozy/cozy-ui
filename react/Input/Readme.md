b38422e14a5f1c1d8ff161bd60c5a333995aa333
GoOz <gooz@foojin.com>
2018-01-04 09:45:27 +0100

    feat: add Input component

---
+++ react/Input/Readme.md
@@ -0,0 +1,16 @@
+#### Input's available types (text, password, email, url)
+
+```
+<form>
+  <p><Input placeholder="This is a input[type=text]" /></p>
+  <p><Input type="password" placeholder="This is a input[type=password]" /></p>
+  <p><Input type="email" placeholder="This is a input[type=email]" /></p>
+  <p><Input type="url" placeholder="This is a input[type=url]" /></p>
+</form>
+```
+
+#### Input when there's an error
+
+```
+<Input error placeholder="This is a input[type=text] with error" />
+```
---
+++ react/Input/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+import styles from './styles'
+
+const Input = props => {
+  const { type, id, className, value, placeholder, error } = props
+  return (
+    <input
+      type={type}
+      id={id}
+      className={cx(
+        styles['c-input-text'], {
+          [styles[`is-error`]] : error
+        },
+        className)}
+      placeholder={placeholder}
+      value={value}
+    />
+  )
+}
+
+Input.propTypes = {
+  type: PropTypes.oneOf(['text','password','email','url']),
+  id: PropTypes.string,
+  className: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  error: PropTypes.bool
+}
+
+Input.defaultProps = {
+  type: 'text',
+  id: '',
+  className: '',
+  value: '',
+  placeholder: '',
+  error: false
+}
+
+export default Input
