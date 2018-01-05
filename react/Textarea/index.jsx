d1a50c0b1b5242133799098b57ccb2729c929896
GoOz <gooz@foojin.com>
2018-01-05 10:46:28 +0100

    feat: add Textarea component

---
+++ react/Textarea/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+import styles from './styles'
+
+const Textarea = props => {
+  const { className, placeholder, children, error } = props
+  return (
+    <textarea
+      placeholder={placeholder}
+      className={cx(
+        styles['c-textarea'], {
+          [styles[`is-error`]] : error
+        },
+        className)}
+    >
+      {children}
+    </textarea>
+  )
+}
+
+Textarea.propTypes = {
+  children: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  error: PropTypes.bool
+}
+
+Textarea.defaultProps = {
+  placeholder: '',
+  className: '',
+  error: false
+}
+
+export default Textarea
