f8699662eb2ddedb3f20eb0719d30683ed6a0a16
GoOz <gooz@foojin.com>
2018-01-10 11:07:59 +0100

    chore: added category for components to order things a bit

--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -1,6 +1,54 @@
 const path = require('path')
 
 module.exports = {
+  title: 'Cozy UI React components',
+  sections: [
+    {
+      name: 'Basics',
+      components: () => [
+        '../react/Button/index.jsx',
+        '../react/Spinner/index.jsx',
+        '../react/Icon/index.jsx'
+      ]
+    },
+    {
+      name: 'Forms',
+      components: () => [
+        '../react/Input/index.jsx',
+        '../react/Textarea/index.jsx',
+        '../react/Label/index.jsx',
+        '../react/Toggle/index.jsx',
+        '../react/Field/index.jsx'
+      ]
+    },
+    {
+      name: 'Layout',
+      components: () => [
+        '../react/Sidebar/index.jsx'
+      ]
+    },
+    {
+      name: 'Content',
+      components: () => [
+        '../react/Media/Media.jsx',
+        '../react/Modal/index.jsx'
+      ]
+    },
+    {
+      name: 'Navigation',
+      components: () => [
+        '../react/ActionMenu/index.jsx',
+        '../react/Menu/index.jsx'
+      ]
+    },
+    {
+      name: 'Special',
+      components: () => [
+        '../react/Overlay/index.jsx',
+        '../react/IntentOpener/IntentOpener.jsx'
+      ]
+    }
+  ],
   components: '../react/**/*.jsx',
   webpackConfig: require('./webpack.config.js'),
   serverPort: 6161,
