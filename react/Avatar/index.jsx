d1a0881a8bf8cc32d72ae33e9846b005281f8e03
GoOz <gooz@foojin.com>
2018-01-31 19:24:37 +0100

    feat: add Badge component

---
+++ react/Avatar/index.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import palette from '../../stylus/settings/palette.json'
+import styles from './styles'
+
+const nameToColor = (name = "") => {
+  const colors = [
+    palette["azure"],
+    palette["melon"],
+    palette["blazeOrange"],
+    palette["pomegranate"],
+    palette["mango"],
+    palette["pumpkinOrange"],
+    palette["darkPeriwinkle"],
+    palette["purpley"],
+    palette["lightishPurple"],
+    palette["weirdGreen"],
+    palette["puertoRico"],
+    palette["emerald"]
+  ];
+  return colors[
+    Array.from(name.toUpperCase())
+      .map(letter => letter.charCodeAt(0))
+      .reduce((sum, number) => sum + number, 0) % colors.length
+  ];
+};
+
+
+export const Avatar = ({ firstname, lastname, image, size, className }) => {
+  const bgColor = {
+    backgroundColor: `${nameToColor(firstname)}`,
+    color: "white"
+  };
+  return (
+    <div
+      className={classNames(
+        styles['c-avatar'], {
+          [styles[`c-avatar--${size}`]]: size
+        },
+        className
+      )}
+      style={bgColor}
+    >
+      {!image &&
+        <span className={styles["c-avatar-initials"]}>
+          {[
+            (firstname[0] || "").toUpperCase(),
+            (lastname[0] || "").toUpperCase()
+          ].join("")}
+        </span>
+      }
+      {image &&
+        <img
+          src={image}
+          className={styles["c-avatar-image"]}
+          alt=''
+        />
+      }
+    </div>
+  )
+}
+
+Avatar.propTypes = {
+  firstname: PropTypes.string,
+  lastname: PropTypes.string,
+  image: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium']),
+  className: PropTypes.string
+}
+
+Avatar.defaultProps = {
+  firstname: '',
+  lastname: '',
+  image: '',
+  size: 'medium',
+  className: ''
+}
+
+export default Avatar
