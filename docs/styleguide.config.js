26f358443a5773cecb4e0a3b1054fd8ee7e79343
Grégory Le Garec <gregory.legarec@gmail.com>
2018-04-09 16:39:55 +0200

    refacto: extract IntentIframe from IntentModal 🔨

--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -51,6 +51,7 @@ module.exports = {
       components: () => [
         '../react/Overlay/index.jsx',
         '../react/Alerter/index.jsx',
+        '../react/IntentIframe/IntentIframe.jsx',
         '../react/IntentModal/IntentModal.jsx',
         '../react/IntentOpener/IntentOpener.jsx'
       ]
