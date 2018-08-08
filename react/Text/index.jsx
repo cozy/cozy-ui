2f426153ba8260681318d90095e40a1b53eb87fb
Enguerran <engcolson@gmail.com>
2018-08-08 16:44:57 +0200

    fix: Import needs the filename
    Or webpack needs a configuration to find the correct filename.

--- react/Text/index.jsx
+++ react/Text/index.jsx
@@ -3,13 +3,12 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 import styles from './styles.styl'
 
-const mkText = baseClass => props => {
+const BaseText = props => {
   const { className, children, tag, ellipsis, ...restProps } = props
   const Tag = tag
   return (
     <Tag
       className={cx(
-        baseClass,
         {
           ['u-ellipsis']: ellipsis
         },
@@ -22,12 +21,24 @@ const mkText = baseClass => props => {
   )
 }
 
-export const Text = mkText(styles['u-text'])
-export const MainTitle = mkText(styles['u-title-h1'])
-export const Title = mkText(styles['u-title-h2'])
-export const SubTitle = mkText(styles['u-title-h3'])
-export const Bold = mkText(styles['u-title-h4'])
-export const Caption = mkText(styles['u-caption'])
+export const Text = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-text'], className)} {...props} />
+)
+export const MainTitle = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-title-h1'], className)} {...props} />
+)
+export const Title = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-title-h2'], className)} {...props} />
+)
+export const SubTitle = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-title-h3'], className)} {...props} />
+)
+export const Bold = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-title-h4'], className)} {...props} />
+)
+export const Caption = ({ className, ...props }) => (
+  <BaseText className={cx(styles['u-caption'], className)} {...props} />
+)
 
 // Props
 const commonProps = {
