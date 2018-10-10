17ef52875e031f87b71ba8376c4e9074b6b87c43
Cédric Patchane <code@patchane.com>
2018-10-10 17:23:06 +0200

    fix: wrong react-dom require
    Don't use default but directly the imported package

--- react/Portal/index.jsx
+++ react/Portal/index.jsx
@@ -1,6 +1,6 @@
 let Portal
 if (process.env.USE_REACT) {
-  const ReactDOM = require('react-dom').default
+  const ReactDOM = require('react-dom')
   Portal = ({ into, children }) => {
     const targetElement = document.querySelector(into)
     return ReactDOM.createPortal(children, targetElement)
