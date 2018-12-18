7fc1343360be0d8e98751f2db586fd7db1824fe0
Patrick Browne <pt.browne@gmail.com>
2018-12-18 19:04:12 +0100

    Revert "Merge pull request #729 from cozy/refacto/theme"
    This reverts commit da9be5318a6f5c4ea32a9bb85a98b44fbe8c8997, reversing
    changes made to 5161e0241c51ffcd89fa1144f10a5a00ea1ccdee.

--- react/palette.js
+++
@@ -1,9 +0,0 @@
-import palette from '../stylus/settings/theme.json'
-
-const colors = {}
-
-Object.keys(palette).forEach(function(name) {
-  colors[name] = 'var(--' + name + ')'
-})
-
-export default colors
