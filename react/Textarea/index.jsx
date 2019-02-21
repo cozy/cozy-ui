ccfa432cdc592ae5a92f91c27f40b71e47c99ae9
Grégory Le Garec <gregory.legarec@gmail.com>
2019-02-21 09:52:33 +0100

    feat(Inputs): Add missing onChange propType 📝

--- react/Textarea/index.jsx
+++ react/Textarea/index.jsx
@@ -42,7 +42,8 @@ Textarea.propTypes = {
   className: PropTypes.string,
   error: PropTypes.bool,
   size: PropTypes.oneOf(['tiny', 'medium']),
-  fullwidth: PropTypes.bool
+  fullwidth: PropTypes.bool,
+  onChange: PropTypes.func
 }
 
 Textarea.defaultProps = {
