cdda573433cfb9013728035ca4067bb70c0d54e5
Patrick Browne <pt.browne@gmail.com>
2019-05-22 11:55:16 +0200

    fix: Correctly receive context and pass props

--- react/I18n/withLocales.jsx
+++ react/I18n/withLocales.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { I18n, translate } from './'
+import omit from 'lodash/omit'
 
-const withLocales = locales => Component =>
-  translate()(props => {
-    return (
-      <I18n dictRequire={localeCode => locales[localeCode]} lang={props.lang}>
-        <Component {...props} />
-      </I18n>
-    )
-  })
+const withLocales = locales => Component => {
+  // The inner components needs to receive t
+  const Translated = translate()(Component)
+
+  class Wrapped extends React.Component {
+    render() {
+      // Do not pass t downwards
+      const { lang, ...rest } = omit(this.props, 't')
+      return (
+        <I18n dictRequire={localeCode => locales[localeCode]} lang={lang}>
+          <Translated {...rest} />
+        </I18n>
+      )
+    }
+  }
+
+  // The outer component needs to receive lang
+  return translate()(Wrapped)
+}
 
 export default withLocales
