44e0f61b4ce4350b755e0e62538e75ba47bc5d09
Patrick Browne <pt.browne@gmail.com>
2019-05-22 10:21:29 +0200

    fix(AppSections): Fix warning, wrap in I18n for children to get t

--- react/AppSections/components/AppsSection.spec.jsx
+++ react/AppSections/components/AppsSection.spec.jsx
@@ -11,6 +11,7 @@ import SmallAppItem from './SmallAppItem'
 
 import mockApps from '../../mocks/apps'
 import en from '../locales/en'
+import { I18n } from '../../I18n'
 
 const i18nContext = I18nContext({ locale: en })
 const tMock = i18nContext.t
@@ -19,14 +20,16 @@ describe('AppsSection component', () => {
   let component
   const setup = ({ onAppClick }) => {
     component = shallow(
-      <AppsSection
-        t={tMock}
-        lang="en"
-        subtitle={<h3>Test Apps</h3>}
-        appsList={mockApps}
-        onAppClick={onAppClick}
-      />
-    )
+      <I18n lang="en" dictRequire={() => en}>
+        <AppsSection
+          t={tMock}
+          lang="en"
+          subtitle={<h3>Test Apps</h3>}
+          appsList={mockApps}
+          onAppClick={onAppClick}
+        />
+      </I18n>
+    ).dive()
   }
   it('should be rendered correctly with apps list, subtitle and onAppClick', () => {
     const mockOnAppClick = jest.fn()
