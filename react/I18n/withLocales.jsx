595f9fe053bf7f34521d94119f15992e7274d2a7
Patrick Browne <pt.browne@gmail.com>
2019-05-29 15:21:26 +0200

    feat: Ability to use a function for withLocales arg

--- react/I18n/withLocales.jsx
+++ react/I18n/withLocales.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import { I18n, translate } from './'
 import omit from 'lodash/omit'
 
-const withLocales = locales => Component => {
+/**
+ *
+ * @param  {Function|Object} localesOrRequire - Either a function returning the locale for a lang,
+ *                                              or an object containing all the locales
+ * @return {Component}
+ */
+const withLocales = localesOrRequire => Component => {
   // The inner components needs to receive t
   const Translated = translate()(Component)
+
+  const requireLocale =
+    typeof localesOrRequire === 'function'
+      ? localesOrRequire
+      : localeCode => localesOrRequire[localeCode]
 
   class Wrapped extends React.Component {
     render() {
       // Do not pass t downwards
       const { lang, ...rest } = omit(this.props, 't')
       return (
-        <I18n dictRequire={localeCode => locales[localeCode]} lang={lang}>
+        <I18n dictRequire={requireLocale} lang={lang}>
           <Translated {...rest} />
         </I18n>
       )
