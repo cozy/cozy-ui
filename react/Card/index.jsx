913c565b5835e928931059ff23f3a20d601da537
Grégory Le Garec <gregory.legarec@gmail.com>
2019-06-11 12:13:54 +0200

    feat(Card): Add inset prop ✨

--- react/Card/index.jsx
+++ react/Card/index.jsx
@@ -1,13 +1,30 @@
 import React, { PureComponent } from 'react'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
 
 import styles from './styles.styl'
 
 export class Card extends PureComponent {
   render() {
-    const { className, ...rest } = this.props
-    return <div className={cx(styles['c-card'], className)} {...rest} />
+    const { className, inset, ...rest } = this.props
+    return (
+      <div
+        className={cx(
+          styles['c-card'],
+          {
+            [styles['c-card--inset']]: inset
+          },
+          className
+        )}
+        {...rest}
+      />
+    )
   }
+}
+
+Card.propTypes = {
+  className: PropTypes.string,
+  inset: PropTypes.bool
 }
 
 export default Card
