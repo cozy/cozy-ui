5b12890093216c05a12b2e879e677f9871c7bdfc
Patrick Browne <pt.browne@gmail.com>
2019-07-24 17:15:48 +0200

    feat: Responsive stack

--- react/Stack/index.jsx
+++ react/Stack/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
 import styles from './styles.styl'
 
 const sparseStyle = styles['Stack--sparse']
 const denseStyle = styles['Stack--dense']
 
-const Stack = ({ sparse, dense, ...props }) => {
+const Stack = ({ spacing, ...props }) => {
   return (
     <div
       {...props}
       className={cx(props.className, styles.Stack, {
-        [sparseStyle]: sparse,
-        [denseStyle]: dense
+        [sparseStyle]: spacing === 'sparse',
+        [denseStyle]: spacing === 'dense'
       })}
     />
   )
+}
+
+Stack.propTypes = {
+  spacing: PropTypes.oneOf(['sparse', 'dense'])
 }
 
 export default Stack
