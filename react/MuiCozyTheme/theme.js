ecde54a627b2e009dd096f8450b57caac956ff53
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-22 10:00:31 +0100

    refactor: Moved overrides outside theme declaration
    Allows us to use theme variables in the overrides

--- react/MuiCozyTheme/theme.js
+++ react/MuiCozyTheme/theme.js
@@ -42,199 +42,6 @@ export const theme = createMuiTheme({
   shape: {
     borderRadius: defaultValues.borderRadius
   },
-  overrides: {
-    MuiOutlinedInput: {
-      root: {
-        '&$disabled': {
-          background: 'var(--paleGrey)'
-        },
-        '&$focused $notchedOutline': {
-          borderWidth: '0.0625rem'
-        }
-      },
-      notchedOutline: {
-        borderColor: 'var(--silver)'
-      }
-    },
-    MuiButton: {
-      root: {
-        borderRadius: 0
-      }
-    },
-    MuiExpansionPanel: {
-      rounded: {
-        borderRadius: defaultValues.borderRadius
-      },
-      root: {
-        boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.08)',
-        borderWidth: '0.0625rem',
-        borderStyle: 'solid',
-        borderColor: 'var(--silver)',
-        overflow: 'hidden',
-        marginBottom: '1rem'
-      }
-    },
-    MuiExpansionPanelSummary: {
-      expanded: {},
-      root: {
-        backgroundColor: 'var(--paleGrey)',
-        textTransform: 'uppercase',
-        fontWeight: 'bold',
-        fontSize: '0.875rem',
-        minHeight: '3.5rem',
-        padding: 0,
-        color: 'var(--charcoalGrey)',
-        '&$expanded': {
-          minHeight: '3.5rem'
-        }
-      },
-      content: {
-        margin: '0.75rem 0',
-        paddingLeft: '0.5rem',
-        paddingRight: '0.5rem',
-        '& > :last-child': {
-          paddingRight: 0
-        },
-        '&$expanded': {
-          margin: '0.75rem 0'
-        }
-      }
-    },
-    MuiExpansionPanelDetails: {
-      root: {
-        padding: 0,
-        borderTop: '0.0625rem solid var(--silver)'
-      }
-    },
-    MuiList: {
-      root: {
-        borderTop: '1px solid var(--silver)',
-        borderBottom: '1px solid var(--silver)'
-      },
-      padding: {
-        paddingTop: 0,
-        paddingBottom: 0
-      }
-    },
-    MuiListItem: {
-      root: {
-        minHeight: '4.25rem',
-        paddingTop: '.5rem',
-        paddingBottom: '.5rem',
-        '&$selected, &$selected:hover': {
-          backgroundColor: 'var(--zircon)'
-        },
-        '&:hover, &:focus': {
-          backgroundColor: 'var(--paleGrey)'
-        },
-        '&:not(:first-child)': {
-          borderTop: '1px solid var(--silver)'
-        }
-      },
-      gutters: {
-        paddingLeft: '1rem',
-        paddingRight: '1rem'
-      },
-      dense: {
-        minHeight: '3.5rem'
-      },
-      secondaryAction: {
-        paddingRight: '.5rem'
-      },
-      button: {
-        '&:hover': {
-          backgroundColor: getCssVariableValue('paleGrey')
-        }
-      }
-    },
-    MuiListSubheader: {
-      root: {
-        borderTop: '1px solid transparent',
-        borderBottom: '1px solid var(--zircon)',
-        marginBottom: '-1px',
-        padding: 0,
-        backgroundColor: 'var(--zircon)',
-        textIndent: '2rem',
-        fontWeight: 'bold',
-        fontSize: '.75rem',
-        lineHeight: 1.33,
-        color: 'var(--coolGrey)'
-      },
-      gutters: {
-        paddingLeft: 0,
-        paddingRight: 0
-      },
-      sticky: {
-        backgroundColor: 'var(--zircon)'
-      }
-    },
-    MuiListItemSecondaryAction: {
-      root: {
-        zIndex: 1
-      }
-    },
-    MuiMenuItem: {
-      root: {
-        minHeight: 'auto',
-        paddingTop: 4,
-        paddingBottom: 4,
-        color: getCssVariableValue('charcoalGrey'),
-        border: 0,
-        '&:last-child': {
-          borderBottom: 0
-        }
-      },
-      gutters: {
-        paddingLeft: 24,
-        paddingRight: 24
-      }
-    },
-    MuiTextField: {
-      borderWidth: '0.0625rem'
-    },
-    MuiFormHelperText: {
-      root: {
-        fontStyle: 'italic',
-        fontSize: '0.875rem'
-      }
-    },
-    MuiDialog: {
-      root: {
-        color: getCssVariableValue('charcoalGrey'),
-        'border-radius': '8px'
-      },
-      paper: {
-        'box-sizing': 'border-box',
-        [theme.breakpoints.down('md')]: {
-          padding: '16px'
-        },
-        [theme.breakpoints.up('md')]: {
-          padding: '32px'
-        }
-      },
-      paperFullScreen: {
-        '&$paperScrollBody': {
-          'min-height': '100%',
-          height: 'auto'
-        }
-      }
-    },
-    MuiDialogContent: {
-      root: {
-        padding: '8px 0'
-      }
-    },
-    MuiDialogActions: {
-      root: {
-        padding: '0'
-      }
-    },
-    MuiDialogTitle: {
-      root: {
-        padding: '0 0 8px 0'
-      }
-    }
-  },
   breakpoints: {
     // Define custom breakpoint values.
     // These will apply to Material-UI components that use responsive
@@ -255,6 +62,200 @@ export const theme = createMuiTheme({
     }
   }
 })
+
+theme.overrides = {
+  MuiOutlinedInput: {
+    root: {
+      '&$disabled': {
+        background: 'var(--paleGrey)'
+      },
+      '&$focused $notchedOutline': {
+        borderWidth: '0.0625rem'
+      }
+    },
+    notchedOutline: {
+      borderColor: 'var(--silver)'
+    }
+  },
+  MuiButton: {
+    root: {
+      borderRadius: 0
+    }
+  },
+  MuiExpansionPanel: {
+    rounded: {
+      borderRadius: defaultValues.borderRadius
+    },
+    root: {
+      boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.08)',
+      borderWidth: '0.0625rem',
+      borderStyle: 'solid',
+      borderColor: 'var(--silver)',
+      overflow: 'hidden',
+      marginBottom: '1rem'
+    }
+  },
+  MuiExpansionPanelSummary: {
+    expanded: {},
+    root: {
+      backgroundColor: 'var(--paleGrey)',
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+      fontSize: '0.875rem',
+      minHeight: '3.5rem',
+      padding: 0,
+      color: 'var(--charcoalGrey)',
+      '&$expanded': {
+        minHeight: '3.5rem'
+      }
+    },
+    content: {
+      margin: '0.75rem 0',
+      paddingLeft: '0.5rem',
+      paddingRight: '0.5rem',
+      '& > :last-child': {
+        paddingRight: 0
+      },
+      '&$expanded': {
+        margin: '0.75rem 0'
+      }
+    }
+  },
+  MuiExpansionPanelDetails: {
+    root: {
+      padding: 0,
+      borderTop: '0.0625rem solid var(--silver)'
+    }
+  },
+  MuiList: {
+    root: {
+      borderTop: '1px solid var(--silver)',
+      borderBottom: '1px solid var(--silver)'
+    },
+    padding: {
+      paddingTop: 0,
+      paddingBottom: 0
+    }
+  },
+  MuiListItem: {
+    root: {
+      minHeight: '4.25rem',
+      paddingTop: '.5rem',
+      paddingBottom: '.5rem',
+      '&$selected, &$selected:hover': {
+        backgroundColor: 'var(--zircon)'
+      },
+      '&:hover, &:focus': {
+        backgroundColor: 'var(--paleGrey)'
+      },
+      '&:not(:first-child)': {
+        borderTop: '1px solid var(--silver)'
+      }
+    },
+    gutters: {
+      paddingLeft: '1rem',
+      paddingRight: '1rem'
+    },
+    dense: {
+      minHeight: '3.5rem'
+    },
+    secondaryAction: {
+      paddingRight: '.5rem'
+    },
+    button: {
+      '&:hover': {
+        backgroundColor: getCssVariableValue('paleGrey')
+      }
+    }
+  },
+  MuiListSubheader: {
+    root: {
+      borderTop: '1px solid transparent',
+      borderBottom: '1px solid var(--zircon)',
+      marginBottom: '-1px',
+      padding: 0,
+      backgroundColor: 'var(--zircon)',
+      textIndent: '2rem',
+      fontWeight: 'bold',
+      fontSize: '.75rem',
+      lineHeight: 1.33,
+      color: 'var(--coolGrey)'
+    },
+    gutters: {
+      paddingLeft: 0,
+      paddingRight: 0
+    },
+    sticky: {
+      backgroundColor: 'var(--zircon)'
+    }
+  },
+  MuiListItemSecondaryAction: {
+    root: {
+      zIndex: 1
+    }
+  },
+  MuiMenuItem: {
+    root: {
+      minHeight: 'auto',
+      paddingTop: 4,
+      paddingBottom: 4,
+      color: getCssVariableValue('charcoalGrey'),
+      border: 0,
+      '&:last-child': {
+        borderBottom: 0
+      }
+    },
+    gutters: {
+      paddingLeft: 24,
+      paddingRight: 24
+    }
+  },
+  MuiTextField: {
+    borderWidth: '0.0625rem'
+  },
+  MuiFormHelperText: {
+    root: {
+      fontStyle: 'italic',
+      fontSize: '0.875rem'
+    }
+  },
+  MuiDialog: {
+    root: {
+      color: getCssVariableValue('charcoalGrey'),
+      'border-radius': '8px'
+    },
+    paper: {
+      'box-sizing': 'border-box',
+      [theme.breakpoints.down('md')]: {
+        padding: '16px'
+      },
+      [theme.breakpoints.up('md')]: {
+        padding: '32px'
+      }
+    },
+    paperFullScreen: {
+      '&$paperScrollBody': {
+        'min-height': '100%',
+        height: 'auto'
+      }
+    }
+  },
+  MuiDialogContent: {
+    root: {
+      padding: '8px 0'
+    }
+  },
+  MuiDialogActions: {
+    root: {
+      padding: '0'
+    }
+  },
+  MuiDialogTitle: {
+    root: {
+      padding: '0 0 8px 0'
+    }
+  }
+}
 
 // Override default shadow for elevation 8 used for Popover
 theme.shadows[8] =
