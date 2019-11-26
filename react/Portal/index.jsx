fb9a70e266dc44a866ecc0662568550ca4d20f1c
Patrick Browne <pt.browne@gmail.com>
2019-11-26 16:04:39 +0100

    fix: Prevent webpack from erroring if preact-portal is not found

--- react/Portal/index.jsx
+++ react/Portal/index.jsx
@@ -1,6 +1,11 @@
 let Portal
 if (process.env.USE_PREACT) {
-  Portal = require('preact-portal')
+  try {
+    Portal = require('preact-portal')
+  } catch (e) {
+    // We need this try/catch for webpack not to error if preact-portal
+    // is not found
+  }
 } else {
   const ReactDOM = require('react-dom')
   Portal = ({ into, children }) => {
