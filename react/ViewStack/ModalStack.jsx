97b92258bc6e79b5ad2452a24e076b34228a757b
Patrick Browne <pt.browne@gmail.com>
2019-11-26 10:46:10 +0100

    docs: Add readme

--- react/ViewStack/ModalStack.jsx
+++ react/ViewStack/ModalStack.jsx
@@ -3,6 +3,18 @@ import Modal from '../Modal'
 import { ViewStackContext } from './context'
 import { useStack } from './hooks'
 
+/**
+ * Wraps children so that they can push views that will appear as modals.
+ *
+ * - Children receive stack{Pop,Push} methods through `useViewStack`.
+ * - When stackPush(<View />) is called, a modal containing <View/> is displayed
+ * - When stackPop() is called, the modal disappears
+ * - Several modals can be stacked in this manner
+ * - The <Modal /> element wrapping <View /> can receive props via the 2nd
+ * argument of stackPush : `stackPush(<SmallView />, { size: 'xsmall' })`
+ *
+ * Can be used as a replacement of a <ViewStack /> on mobile
+ */
 const ModalStack = ({ children }) => {
   const [stChildren, , stackPush, stackPop] = useStack(
     React.Children.toArray(children)
