ec4c8050ea2d4ceddbe8640b3e2523e94d8debc4
Patrick Browne <pt.browne@gmail.com>
2019-12-19 19:57:26 +0100

    feat: Codemod for useI18n
    - A codemod provides basic auto migration for function component to
      use useI18n instead of translate()
    
    ```
    const Component = ({t, f}) => {}
    export default translate()(Component)
    ```
    
    becomes
    
    ```
    import { useI18n } from "cozy-ui/transpiled/react"
    const Component = (t, f) => {
      const { t, f } = useI18n()
    }
    export default Component
    ```

---
+++ codemods/use-i18n-hook.js
@@ -0,0 +1,81 @@
+import makeUtils from './utils'
+
+export default function transformer(file, api) {
+  const j = api.jscodeshift
+  const utils = makeUtils(j)
+  const root = j(file.source)
+
+  const isI18nProp = prop => {
+    return prop.key && (prop.key.name === 't' || prop.key.name === 'f')
+  }
+
+  const findI18nProps = params => {
+    const props = params[0]
+    if (!props) {
+      return
+    }
+    if (props.type !== 'ObjectPattern') {
+      return
+    }
+    return props.properties
+      ? props.properties.filter(isI18nProp).map(prop => prop.key.name)
+      : []
+  }
+
+  const findNearest = (path, condition) => {
+    while (path && !condition(path) && path.parentPath) {
+      path = path.parentPath
+    }
+    return path
+  }
+
+  const replaceI18nPropsByHook = arrowFunctionBodyPath => {
+    const arrowFunctionBody = arrowFunctionBodyPath.node
+    const i18nProps = findI18nProps(arrowFunctionBody.params)
+    if (!i18nProps || !i18nProps.length) {
+      return
+    }
+
+    // TODO why some body do not have splice ?
+    if (!arrowFunctionBody.body.body || !arrowFunctionBody.body.body.splice) {
+      return false
+    }
+
+    arrowFunctionBody.params[0].properties = arrowFunctionBody.params[0].properties.filter(
+      prop => !isI18nProp(prop)
+    )
+
+    arrowFunctionBody.body.body.splice(
+      0,
+      0,
+      `const { ${i18nProps} } = useI18n()`
+    )
+    utils.hoc.removeHOC(arrowFunctionBodyPath, 'translate')
+
+    const declarator = findNearest(
+      arrowFunctionBodyPath,
+      x => x.node.type === 'VariableDeclarator'
+    )
+    const ComponentName = declarator.node.id.name
+    utils.hoc.removeDefaultExportHOC(root, ComponentName, 'translate')
+    return true
+  }
+
+  const components = root.find(j.ArrowFunctionExpression)
+
+  let needToAddUseI18nImport = false
+  components.forEach(path => {
+    if (replaceI18nPropsByHook(path)) {
+      needToAddUseI18nImport = true
+    }
+  })
+
+  if (needToAddUseI18nImport) {
+    utils.imports.add(root, {
+      path: 'cozy-ui/transpiled/react',
+      identifiers: ['useI18n']
+    })
+  }
+
+  return root.toSource()
+}
