ab01c104594c5f656fb29506f1c78c180bfa129b
Patrick Browne <pt.browne@gmail.com>
2020-04-17 09:44:03 +0200

    feat: Add BarTitle component

---
+++ react/BarTitle/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './styles.styl'
+
+const BarTitle = ({ children }) => {
+  return <h1 className={styles.BarTitle}>{children}</h1>
+}
+
+export default BarTitle
