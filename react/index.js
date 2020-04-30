fffd1065ba3f0cf6905b40637bbb0a16385fbdb6
Patrick Browne <pt.browne@gmail.com>
2020-04-30 10:24:57 +0200

    feat: Export CozyTheme

--- react/index.js
+++ react/index.js
@@ -110,3 +110,4 @@ export {
 export { default as BarContextProvider } from './BarContextProvider'
 export { default as BarTitle } from './BarTitle'
 export { default as UploadQueue } from './UploadQueue'
+export { default as CozyTheme } from './CozyTheme'
