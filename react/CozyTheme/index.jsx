c22b6240230197c882552b443cdfdfd4666ff47d
Patrick Browne <pt.browne@gmail.com>
2020-05-07 16:25:39 +0200

    fix: Nested "normal" theme work as expected

--- react/CozyTheme/index.jsx
+++ react/CozyTheme/index.jsx
@@ -1,14 +1,20 @@
 import React, { createContext, useContext } from 'react'
 import MuiCozyTheme from '../MuiCozyTheme'
 import styles from './styles.styl'
+import paletteStyles from '../../stylus/settings/palette.styl'
 import cx from 'classnames'
 
 export const CozyThemeContext = createContext()
 
+const allStyles = {
+  ...styles,
+  'CozyTheme--normal': paletteStyles['CozyTheme--normal']
+}
+
 const CozyTheme = ({ variant, children, className }) => (
   <CozyThemeContext.Provider value={variant}>
     <MuiCozyTheme variant={variant}>
-      <div className={cx(className, styles[`CozyTheme--${variant}`])}>
+      <div className={cx(className, allStyles[`CozyTheme--${variant}`])}>
         {children}
       </div>
     </MuiCozyTheme>
