895603dd7113b561bd1af64fc953d3490a4d4df2
Patrick Browne <pt.browne@gmail.com>
2020-08-31 17:13:12 +0200

    docs: Add doc on Divider

--- react/MuiCozyTheme/Divider/index.jsx
+++ react/MuiCozyTheme/Divider/index.jsx
@@ -1,3 +1,10 @@
+import React from 'react'
 import Divider from '@material-ui/core/Divider'
 
 export default Divider
+
+export const CardDivider = () => {
+  // To circumvent a theme problem, we have to remove the margins
+  // https://github.com/cozy/cozy-ui/issues/1534 */
+  return <Divider className="u-ml-0 u-maw-100" />
+}
