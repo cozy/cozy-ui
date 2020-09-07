b72306026db14b0f7ef1ac072b73fc44bf47d90b
Yannick Lohse <contact@yannick-lohse.fr>
2020-09-07 10:25:45 +0200

    feat: Badge update
    BREAKING CHANGE: Badge component now uses Material-UI. To migrate, rename the "content" prop to "badgeContent", and "type" to "color".

--- react/Badge/Readme.md
+++ react/Badge/Readme.md
@@ -1,55 +1,96 @@
-`<Badge />` component wraps the element on which you want to apply a badge. In the following examples, it is applied to compact `<ButtonAction />`.
+`<Badge />` component wraps the element you want to apply a badge to. It is based on the [Badge component from Material-UI](https://v3.material-ui.com/api/badge/).
 
-You should use `<Badge />` to provide additional information related to the wrapped content.
+### Basic Badges
 
-This component spreads all other props to its root element.
+The default version of badges is used to indicate an amount of new things that the user should see. This could be a number of new messages in an inbox or the number of new files in a folder.
 
-#### Available types
-
-Default type is `normal`
+The `dot` variant can be used to indicate that the item _itself_ is new. A `dot` badge on a folder for example indicates that the folder is new, not that it contains new elements.
 
 ```
-import Badge from 'cozy-ui/transpiled/react/Badge'
-import ButtonAction from 'cozy-ui/transpiled/react/ButtonAction';
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme';
+import Badge from 'cozy-ui/transpiled/react/Badge';
+import Icon from 'cozy-ui/transpiled/react/Icon';
+import Variants from 'cozy-ui/docs/components/Variants';
 
-<div>
-  <p>
-    <Badge content="2" type="normal">
-      <ButtonAction label='Normal' rightIcon='openwith' compact />
-    </Badge>
-  </p>
-  <p>
-    <Badge content="2" type="new">
-      <ButtonAction type='new' leftIcon="plus" label='New' compact />
-    </Badge>
-  </p>
-  <p>
-    <Badge content="2" type="error">
-      <ButtonAction type='error' label='Error' rightIcon='file-none' compact />
-    </Badge>
-  </p>
-</div>
+const initialVariants = [
+  { error: false, dot: false, large: false, small: false },
+  { error: true, dot: true, large: false, small: false },
+  { error: false, dot: false, large: false, small: false },
+];
+
+
+<MuiCozyTheme>
+  <Variants initialVariants={initialVariants}>{
+    variant => (
+      <p>
+        <Badge badgeContent={4} color={variant.error ? 'error' : variant.secondaryColor ? 'secondary' : 'primary'} variant={variant.dot ? 'dot' : 'standard'} size={variant.large ? 'large' : variant.small ? 'small' : 'medium'} anchorOrigin={{vertical: variant.bottom ? 'bottom' : 'top', 'horizontal': variant.left ? 'left' : 'right'}}>
+          <Icon icon="circle-filled" size={variant.large ? '32' : variant.small ? '16' : '24'} color="var(--slateGrey)" />
+        </Badge>
+      </p>
+    )
+  }</Variants>
+</MuiCozyTheme>
 ```
 
+### Qualifier badge
 
-#### Alignment
-
-Only supports `bottom-right` 
-
+The qualifier badge is used to provider extra information about the item it's annotating.
 
 ```
-import Badge from 'cozy-ui/transpiled/react/Badge'
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme';
+import Badge from 'cozy-ui/transpiled/react/Badge';
 import Icon from 'cozy-ui/transpiled/react/Icon';
 
-
-
-<div>
+<MuiCozyTheme>
   <p>
-    <Badge 
-      content={ <Icon icon="link" size={10} />} type="normal" alignment="bottom-right" size='medium'>
-      <Icon icon="cloud" size="32" />
+    <Badge badgeContent={<Icon icon="link" size="10" />} color="primary" variant="qualifier">
+      <Icon icon="circle-filled" size="32" color="var(--slateGrey)" />
     </Badge>
   </p>
-  
-</div>
+</MuiCozyTheme>
+```
+
+### Ghost badge
+
+Ghost badges are specifically used to indicate that a file in a Cozy is a ghost file — it is not truly in the Cozy (usually it is shared from another instance).
+
+```
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme';
+import Badge from 'cozy-ui/transpiled/react/Badge';
+import Icon from 'cozy-ui/transpiled/react/Icon';
+import Avatar from 'cozy-ui/transpiled/react/Avatar';
+
+<MuiCozyTheme>
+  <p>
+    <Badge badgeContent={<Icon icon="folder" size="16" />} color="primary" variant="ghost">
+      <Avatar text="CD" size="small" />
+    </Badge>
+  </p>
+</MuiCozyTheme>
+```
+
+### Double badges
+
+Badges can be combined — in this example, we have a ghost file that is new.
+
+```
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme';
+import Badge from 'cozy-ui/transpiled/react/Badge';
+import Icon from 'cozy-ui/transpiled/react/Icon';
+import Avatar from 'cozy-ui/transpiled/react/Avatar';
+
+<MuiCozyTheme>
+  <p>
+    <Badge
+      badgeContent={
+        <Badge color="error" variant="dot" size="small">
+          <Icon icon="folder" size="16" />
+        </Badge>
+      }
+      color="primary"
+      variant="ghost">
+        <Avatar text="CD" size="small" />
+    </Badge>
+  </p>
+</MuiCozyTheme>
 ```
--- react/Badge/index.jsx
+++ react/Badge/index.jsx
@@ -1,48 +1,167 @@
 import React from 'react'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import styles from './styles.styl'
+import MuiBadge from '@material-ui/core/Badge'
+import { withStyles } from '@material-ui/core/styles'
 
-export const Badge = ({
-  children,
-  content,
-  type,
-  alignment = 'bottom-right',
-  size = 'small',
-  ...props
-}) => {
-  return (
-    <span className={styles['c-badge-root']} {...props}>
-      {children}
-      <span
-        className={cx(styles['c-badge'], {
-          [styles[`c-badge--${alignment}`]]: alignment
-        })}
-      >
-        <div
-          className={cx(styles['c-badge--wrapper'], {
-            [styles[`c-badge--size-${size}`]]: size,
-            [styles[`c-badge--${type}`]]: type
-          })}
-        >
-          {content}
-        </div>
-      </span>
-    </span>
-  )
-}
+const LARGE_BADGE = '1.25rem'
+const MEDIUM_BADGE = '1.125rem'
+const SMALL_BADGE = '1rem'
+
+const LARGE_DOT = '.875rem'
+const MEDIUM_DOT = '.75rem'
+const SMALL_DOT = '.625rem'
+
+const customStyles = theme => ({
+  top: {
+    top: '16%'
+  },
+  bottom: {
+    top: '82%',
+    '&$large': {
+      top: '86%'
+    },
+    '&$qualifier': {
+      top: '80%'
+    },
+    '&$ghost': {
+      top: '76%'
+    }
+  },
+  left: {
+    right: '90%'
+  },
+  right: {
+    right: '10%',
+    '&$qualifier': {
+      right: '12%'
+    },
+    '&$ghost': {
+      right: '12%'
+    }
+  },
+  large: {
+    height: LARGE_BADGE,
+    minWidth: LARGE_BADGE,
+    fontSize: '.6875rem'
+  },
+  medium: {
+    height: MEDIUM_BADGE,
+    minWidth: MEDIUM_BADGE,
+    fontSize: '.625rem'
+  },
+  small: {
+    height: SMALL_BADGE,
+    minWidth: SMALL_BADGE,
+    fontSize: '.5rem',
+    lineHeight: '0',
+    padding: '0 3px'
+  },
+  colorPrimary: {
+    border: `2px solid ${theme.palette.primary.contrastText}`
+  },
+  colorSecondary: {
+    border: `2px solid ${theme.palette.secondary.contrastText}`
+  },
+  colorError: {
+    border: `2px solid ${theme.palette.error.contrastText}`
+  },
+  dot: {
+    '&$large': {
+      height: LARGE_DOT,
+      minWidth: LARGE_DOT
+    },
+    '&$medium': {
+      height: MEDIUM_DOT,
+      minWidth: MEDIUM_DOT
+    },
+    '&$small': {
+      height: SMALL_DOT,
+      minWidth: SMALL_DOT
+    }
+  },
+  qualifier: {
+    backgroundColor: 'white',
+    color: 'var(--slateGrey)',
+    border: '1px solid var(--silver)'
+  },
+  ghost: {
+    backgroundColor: 'white',
+    color: 'var(--coolGrey)',
+    border: '1px solid var(--silver)',
+    borderRadius: '6px',
+    height: 'auto',
+    minWidth: 'auto',
+    padding: '3px'
+  }
+})
+
+const customVariants = ['qualifier', 'ghost']
+
+export const Badge = withStyles(customStyles)(
+  ({ classes, anchorOrigin, size, variant, ...props }) => {
+    const {
+      badge,
+      colorPrimary,
+      colorSecondary,
+      colorError,
+      dot,
+      qualifier,
+      ghost,
+      top,
+      bottom,
+      left,
+      right,
+      large,
+      medium,
+      small,
+      ...customClasses
+    } = classes
+    const isCustomvariant = customVariants.includes(variant)
+    const verticalClasses = { top, bottom }
+    const horizontalClasses = { left, right }
+    const sizeClasses = { large, medium, small }
+    if (isCustomvariant)
+      anchorOrigin = { vertical: 'bottom', horizontal: 'right' }
+
+    return (
+      <MuiBadge
+        classes={{
+          colorPrimary,
+          colorSecondary,
+          colorError,
+          dot,
+          badge: cx(
+            badge,
+            verticalClasses[anchorOrigin.vertical],
+            horizontalClasses[anchorOrigin.horizontal],
+            variant === 'qualifier' ? qualifier : null,
+            variant === 'ghost' ? ghost : null,
+            isCustomvariant ? null : sizeClasses[size],
+            customClasses
+          )
+        }}
+        variant={isCustomvariant ? 'standard' : variant}
+        {...props}
+      />
+    )
+  }
+)
 
 Badge.propTypes = {
-  /** The content of the badge */
-  content: PropTypes.node,
-  /** The type of the badge */
-  type: PropTypes.oneOf(['normal', 'new', 'error']),
-  alignment: PropTypes.oneOf(['bottom-right'])
+  anchorOrigin: PropTypes.shape({
+    horizontal: PropTypes.oneOf('left', 'right'),
+    vertical: PropTypes.oneOf('bottom', 'top')
+  }),
+  size: PropTypes.oneOf('small', 'medium', 'large'),
+  showZero: PropTypes.bool,
+  variant: PropTypes.oneOf('standaard', 'dot', 'qualifier', 'ghost')
 }
 
 Badge.defaultProps = {
-  content: '',
-  type: 'normal'
+  anchorOrigin: { horizontal: 'right', vertical: 'top' },
+  size: 'medium',
+  showZero: true
 }
 
 export default Badge
