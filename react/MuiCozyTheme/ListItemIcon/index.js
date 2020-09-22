6f1575a67d75d46dbfd863b4a43361b4e4781b24
Patrick Browne <pt.browne@gmail.com>
2020-09-22 13:45:38 +0200

    fix: ListItemIcon size is fixed

--- react/MuiCozyTheme/ListItemIcon/index.js
+++ react/MuiCozyTheme/ListItemIcon/index.js
@@ -1,3 +1,9 @@
+import { withStyles } from '@material-ui/core/styles'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 
-export default ListItemIcon
+export default withStyles({
+  root: {
+    width: 32,
+    justifyContent: 'center'
+  }
+})(ListItemIcon)
