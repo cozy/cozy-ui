1cfcf65f83f32364c7c16a7acc8018f4b2f8cc94
Patrick Browne <pt.browne@gmail.com>
2020-11-16 10:02:47 +0100

    fix: Add changelog step

--- release.config.js
+++ release.config.js
@@ -3,6 +3,9 @@ module.exports = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md"
+    }],
     ["@semantic-release/git", {
       "assets": ["CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
