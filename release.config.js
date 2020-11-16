0e124899eb52e1f128b5358f42246d75927642d8
Patrick Browne <pt.browne@gmail.com>
2020-11-16 09:35:37 +0100

    fix: Add npm publication with semantic-release
    We need to setup each plugin. Order of plugins taken from https://github.com/semantic-release/semantic-release/blob/master/docs/usage/plugins.md

--- release.config.js
+++ release.config.js
@@ -7,6 +7,7 @@ module.exports = {
       "assets": ["CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
+    "@semantic-release/npm",
     "@semantic-release/github"
   ]
 }
