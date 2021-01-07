00388fb73c016cf65a8c6d7ee8e8a90d74af2926
Patrick Browne <pt.browne@gmail.com>
2021-01-07 18:05:50 +0100

    chore: Lint

--- react/HistoryRow/index.jsx
+++ react/HistoryRow/index.jsx
@@ -36,9 +36,7 @@ const HistoryRow = ({
         </div>
         <div className="u-media u-media-grow u-stack-xs u-row-m">
           <Bd>
-            <Typography variant="h6">
-              {primaryText}
-            </Typography>
+            <Typography variant="h6">{primaryText}</Typography>
             <Typography gutterBottom variant="h6">
               {tag}
             </Typography>
