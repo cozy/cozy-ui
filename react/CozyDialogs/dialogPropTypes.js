79bef46b5e44509e841465bc6e22e1206d44b10c
Patrick Browne <pt.browne@gmail.com>
2021-01-13 11:40:16 +0100

    fix: OnClose is not required for CozyDialogs

--- react/CozyDialogs/dialogPropTypes.js
+++ react/CozyDialogs/dialogPropTypes.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 export default {
   open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func,
   title: PropTypes.node,
   content: PropTypes.node,
   actions: PropTypes.node,
