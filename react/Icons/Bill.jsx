3ec5e02385ff103bb474eb1b3d70518fe8d7e69e
paultranvan <paul@cozycloud.cc>
2021-01-14 16:11:01 +0100

    feat: Add bill icon

---
+++ react/Icons/Bill.jsx
@@ -0,0 +1,15 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/bill.svg` to regenerate;
+import React from 'react'
+
+function SvgBill(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M13 8a3.99 3.99 0 012.669 1.02l.159.151-1.414 1.415a1.989 1.989 0 00-3.051.28l-.085.134H13v2h-1.722c.347.595.985 1 1.722 1a2 2 0 001.288-.47l.125-.116 1.415 1.415a3.987 3.987 0 01-6.632-1.639L9.142 13H8v-2h1.142A3.99 3.99 0 0113 8zm-8 4a1 1 0 010 2H1a1 1 0 010-2h4zm0-4a1 1 0 010 2H1a1 1 0 010-2h4zm10-8a1 1 0 010 2H1a1 1 0 010-2h14zm0 4a1 1 0 010 2H1a1 1 0 010-2h14z"
+      />
+    </svg>
+  )
+}
+
+export default SvgBill
