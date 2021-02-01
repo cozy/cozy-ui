b0dc2591efc00ef599666e558bf21e6a3cfbfccd
Patrick Browne <pt.browne@gmail.com>
2021-02-01 17:42:21 +0100

    chore: Lint

--- scripts/screenshots.js
+++ scripts/screenshots.js
@@ -366,9 +366,11 @@ const screenshotKSSStyleguide = async (page, args) => {
   const ignore = [/^utilities/]
 
   const WIDTH = 800
-  await page.setViewport({ width: WIDTH, height: 800})
+  await page.setViewport({ width: WIDTH, height: 800 })
   const links = await page.evaluate(() => {
-    const navLinks = Array.from(document.querySelectorAll('.kss-nav > .kss-nav__item > a[href]'))
+    const navLinks = Array.from(
+      document.querySelectorAll('.kss-nav > .kss-nav__item > a[href]')
+    )
     return navLinks.map(node => node.getAttribute('href'))
   })
 
@@ -376,8 +378,9 @@ const screenshotKSSStyleguide = async (page, args) => {
     await page
     await page.goto(resolveLink(link))
 
-    const sections = Array.from(await page.$$('.kss-section--depth-2'))
-      .concat(Array.from(await page.$$('.kss-section--depth-3')))
+    const sections = Array.from(await page.$$('.kss-section--depth-2')).concat(
+      Array.from(await page.$$('.kss-section--depth-3'))
+    )
     for (let section of sections) {
       const idProp = await section.getProperty('id')
       const idValue = await idProp.jsonValue()
@@ -391,16 +394,16 @@ const screenshotKSSStyleguide = async (page, args) => {
 
       // Need to resize the viewport otherwise screenshots are blank
       const body = await page.$('body')
-      const bodySize = await body.boundingBox();
-      await page.setViewport({ height: Math.ceil(bodySize.height), width:  WIDTH });
+      const bodySize = await body.boundingBox()
+      await page.setViewport({
+        height: Math.ceil(bodySize.height),
+        width: WIDTH
+      })
 
       console.log('Screenshotting section', idValue)
       await page.screenshot({
         clip: await section.boundingBox(),
-        path: path.join(
-          args.screenshotDir,
-          `kss-${ref}-${id}.png`
-        )
+        path: path.join(args.screenshotDir, `kss-${ref}-${id}.png`)
       })
     }
   }
@@ -464,7 +467,7 @@ const main = async () => {
   if (args.mode == 'react') {
     await screenshotReactStyleguide(page, args, config)
   } else if (args.mode == 'stack') {
-  await screenshotStackExamples(page, args)
+    await screenshotStackExamples(page, args)
   } else if (args.mode == 'kss') {
     await screenshotKSSStyleguide(page, args)
   }
