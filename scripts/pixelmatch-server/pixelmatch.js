5e8e14cf156d79d1ba237f7020623a03a9b8f0b8
Patrick Browne <pt.browne@gmail.com>
2021-02-03 15:41:07 +0100

    feat: Allow screenshots of different heights to be compared
    Pixelmatch can only compare images with the same dimensions.
    Resize the shortest screenshot to be as tall as the tallest screenshot.

--- scripts/pixelmatch-server/pixelmatch.js
+++ scripts/pixelmatch-server/pixelmatch.js
@@ -15,17 +15,78 @@ const parsePixelmatchOutput = output => {
   }
 }
 
+const fileOutputRegex = /(\d+) x (\d+)/
+const parseFileOutput = output => {
+  const match = fileOutputRegex.exec(output)
+  if (!match) {
+    return {
+      width: NaN,
+      height: NaN
+    }
+  } else {
+    return {
+      width: parseInt(match[1], 0),
+      height: parseInt(match[2], 0)
+    }
+  }
+}
+
+const getDimensions = async filename => {
+  const { stdout: fileOutput } = await spawnP(`file`, [filename])
+  const dimensions = parseFileOutput(fileOutput)
+  return dimensions
+}
+
+const resizeImage = (filename, options) => {
+  return spawnP(`mogrify`, [
+    '-background',
+    `${options.background}`,
+    '-extent',
+    `${options.width}x${options.height}`,
+    filename
+  ])
+}
+
 const performDiff = async (app, filename) => {
   const pristineDir = app.get('pristineDir')
   const currentDir = app.get('currentDir')
   const diffsDir = app.get('diffsDir')
 
-  const { stdout } = await spawnP(`pixelmatch`, [
-    path.join(pristineDir, filename),
-    path.join(currentDir, filename),
-    path.join(diffsDir, filename)
+  const pristineFilename = path.join(pristineDir, filename)
+  const currentFilename = path.join(currentDir, filename)
+  const diffFilename = path.join(diffsDir, filename)
+
+  const { width: pristineW, height: pristineH } = await getDimensions(
+    pristineFilename
+  )
+
+  const { width: currentW, height: currentH } = await getDimensions(
+    currentFilename
+  )
+
+  // Make sure image have the same height for pixelmatch to work
+  // correctly
+  if (currentH < pristineH) {
+    await resizeImage(currentFilename, {
+      width: currentW,
+      height: pristineH,
+      background: 'pink'
+    })
+  } else if (currentH > pristineH) {
+    await resizeImage(pristineFilename, {
+      width: pristineW,
+      height: currentH,
+      background: 'pink'
+    })
+  }
+
+  const { stdout: pmOutput } = await spawnP(`pixelmatch`, [
+    pristineFilename,
+    currentFilename,
+    diffFilename
   ])
-  fs.writeFileSync(`diffs/${filename}.pixelmatch.txt`, stdout)
+
+  fs.writeFileSync(`diffs/${filename}.pixelmatch.txt`, pmOutput)
 }
 
 module.exports = {
