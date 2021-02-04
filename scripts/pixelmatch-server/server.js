90daa1c9d824cee5435d36325ed9dc0e675e5606
Patrick Browne <pt.browne@gmail.com>
2021-02-04 15:48:06 +0100

    feat: Pixelmatch server crashes if folders are not set up

--- scripts/pixelmatch-server/server.js
+++ scripts/pixelmatch-server/server.js
@@ -53,35 +53,92 @@ const shouldRegenerateDiff = async (currentFile, diffFile) => {
   return diffStat.mtime < currentStat.mtime
 }
 
-const q = new Queue(10)
+const fsExistsPromise = filename => {
+  return new Promise((resolve, reject) => {
+    fs.exists(filename, (res, err) => {
+      if (res !== undefined) {
+        resolve(res)
+      } else {
+        console.log(res)
+        reject(err)
+      }
+    })
+  })
+}
 
-app.set('pristineDir', path.join(__dirname, '../../old_screenshots'))
-app.set('currentDir', path.join(__dirname, '../../screenshots'))
-app.set('diffsDir', path.join(__dirname, '../../diffs'))
-
-app.get('/', async (req, res) => {
-  const data = await index(req)
-  res.send(data)
-})
-
-app.use('/screenshots', express.static(app.get('currentDir')))
-app.use('/old_screenshots', express.static(app.get('pristineDir')))
-app.use(
-  '/diffs',
-  async (req, res, next) => {
-    const filename = req.url.slice(1)
-    const currentFile = path.join(app.get('currentDir'), filename)
-    const diffFile = path.join(app.get('diffsDir'), filename)
-    const shouldDiff = await shouldRegenerateDiff(currentFile, diffFile)
-    if (shouldDiff) {
-      q.enqueue(() => performDiff(app, filename)).then(next)
-    } else {
-      next()
+const asyncAssert = async (prom, message) => {
+  try {
+    const res = await prom
+    if (!res) {
+      console.error(message)
+      process.exit(1)
     }
-  },
-  express.static(app.get('diffsDir'))
-)
+  } catch (e) {
+    console.log('e', e)
+    const err = new Error(`Error inside asyncAssert: ${e.message}`)
+    err.original = e
+    throw err
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Pixelmatch server serving on http://localhost:${port}`)
-})
+const main = async () => {
+  const pristineScreenshotsDir = path.join(
+    __dirname,
+    '../../pristine_screenshots'
+  )
+  const currentScreenshotsDir = path.join(__dirname, '../../screenshots')
+  const diffsDir = path.join(__dirname, '../../diffs')
+
+  await asyncAssert(
+    fsExistsPromise(pristineScreenshotsDir),
+    `${pristineScreenshotsDir} does not exist. Please create it and store pristine screenshots there.`
+  )
+  await asyncAssert(
+    fsExistsPromise(currentScreenshotsDir),
+    `${currentScreenshotsDir} does not exist. Please create it and store screenshots there.`
+  )
+  await asyncAssert(
+    fsExistsPromise(diffsDir),
+    `${diffsDir} does not exist. Please create it.`
+  )
+
+  app.set('pristineDir', pristineScreenshotsDir)
+  app.set('currentDir', currentScreenshotsDir)
+  app.set('diffsDir', diffsDir)
+
+  app.get('/', async (req, res) => {
+    const data = await index(req)
+    res.send(data)
+  })
+
+  app.use('/screenshots', express.static(app.get('currentDir')))
+  app.use('/old_screenshots', express.static(app.get('pristineDir')))
+
+  const diffQueue = new Queue(10)
+  app.use(
+    '/diffs',
+    async (req, res, next) => {
+      const filename = req.url.slice(1)
+      const currentFile = path.join(app.get('currentDir'), filename)
+      const diffFile = path.join(app.get('diffsDir'), filename)
+      const shouldDiff = await shouldRegenerateDiff(currentFile, diffFile)
+      if (shouldDiff) {
+        diffQueue.enqueue(() => performDiff(app, filename)).then(next)
+      } else {
+        next()
+      }
+    },
+    express.static(app.get('diffsDir'))
+  )
+
+  app.listen(port, () => {
+    console.log(`Pixelmatch server serving on http://localhost:${port}`)
+  })
+}
+
+if (require.main === module) {
+  main().catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+}
