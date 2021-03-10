8f768082ba0f52958ab7d24bd138537b80248b99
Patrick Browne <pt.browne@gmail.com>
2021-03-10 18:02:43 +0100

    feat: Improve useCycle and add doc

--- react/hooks/useCycle.jsx
+++ react/hooks/useCycle.jsx
@@ -1,6 +1,16 @@
 import { useState, useCallback, useEffect } from 'react'
 
-const useCarrousel = (initialState, min, max) => {
+/**
+ * When calling setPrev when we are at index min, goes to max
+ * When calling setNext when we are at index max, goes to min
+ *
+ * @param  {number} initialState - If initialState becomes defined (undefined -> value),
+ * index will be set to value (useful when the initial state is not known yet)
+ * @param  {number} min
+ * @param  {number} max
+ * @return {Array} - [index, setPrev, setNext]
+ */
+const useCycle = (initialState, min, max) => {
   const [curIndex, setIndex] = useState(initialState)
 
   const handlePrev = useCallback(() => {
@@ -13,8 +23,10 @@ const useCarrousel = (initialState, min, max) => {
     setIndex(next > max ? min : next)
   }, [curIndex, max, min])
 
+  // If the initialState is not initially set, the index will be undefined and
+  // when it becomes defined, index will be set to the defined initialState value
   useEffect(() => {
-    if (curIndex === undefined && initialState !== initialState) {
+    if (curIndex === undefined && initialState !== undefined) {
       setIndex(initialState)
     }
   }, [curIndex, initialState])
@@ -22,4 +34,4 @@ const useCarrousel = (initialState, min, max) => {
   return [curIndex, handlePrev, handleNext]
 }
 
-export default useCarrousel
+export default useCycle
