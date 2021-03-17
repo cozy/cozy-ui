e8a7d02ab5c13fc035e82a58ca535ed0fe15a2c8
Patrick Browne <pt.browne@gmail.com>
2021-03-17 12:16:57 +0100

    fix: NestedSelect would not show back button when navigating deeply
    The showBack property was not to be taken on the props of the
    NestedSelectModal but is managed by the NestedSelect that renders
    the HeaderComponent with the showBack property if we are inside
    a category.

--- react/NestedSelect/Modal.jsx
+++ react/NestedSelect/Modal.jsx
@@ -1,21 +1,46 @@
 import React from 'react'
 import Icon from '../Icon'
 
-import { Img } from '../Media'
+import { Media, Bd, Img } from '../Media'
 import palette from '../palette'
 import NestedSelect from './NestedSelect'
 import Dialog, {
   DialogTitle,
   DialogContent
 } from 'cozy-ui/transpiled/react/Dialog'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import { useCozyDialog, DialogCloseButton } from '../CozyDialogs'
 
 import styles from './styles.styl'
 
 import LeftIcon from 'cozy-ui/transpiled/react/Icons/Left'
 
+const NestedSelectDialogHeader = ({ onClickBack, showBack, title }) => {
+  const { dialogTitleProps } = useCozyDialog({
+    open: true
+  })
+  return (
+    <DialogTitle {...dialogTitleProps}>
+      <Media>
+        {showBack && (
+          <Img className={styles.Modal__back}>
+            <IconButton onClick={onClickBack}>
+              <Icon icon={LeftIcon} color={palette['coolGrey']} />
+            </IconButton>
+          </Img>
+        )}
+        <Bd>{title}</Bd>
+      </Media>
+    </DialogTitle>
+  )
+}
+
+const NestedSelectDialogContent = ({ children }) => {
+  return <DialogContent className="u-p-0">{children}</DialogContent>
+}
+
 const NestedSelectModal = props => {
-  const { dialogProps, dialogTitleProps } = useCozyDialog({
+  const { dialogProps } = useCozyDialog({
     open: true
   })
   return (
@@ -25,21 +50,15 @@ const NestedSelectModal = props => {
       title={props.title}
       onClose={props.onClose}
     >
-      <DialogTitle {...dialogTitleProps}>
-        {props.showBack && (
-          <Img className={styles.Modal__back} onClick={props.onClickBack}>
-            <Icon icon={LeftIcon} color={palette['coolGrey']} />
-          </Img>
-        )}
-        {props.title}
-      </DialogTitle>
       <DialogCloseButton
         onClick={props.onClose}
         data-test-id={`modal-close-button-nested-select`}
       />
-      <DialogContent className="u-p-0">
-        <NestedSelect {...props} />
-      </DialogContent>
+      <NestedSelect
+        HeaderComponent={NestedSelectDialogHeader}
+        ContentComponent={NestedSelectDialogContent}
+        {...props}
+      />
     </Dialog>
   )
 }
