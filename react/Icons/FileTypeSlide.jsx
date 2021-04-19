0924978f4bd354fda709d15dc8e5c03b601aaed9
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-04-19 11:58:15 +0200

    feat: Replace Slide icon by vertical one

--- react/Icons/FileTypeSlide.jsx
+++ react/Icons/FileTypeSlide.jsx
@@ -1,4 +1,4 @@
-// Automatically created, please run `scripts/generate-svg-icon.sh assets/icons/illus/file-type-slide.svg` to regenerate;
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/illus/file-type-slide.svg` to regenerate;
 import React from 'react'
 
 function SvgFileTypeSlide(props) {
@@ -6,12 +6,12 @@ function SvgFileTypeSlide(props) {
     <svg viewBox="0 0 32 32" {...props}>
       <g fill="none" fillRule="evenodd">
         <path
+          d="M3 2.002C3 .896 3.89 0 4.997 0H22l7 7v22.996A2 2 0 0127.003 32H4.997A1.995 1.995 0 013 29.998V2.002z"
           fill="#FFDEC1"
-          d="M0 3.991C0 2.891.89 2 2.004 2H25l7 7v19.005C32 29.107 31.11 30 29.998 30H2.002C.896 30 0 29.1 0 28.009V3.99z"
         />
         <path
+          d="M21.5 0c-.276 0-.5.23-.5.5V8h7.5c.276 0 .5-.232.5-.5V7l-7-7h-.5zM22.996 24v2h-10v-2h10zM11 24v2H9v-2h2zm11.996-4v2h-10v-2h10zM11 20v2H9v-2h2zm5-10v4h4a4 4 0 11-4-4zm1-1c2 0 4 2 4 4h-4V9z"
           fill="#FF9433"
-          d="M24.5 2c-.276 0-.5.23-.5.5V10h7.5c.276 0 .5-.232.5-.5V9l-7-7h-.5zM9.996 18h16v2h-16v-2zm0 4h16v2h-16v-2zM6 18h2v2H6v-2zm0 4h2v2H6v-2zm10-6a4 4 0 004-4h-4V8a4 4 0 100 8zm1-5V7c2 0 4 2 4 4h-4z"
         />
       </g>
     </svg>
