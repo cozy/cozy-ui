b1c13c5a2ebc989597f3429d6ecaf8340cd06dc0
Patrick Browne <pt.browne@gmail.com>
2021-05-27 15:24:01 +0200

    feat: Put styles as part of theme
    This way ListItemIcon is only re-exported and we have access to
    its type definition in apps

--- react/MuiCozyTheme/ListItemIcon/index.js
+++ react/MuiCozyTheme/ListItemIcon/index.js
@@ -1,13 +1,7 @@
-import { withStyles } from '@material-ui/core/styles'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 
 export const smallSize = 16
 export const mediumSize = 24
 export const largeSize = 32
 
-export default withStyles({
-  root: {
-    width: 32,
-    justifyContent: 'center'
-  }
-})(ListItemIcon)
+export default ListItemIcon
