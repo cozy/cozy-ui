98ea26d03b25b27c6913484110cc441adbeddb8f
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-06-24 11:56:08 +0200

    fix: InvertedTheme success and action colors
    Some of color with alpha use black color as a basis,
    unfortunately not the dynamic approach with primary
    color as param. See MUI default theme doc.
    So in the inverted theme, that use white color as basis,
    we have to override them.

--- react/MuiCozyTheme/theme.jsx
+++ react/MuiCozyTheme/theme.jsx
@@ -5,6 +5,7 @@ import merge from 'lodash/merge'
 import { getCssVariableValue } from '../utils/color'
 import isTesting from '../helpers/isTesting'
 import AccordionExpandIcon from './AccordionExpandIcon'
+import defaultPalette from '../../theme/palette.json'
 
 export const defaultValues = {
   borderRadius: 6,
@@ -650,19 +651,30 @@ const invertedPalette = {
     selected: getCssVariableValue('primaryColorDark')
   },
   primary: {
-    main: '#fff'
+    main: '#FFFFFF'
   },
   secondary: {
-    main: '#fff'
+    main: '#FFFFFF'
   },
   text: {
-    primary: '#fff',
-    secondary: '#fff'
+    primary: '#FFFFFF',
+    secondary: '#FFFFFF'
+  },
+  success: {
+    main: defaultPalette.Success['400']
   },
   error: {
     main: '#fcc0c0' // lighten($errorColor, 70%)
   },
   divider: '#FFFFFF29' // 16% opacity
+}
+invertedPalette.action = {
+  active: `${invertedPalette.primary.main}8A`, // 54% opacity
+  hover: `${invertedPalette.primary.main}0A`, // 4% opacity
+  selected: `${invertedPalette.primary.main}14`, // 8% opacity
+  disabled: `${invertedPalette.primary.main}40`, // 26% opacity
+  disabledBackground: `${invertedPalette.primary.main}1F`, // 12% opacity
+  focus: `${invertedPalette.primary.main}1F` // 12% opacity
 }
 
 const invertedTypography = makeTypography(invertedPalette)
