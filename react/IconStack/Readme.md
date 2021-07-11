ac2c0ea675650fa8aa9d4cd607e9cf15dc1e420a
Alexis G <merkkur39@gmail.com>
2021-07-11 17:59:31 +0200

    feat: Improve IconStack

--- react/IconStack/Readme.md
+++ react/IconStack/Readme.md
@@ -10,8 +10,11 @@ import IconStack from 'cozy-ui/transpiled/react/IconStack';
 import Icon from 'cozy-ui/transpiled/react/Icon';
 import FileDuotoneIcon from "cozy-ui/transpiled/react/Icons/FileDuotone";
 import BankIcon from "cozy-ui/transpiled/react/Icons/Bank";
+
 <IconStack
-  background={<Icon icon={FileDuotoneIcon} color="blue"  size={32} />}
-  foreground={<Icon icon={BankIcon} color="red" height={16} width={16} />}
+  backgroundIcon={<Icon icon={FileDuotoneIcon} color="blue"  size={32} />}
+  foregroundIcon={<Icon icon={BankIcon} color="red" height={16} width={16} />}
+  backgroundClassName={'optional-bg-wrapper-class'}
+  foregroundClassName={'optional-fg-wrapper-class'}
 />
 ```
--- react/IconStack/index.jsx
+++ react/IconStack/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import classNames from 'classnames'
+
+import migrateProps from '../helpers/migrateProps'
 import styles from './styles.styl'
 
-const IconStack = ({ className, background, foreground }) => {
+const IconStack = ({
+  backgroundClassName,
+  foregroundClassName,
+  backgroundIcon,
+  foregroundIcon
+}) => {
   return (
-    <div className={classNames(styles['IconStack-wrapper'], className)}>
-      {background}
-      <div className={classNames(styles['IconStack-foregroundIcon'])}>
-        {foreground}
+    <div
+      className={classNames(styles['IconStack-wrapper'], backgroundClassName)}
+    >
+      {backgroundIcon}
+      <div
+        className={classNames(
+          styles['IconStack-foregroundIcon'],
+          foregroundClassName
+        )}
+      >
+        {foregroundIcon}
       </div>
     </div>
   )
 }
 
-export default IconStack
+export default migrateProps([
+  { src: 'className', dest: 'backgroundClassName' },
+  { src: 'background', dest: 'backgroundIcon' },
+  { src: 'foreground', dest: 'foregroundIcon' }
+])(IconStack)
