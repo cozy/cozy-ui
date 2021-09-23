53ef0ceb352946e98a0300c8a047f9a9b56ca302
Florian Pires <florianpires@cozycloud.cc>
2021-09-23 17:18:30 +0200

    fix: Lint

--- examples/webpack-app/index.js
+++ examples/webpack-app/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment} from 'react'
+import React, { Fragment } from 'react'
 import { render } from 'react-dom'
 import {
   Nav,
@@ -25,9 +25,15 @@ const NavLink = genNavLink(RRNavLink)
 
 const ContactChip = ({ contact }) => (
   <Chip style={{ paddingLeft: '0.25rem' }}>
-    <Avatar textId={ contact.name } text={contact.initials} size='small' style={{ marginRight: '0.5rem' }}/> {contact.name}
+    <Avatar
+      textId={contact.name}
+      text={contact.initials}
+      size="small"
+      style={{ marginRight: '0.5rem' }}
+    />{' '}
+    {contact.name}
   </Chip>
-);
+)
 
 const icons = [
   'arrow',
@@ -87,33 +93,30 @@ const icons = [
   'spinner-blue',
   'spinner-grey',
   'spinner-red',
-  'spinner-white',
+  'spinner-white'
 ]
 
-const colors = [
-  'crimson',
-  'royalblue',
-  'green',
-  'orange',
-  'crimson'
-]
+const colors = ['crimson', 'royalblue', 'green', 'orange', 'crimson']
 
 const IconSection = () => {
   let colorIndex = 0
   return (
     <Fragment>
       <h2>Icon</h2>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(max-content, 200px))' }}>
-        {
-          icons.map(iconId => (
-            <div key={iconId}>
-              <span style={{ color: colors[colorIndex++ % colors.length] }}>
-                <Icon icon={`#${iconId}`} style={{ marginRight: '0.5rem' }}/>
-              </span>
-              { iconId }
-            </div>
-          ))
-        }
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(max-content, 200px))'
+        }}
+      >
+        {icons.map(iconId => (
+          <div key={iconId}>
+            <span style={{ color: colors[colorIndex++ % colors.length] }}>
+              <Icon icon={`#${iconId}`} style={{ marginRight: '0.5rem' }} />
+            </span>
+            {iconId}
+          </div>
+        ))}
       </div>
     </Fragment>
   )
@@ -124,55 +127,58 @@ const ChipSection = () => {
     <Fragment>
       <h2>Chip</h2>
       <p>
-        {
-          [
-            'Charles Babbage',
-            'Grace Hopper',
-            'John McCarthy',
-            'Donald Knuth',
-            'Barbara Liskov'
-          ].map(name => <ContactChip key={name} contact={{
-            initials: name.split(' ').map(x => x[0]).join(''),
-            name
-          }} />)
-        }
-
+        {[
+          'Charles Babbage',
+          'Grace Hopper',
+          'John McCarthy',
+          'Donald Knuth',
+          'Barbara Liskov'
+        ].map(name => (
+          <ContactChip
+            key={name}
+            contact={{
+              initials: name
+                .split(' ')
+                .map(x => x[0])
+                .join(''),
+              name
+            }}
+          />
+        ))}
       </p>
     </Fragment>
   )
 }
 
-
-
 const App = () => (
-      <Layout>
-        <Sidebar>
-          <Nav>
-            <NavItem>
-              <NavLink>
-                <NavIcon icon="#icon-warning"/>
-                <NavText>Section 1</NavText>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink>
-                <NavIcon icon="#icon-file"/>
-                <NavText>Section 2</NavText>
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Sidebar>
-        <Main>
-          <Content>
-            <p>
-              <Button label='A button' />
-            </p>
-            <IconSection />
-            <ChipSection />
-          </Content>
-          <IconSprite />
-        </Main>
-      </Layout>
-);
+  <Layout>
+    <Sidebar>
+      <Nav>
+        <NavItem>
+          <NavLink>
+            <NavIcon icon="#icon-warning" />
+            <NavText>Section 1</NavText>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <NavIcon icon="#icon-file" />
+            <NavText>Section 2</NavText>
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Sidebar>
+    <Main>
+      <Content>
+        <p>
+          <Button label="A button" />
+        </p>
+        <IconSection />
+        <ChipSection />
+      </Content>
+      <Sprite />
+    </Main>
+  </Layout>
+)
 const node = document.querySelector('#app')
 render(<App />, node)
