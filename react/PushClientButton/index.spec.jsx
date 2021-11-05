48d7c859aee9ddd69b120cf3cd758dec00cda4a2
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-11-05 11:53:53 +0100

    test: Fix warning for missing required prop for PushClientButton

--- react/PushClientButton/index.spec.jsx
+++ react/PushClientButton/index.spec.jsx
@@ -10,14 +10,16 @@ jest.mock('cozy-ui/transpiled/react/Icons/DeviceLaptop', () => () => (
 
 describe('PushClientButton', () => {
   it('should use DeviceLaptop if icon prop is not defined', async () => {
-    const { getByTestId } = render(<PushClientButton />)
+    const { getByTestId } = render(<PushClientButton label="label" href="#" />)
 
     expect(getByTestId(/device-laptop/)).toBeInTheDocument()
   })
 
   it('should accept a custom icon', async () => {
     const CustomIcon = () => <div data-testid="custom-icon" />
-    const { getByTestId } = render(<PushClientButton icon={CustomIcon} />)
+    const { getByTestId } = render(
+      <PushClientButton label="label" href="#" icon={CustomIcon} />
+    )
 
     expect(getByTestId(/custom-icon/)).toBeInTheDocument()
   })
