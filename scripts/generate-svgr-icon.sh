206cd4b6bfaaaa4a6b7000aa3d391acf6d004508
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-01-19 10:08:30 +0100

    feat: Svgr generator script can now deal with folder

--- scripts/generate-svgr-icon.sh
+++ scripts/generate-svgr-icon.sh
@@ -2,13 +2,26 @@
 
 set -euo pipefail
 
-svgFileFullPath=$1
-svgFilename=`basename $svgFileFullPath`
+createSvgr()
+{
+  svgFileFullPath=$1
+  svgFilename=`basename $svgFileFullPath`
+  jsFilename=`echo $svgFilename | gsed -E 's/^([a-z])/\U\1/g; s/-([a-z])/\U\1/g; s/svg$/jsx/g'`
+  jsFileFullPath="react/Icons/${jsFilename}"
 
-jsFilename=`echo $svgFilename | gsed -E 's/^([a-z])/\U\1/g; s/-([a-z])/\U\1/g; s/svg$/jsx/g'`
-jsFileFullPath="react/Icons/${jsFilename}"
+  echo $svgFileFullPath
 
-node_modules/.bin/svgr $svgFileFullPath \
-  --no-dimensions \
-  --template scripts/svgr-template.js > $jsFileFullPath
-node_modules/.bin/eslint --fix $jsFileFullPath
+  node_modules/.bin/svgr $svgFileFullPath \
+    --no-dimensions \
+    --template scripts/svgr-template.js > $jsFileFullPath
+  node_modules/.bin/eslint --fix $jsFileFullPath
+}
+
+if [ -d "$1" ]; then
+  for i in "$1"/*.svg
+  do
+    createSvgr $i
+  done
+else
+  createSvgr $1
+fi
