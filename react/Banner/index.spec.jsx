de25529e162533411b7f76b5f44d0fd4a682844c
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-02-21 16:50:02 +0100

    fix(Grid): Use justifyContent props inside Grid component
    This triggers a lot of warnings in our application
    
    cozy-sharing Banner is waiting for that change

---
+++ react/Banner/index.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Banner from './index'
+
+describe('Grid', () => {
+  it('should render correctly justify content', () => {
+    // Given
+    console.error = jest.fn()
+
+    // When
+    const { container } = render(<Banner />)
+
+    // Then
+    expect(console.error).not.toHaveBeenCalled()
+    expect(
+      container.querySelector('.MuiGrid-justify-content-xs-space-between')
+    ).toBeInTheDocument()
+  })
+})
