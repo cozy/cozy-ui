29f32bd62b2a1b8dbb199a48ca52e4131a5b6f28
Crash-- <quentin.valmori@gmail.com>
2022-04-19 11:38:49 +0200

    refactor: Deactivate audio preload when testing
    In order to avoid false positive in argos

--- react/Viewer/AudioViewer.jsx
+++ react/Viewer/AudioViewer.jsx
@@ -5,12 +5,17 @@ import FileTypeAudioIcon from '../Icons/FileTypeAudio'
 import withFileUrl from './withFileUrl'
 import styles from './styles.styl'
 
+import isTesting from '../../react/helpers/isTesting'
 
 const AudioViewer = ({ file, url }) => (
   <div className={styles['viewer-audioviewer']}>
     <Icon icon={FileTypeAudioIcon} width={160} height={140} />
     <p className={styles['viewer-filename']}>{file.name}</p>
-    <audio src={url} controls="controls" />
+    <audio
+      src={url}
+      controls="controls"
+      preload={isTesting() ? 'none' : 'auto'}
+    />
   </div>
 )
 
