41eca0af345f7940ebab7f5403ba4286ee8f3b07
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-04-29 16:37:08 +0200

    fix: Export toolbarPropsPropType correctly
    to be able to `import { toolbarPropsPropType } from
    'cozy-ui/transpiled/react/Viewer'`

--- react/Viewer/index.jsx
+++ react/Viewer/index.jsx
@@ -2,7 +2,7 @@ import ViewerContainer from './ViewerContainer'
 
 export { default as Viewer } from './Viewer'
 export { default as ViewerContainer } from './ViewerContainer'
-export { toolbarPropsPropType } from './proptypes'
+export * as toolbarPropsPropType from './proptypes'
 export {
   default as ViewerWithCustomPanelAndFooter
 } from './ViewerWithCustomPanelAndFooter'
