c8486c39dd953d748e1b02e05848d98991ceb35c
Merkur39 <merkkur39@gmail.com>
2022-06-30 10:51:48 +0200

    feat(Icon): Change `setting` icon

--- react/Icons/Setting.jsx
+++ react/Icons/Setting.jsx
@@ -1,4 +1,4 @@
-// Automatically created, please run `scripts/generate-svg-icon.sh assets/icons/ui/setting.svg` to regenerate;
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/setting.svg` to regenerate;
 import React from 'react'
 
 function SvgSetting(props) {
@@ -6,7 +6,8 @@ function SvgSetting(props) {
     <svg viewBox="0 0 16 16" {...props}>
       <path
         fillRule="evenodd"
-        d="M0 13v2h5v-2H0zM0 1v2h9V1H0zm9 15v-1h7v-2H9v-1H7v4h2zM3 6v1H0v2h3v1h2V6H3zm13 3V7H7v2h9zm-5-5h2V3h3V1h-3V0h-2v4z"
+        clipRule="evenodd"
+        d="M13.855 5a3.502 3.502 0 01-6.71 0H1a1 1 0 010-2h6.145a3.502 3.502 0 016.71 0H15a1 1 0 110 2h-1.145zM12 4a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM5.5 15.5A3.502 3.502 0 008.855 13H15a1 1 0 100-2H8.855a3.502 3.502 0 00-6.71 0H1a1 1 0 100 2h1.145A3.502 3.502 0 005.5 15.5zM7 12a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
       />
     </svg>
   )
