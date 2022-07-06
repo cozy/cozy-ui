cb9363d3f6de63d894f126f44521c33b1bf6b1c5
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-07-06 16:53:10 +0200

    fix(Chips): Using classname doesn't break style anymore

--- react/Chips/index.jsx
+++ react/Chips/index.jsx
@@ -3,10 +3,16 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 import MuiChip from '@material-ui/core/Chip'
 
-const Chips = ({ label, variant = 'default', color = 'primary', ...props }) => {
+const Chips = ({
+  label,
+  variant = 'default',
+  color = 'primary',
+  className,
+  ...props
+}) => {
   return (
     <MuiChip
-      className={cx({
+      className={cx(className, {
         noLabel: !label,
         ghost: variant === 'ghost',
         [`customColor-${color}`]: color
@@ -22,7 +28,8 @@ const Chips = ({ label, variant = 'default', color = 'primary', ...props }) => {
 Chips.propTypes = {
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   variant: PropTypes.oneOf(['default', 'active', 'ghost']),
-  color: PropTypes.oneOf(['primary', 'success', 'error', 'warning', 'info'])
+  color: PropTypes.oneOf(['primary', 'success', 'error', 'warning', 'info']),
+  className: PropTypes.string
 }
 
 export default Chips
