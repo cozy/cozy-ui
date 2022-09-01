258b91e962b24a45c6e0bd5006c521a079217dc5
Merkur39 <merkkur39@gmail.com>
2022-09-01 15:48:35 +0200

    feat: Add copy icon

---
+++ react/Icons/Copy.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/copy.svg` to regenerate;
+import React from 'react'
+
+function SvgCopy(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14 0a2 2 0 012 2v8a2 2 0 01-2 2h-2v2a2 2 0 01-2 2H3a2 2 0 01-2-2V6a2 2 0 012-2h2V2a2 2 0 012-2h7zm-4 6H3v8h7V6zm4-4H7v2h3a2 2 0 012 2v4h2V2z"
+      />
+    </svg>
+  )
+}
+
+export default SvgCopy
