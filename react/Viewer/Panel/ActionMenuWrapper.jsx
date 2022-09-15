53f1ebf8972bf0479e3bb4afb672be623b9d837f
Merkur39 <merkkur39@gmail.com>
2022-09-15 20:16:24 +0200

    refactor(Viewer): Split ActionMenuWrapper component
    Use ActionMenuDesktop & ActionMenuMobile components

--- react/Viewer/Panel/ActionMenuWrapper.jsx
+++ react/Viewer/Panel/ActionMenuWrapper.jsx
@@ -1,43 +1,20 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
-import cx from 'classnames'
 
 import { useAppLinkWithStoreFallback, useClient } from 'cozy-client'
 
-import List from '../../MuiCozyTheme/List'
-import ListItem from '../../MuiCozyTheme/ListItem'
-import ListItemIcon from '../../MuiCozyTheme/ListItemIcon'
-import ListItemText from '../../ListItemText'
-import Icon from '../../Icon'
-import Copy from '../../Icons/Copy'
-import Edit from '../../Icons/Rename'
-import BottomSheet, { BottomSheetItem } from '../../BottomSheet'
-import ActionMenu, { ActionMenuItem } from '../../ActionMenu'
-import Typography from '../../Typography'
 import useBreakpoints from '../../hooks/useBreakpoints'
 import { useI18n } from '../../I18n'
 import useViewerSnackbar from '../snackbar/ViewerSnackbarProvider'
-import AppLinker from '../../AppLinker'
 import { getCurrentModel } from '../helpers'
-import { makeStyles } from '../../styles'
-
-const useStyles = makeStyles(theme => ({
-  disabledItem: {
-    cursor: 'default',
-    '&:hover': {
-      backgroundColor: 'initial'
-    }
-  },
-  disabledIcon: {
-    fill: theme.palette.text.disabled
-  },
-  disabledTypography: {
-    color: theme.palette.text.disabled
-  }
-}))
+import ActionMenuMobile from './ActionMenuMobile'
+import ActionMenuDesktop from './ActionMenuDesktop'
 
 const mespapiersAppSlug = 'mespapiers'
-const isNotEditableAttributes = ['datetime', 'qualification', 'contact']
+const checkEditableAttribute = name => {
+  const isNotEditableAttributes = ['datetime', 'qualification', 'contact']
+  return !isNotEditableAttributes.includes(name)
+}
 
 const ActionMenuWrapper = forwardRef(({ onClose, file, optionFile }, ref) => {
   const { name, value } = optionFile
@@ -45,8 +22,8 @@ const ActionMenuWrapper = forwardRef(({ onClose, file, optionFile }, ref) => {
   const { t } = useI18n()
   const { showViewerSnackbar } = useViewerSnackbar()
   const client = useClient()
-  const styles = useStyles()
 
+  const isEditableAttribute = checkEditableAttribute(name)
   const currentAppSlug = client.getInstanceOptions().app.slug
   const currentModel = getCurrentModel(name)
 
@@ -87,86 +64,25 @@ const ActionMenuWrapper = forwardRef(({ onClose, file, optionFile }, ref) => {
 
   if (isMobile) {
     return (
-      <BottomSheet backdrop onClose={onClose}>
-        <BottomSheetItem disableGutters>
-          <List>
-            {!isInDriveOrPhotosApp && !isNotEditableAttributes.includes(name) && (
-              <AppLinker app={{ slug: mespapiersAppSlug }} href={url}>
-                {({ onClick, href }) => {
-                  return (
-                    <ListItem
-                      button
-                      component="a"
-                      href={href}
-                      onClick={() => handleEdit(onClick)}
-                      disabled={!isAppLinkLoaded}
-                    >
-                      <ListItemIcon>
-                        <Icon icon={Edit} />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={t(`Viewer.panel.qualification.actions.edit`)}
-                      />
-                    </ListItem>
-                  )
-                }}
-              </AppLinker>
-            )}
-            <ListItem button onClick={handleCopy}>
-              <ListItemIcon>
-                <Icon icon={Copy} />
-              </ListItemIcon>
-              <ListItemText
-                primary={t(`Viewer.panel.qualification.actions.copyClipboard`)}
-              />
-            </ListItem>
-          </List>
-        </BottomSheetItem>
-      </BottomSheet>
+      <ActionMenuMobile
+        onClose={onClose}
+        isEditable={!isInDriveOrPhotosApp && isEditableAttribute}
+        actions={{ handleCopy, handleEdit }}
+        appLink={url}
+        appSlug={mespapiersAppSlug}
+      />
     )
   }
 
   return (
-    <ActionMenu onClose={onClose} anchorElRef={ref}>
-      {!isInDriveOrPhotosApp && !isNotEditableAttributes.includes(name) && (
-        <AppLinker app={{ slug: mespapiersAppSlug }} href={url}>
-          {({ onClick, href }) => {
-            return (
-              <a
-                href={href}
-                onClick={() => handleEdit(onClick)}
-                className="u-p-0"
-              >
-                <ActionMenuItem
-                  className={cx({
-                    [styles.disabledItem]: !isAppLinkLoaded
-                  })}
-                  left={
-                    <Icon
-                      icon={Edit}
-                      className={cx({
-                        [styles.disabledIcon]: !isAppLinkLoaded
-                      })}
-                    />
-                  }
-                >
-                  <Typography
-                    className={cx({
-                      [styles.disabledTypography]: !isAppLinkLoaded
-                    })}
-                  >
-                    {t(`Viewer.panel.qualification.actions.edit`)}
-                  </Typography>
-                </ActionMenuItem>
-              </a>
-            )
-          }}
-        </AppLinker>
-      )}
-      <ActionMenuItem onClick={handleCopy} left={<Icon icon={Copy} />}>
-        <Typography>{t(`Viewer.panel.qualification.actions.copy`)}</Typography>
-      </ActionMenuItem>
-    </ActionMenu>
+    <ActionMenuDesktop
+      ref={ref}
+      onClose={onClose}
+      isEditable={!isInDriveOrPhotosApp && isEditableAttribute}
+      actions={{ handleCopy, handleEdit }}
+      appLink={url}
+      appSlug={mespapiersAppSlug}
+    />
   )
 })
 ActionMenuWrapper.displayName = 'ActionMenuWrapper'
