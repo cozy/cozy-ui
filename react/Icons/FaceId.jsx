6571aed65dcd729a9b0a090475ac8052a6843be5
Antonin Cezard <acezard@gmail.com>
2022-09-30 07:47:37 +0200

    feat: Add FaceId icon
    Most notably used in Flagship context when unlocking app with FaceID

---
+++ react/Icons/FaceId.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/face-id.svg` to regenerate;
+import React from 'react'
+
+function SvgFaceId(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 3a1 1 0 011-1h2a1 1 0 000-2H3a3 3 0 00-3 3v2a1 1 0 002 0V3zm12 0a1 1 0 00-1-1h-2a1 1 0 110-2h2a3 3 0 013 3v2a1 1 0 11-2 0V3zM3 14a1 1 0 01-1-1v-2a1 1 0 10-2 0v2a3 3 0 003 3h2a1 1 0 100-2H3zm11-1a1 1 0 01-1 1h-2a1 1 0 100 2h2a3 3 0 003-3v-2a1 1 0 10-2 0v2zM6 6a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2zM7 6a1 1 0 012 0v2a1 1 0 01-2 0V6zm-1.386 4.21a1 1 0 10-1.228 1.58C5.364 12.55 6.63 13 8 13c1.369 0 2.636-.45 3.614-1.21a1 1 0 10-1.228-1.58C9.765 10.695 8.932 11 8 11c-.932 0-1.765-.306-2.386-.79z"
+      />
+    </svg>
+  )
+}
+
+export default SvgFaceId
