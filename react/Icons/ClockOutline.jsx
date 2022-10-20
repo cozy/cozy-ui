97e00dbfba5b6f522e249fc213ce8dc3bf3a8cf8
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-10-20 16:23:05 +0200

    feat(Icon): Add compare and clock-outline icons

---
+++ react/Icons/ClockOutline.jsx
@@ -0,0 +1,15 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/clock-outline.svg` to regenerate;
+import React from 'react'
+
+function SvgClockOutline(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        clipRule="evenodd"
+        d="M8 0a8 8 0 110 16A8 8 0 018 0zm0 2a6 6 0 100 12A6 6 0 008 2zm0 2a1 1 0 01.993.883L9 5v2.6l2 1.9a1 1 0 01-1.32 1.497l-.094-.083-2.293-2.207a1 1 0 01-.284-.576L7 8V5a1 1 0 011-1z"
+      />
+    </svg>
+  )
+}
+
+export default SvgClockOutline
