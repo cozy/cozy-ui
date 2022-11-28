bd6b7737502bf83e5369cd886cd4213835525ca3
Valentin <dinatalevalentin@gmail.com>
2022-11-28 15:51:06 +0100

    fix: Remove duplicate paths in icons
    Some icons have duplicate paths. It causes bad opacity when we use them. Removed duplicates from "folder", "gear", "share" and "trash" icons.

--- react/Icons/Trash.jsx
+++ react/Icons/Trash.jsx
@@ -1,13 +1,13 @@
-// Automatically created, please run `scripts/generate-svg-icon.sh assets/icons/ui/trash.svg` to regenerate;
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/trash.svg` to regenerate;
 import React from 'react'
 
 function SvgTrash(props) {
   return (
     <svg viewBox="0 0 16 16" {...props}>
-      <g fillRule="evenodd">
-        <path d="M4.5 1H2.007C1.45 1 1 1.448 1 2v1h14V2c0-.556-.45-1-1.007-1H11.5L11 0H5l-.5 1zM2 4h12v9.991A2.01 2.01 0 0111.991 16H4.01A2.01 2.01 0 012 13.991V4z" />
-        <path d="M4.5 1H2.007C1.45 1 1 1.448 1 2v1h14V2c0-.556-.45-1-1.007-1H11.5L11 0H5l-.5 1zM2 4h12v9.991A2.01 2.01 0 0111.991 16H4.01A2.01 2.01 0 012 13.991V4z" />
-      </g>
+      <path
+        d="M4.5 1H2.007C1.45 1 1 1.448 1 2v1h14V2a1 1 0 00-1.007-1H11.5L11 0H5l-.5 1zM2 4h12v9.991A2.01 2.01 0 0111.991 16H4.01A2.01 2.01 0 012 13.991V4z"
+        fillRule="evenodd"
+      />
     </svg>
   )
 }
