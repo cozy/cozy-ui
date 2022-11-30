f1274dd59d7abe180a2967e2c66dfb019280add4
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-30 17:29:45 +0100

    chore(Viewer): Add props description on ViewerContainer

--- react/Viewer/proptypes.js
+++ react/Viewer/proptypes.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 
 export const toolbarPropsPropType = {
-  /** Whether to show the toolbar or not. Note that the built-in close button is in the toolbar. */
+  /** Whether to show the toolbar or not. Note that the built-in close button is in the toolbar */
   showToolbar: PropTypes.bool,
   /** Whether to show close button in toolbar */
   showClose: PropTypes.bool,
+  /** React reference of the toolbar node */
   toolbarRef: PropTypes.object
 }
