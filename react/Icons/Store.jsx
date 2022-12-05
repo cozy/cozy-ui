4d4a1b88e2b53ddc39aa0585320271a91cff1109
Théo Poizat <hello@zatteo.com>
2022-12-05 17:38:43 +0100

    feat: Add FileTypeBankingAccount, Contacts, Papers and Store illustration

---
+++ react/Icons/Store.jsx
@@ -0,0 +1,23 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/illus/store.svg` to regenerate;
+import React from 'react'
+
+function SvgStore(props) {
+  return (
+    <svg viewBox="0 0 32 32" fill="none" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 19v8h10v-8h14v12h-3V20h-7v11H2V19h4zM30 2v6H2V2h28z"
+        fill="#FDD5D5"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 18.99C0 19.548.453 20 .993 20h30.014A.996.996 0 0032 18.99v-1.98a11 11 0 00-.218-1.99L30 7H2L.218 15.02C.098 15.56 0 16.442 0 17.01v1.98z"
+        fill="#F52D2D"
+      />
+    </svg>
+  )
+}
+
+export default SvgStore
