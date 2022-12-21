575de4318999531f63098af8e3eb8d50cac6c96e
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-12-21 11:45:11 +0100

    test: Add tests for `formatLocallyDistanceToNowStrict`

--- react/I18n/format.spec.jsx
+++ react/I18n/format.spec.jsx
@@ -1,4 +1,8 @@
-import { initFormat, formatLocallyDistanceToNow } from './format'
+import {
+  initFormat,
+  formatLocallyDistanceToNow,
+  formatLocallyDistanceToNowStrict
+} from './format'
 
 describe('initFormat', () => {
   beforeEach(() => {
@@ -43,5 +47,39 @@ describe('formatLocallyDistanceToNow', () => {
     const result = formatLocallyDistanceToNow(date)
 
     expect(result).toEqual('about 1 month')
+  })
+})
+
+describe('formatLocallyDistanceToNowStrict', () => {
+  it('should formatDistanceToNowStrict with small value', () => {
+    const date = Date.now() + 29 * 1000 // 29s
+
+    const result = formatLocallyDistanceToNowStrict(date)
+
+    expect(result).toEqual('29 seconds')
+  })
+
+  it('should formatDistanceToNowStrict with medium value', () => {
+    const date = Date.now() + (44 * 60 + 31) * 1000 // 44min 31s
+
+    const result = formatLocallyDistanceToNowStrict(date)
+
+    expect(result).toEqual('44 minutes')
+  })
+
+  it('should formatDistanceToNowStrict with high value', () => {
+    const date = Date.now() + (89 * 60 + 31) * 1000 // 89min 31s
+
+    const result = formatLocallyDistanceToNowStrict(date)
+
+    expect(result).toEqual('1 hour')
+  })
+
+  it('should formatDistanceToNowStrict with very high value', () => {
+    const date = Date.now() + 42 * 24 * 60 * 60 * 1000 // 42d
+
+    const result = formatLocallyDistanceToNowStrict(date)
+
+    expect(result).toEqual('1 month')
   })
 })
