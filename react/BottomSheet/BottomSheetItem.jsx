b6154e78159a06484e22cbe3750f420b1af26674
Théo Poizat <hello@zatteo.com>
2023-01-10 15:25:00 +0100

    feat(BottomSheet): Forward ref in BottomSheetItem

--- react/BottomSheet/BottomSheetItem.jsx
+++ react/BottomSheet/BottomSheetItem.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 import Paper from 'cozy-ui/transpiled/react/Paper'
 
-const BottomSheetItem = ({
-  children,
-  disableGutters,
-  disableElevation,
-  className,
-  ...props
-}) => {
-  return (
-    <Paper
-      elevation={disableElevation ? 0 : 0} // need to set correct shadow values before setting a real elevation value
-      square
-      className={cx({ 'u-p-1': !disableGutters }, className)}
-      {...props}
-    >
-      {children}
-    </Paper>
-  )
-}
+const BottomSheetItem = forwardRef(
+  (
+    { children, disableGutters, disableElevation, className, ...props },
+    ref
+  ) => {
+    return (
+      <Paper
+        ref={ref}
+        elevation={disableElevation ? 0 : 0} // need to set correct shadow values before setting a real elevation value
+        square
+        className={cx({ 'u-p-1': !disableGutters }, className)}
+        {...props}
+      >
+        {children}
+      </Paper>
+    )
+  }
+)
+
+BottomSheetItem.displayName = 'BottomSheetItem'
 
 BottomSheetItem.propTypes = {
   /** CSS classes */
