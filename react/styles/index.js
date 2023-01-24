a2fcc1cc08e3b875b824baec950645bae93f54d6
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-01-24 15:52:57 +0100

    feat: Replace imports @material-ui/core/styles -> @mui/styles

--- react/styles/index.js
+++ react/styles/index.js
@@ -1,1 +1,17 @@
-export * from '@material-ui/core/styles'
+export * from '@mui/material/styles'
+export {
+  createGenerateClassName,
+  createStyles,
+  getThemeProps,
+  jssPreset,
+  makeStyles,
+  mergeClasses,
+  ServerStyleSheets,
+  styled,
+  StylesProvider,
+  useTheme,
+  useThemeVariants,
+  propsToClassKey,
+  withStyles,
+  withTheme
+} from '@mui/styles'
