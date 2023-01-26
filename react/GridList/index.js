081ffc7bf861205ff69b07d58df7de6a8f441e1a
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-01-26 16:35:47 +0100

    feat: Add ImageList components and deprecated GridList component

--- react/GridList/index.js
+++ react/GridList/index.js
@@ -1,3 +1,12 @@
+import React, { forwardRef } from 'react'
 import ImageList from '@mui/material/ImageList'
 
-export default ImageList
+const GridList = forwardRef((props, ref) => {
+  console.warn('Deprecated, use ImageList instead')
+
+  return <ImageList ref={ref} {...props} />
+})
+
+GridList.displayName = 'GridList'
+
+export default GridList
