a55db1962de5f7140bfc94664151138ce50af51a
Théo Poizat <hello@zatteo.com>
2023-02-16 16:10:16 +0100

    feat: Remove unused component BorderedList

--- react/MuiCozyTheme/List/index.js
+++ react/MuiCozyTheme/List/index.js
@@ -1,11 +1,3 @@
 import List from '@material-ui/core/List'
 
-import { withStyles } from '../../styles'
-
-export const BorderedList = withStyles({
-  root: {
-    borderTop: '1px solid var(--silver)'
-  }
-})(List)
-
 export default List
