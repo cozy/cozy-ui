ab2806e7075a0180bf4c6153f161a3d623cbf02c
cballevre <celestin.ballevre@protonmail.com>
2023-03-08 17:38:37 +0100

    feat: Add state on useInstance
    Requests take time to respond depending on the network and the cache. Exposing `state` allows a spinner to be shown to the user to wait

--- react/helpers/useInstance.jsx
+++ react/helpers/useInstance.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from 'react'
 const useInstance = client => {
   const [instance, setInstance] = useState()
   const [context, setContext] = useState()
+  const [state, setState] = useState('idle')
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setState('loading')
         const instancePromise = client
           .getStackClient()
           .fetchJSON('GET', '/settings/instance')
@@ -24,13 +26,17 @@ const useInstance = client => {
         if (!(contextFetched instanceof Error)) {
           setContext(contextFetched)
         }
-      } catch (e) {} //eslint-disable-line
+        setState('loaded')
+      } catch (e) {
+        setState('failed')
+      }
     }
     fetchData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return {
+    state,
     instance,
     context
   }
