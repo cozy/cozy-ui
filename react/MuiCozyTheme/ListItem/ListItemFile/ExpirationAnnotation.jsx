8ae5ce7f1f9798f4d936dbdbe3e5908ee5ef7ee7
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-03-21 09:52:54 +0100

    feat(ListItem): Add ListItemFile based on ListItemBase

---
+++ react/MuiCozyTheme/ListItem/ListItemFile/ExpirationAnnotation.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { computeExpirationDate, isExpired } from 'cozy-client/dist/models/paper'
+
+import Typography from '../../../Typography'
+import { useI18n } from '../../../I18n'
+import { formatLocallyDistanceToNowStrict } from '../../../I18n/format'
+import { withListItemLocales } from '../hoc/withListItemLocales'
+
+const ExpirationAnnotation = ({ file }) => {
+  const { t } = useI18n()
+
+  if (isExpired(file)) {
+    return (
+      <Typography component="span" variant="inherit" color="error">
+        {t('ListItem.file.expired')}
+      </Typography>
+    )
+  }
+
+  const expirationDate = computeExpirationDate(file)
+
+  return (
+    <Typography component="span" variant="inherit" className="u-warning">
+      {t('ListItem.file.expiresIn', {
+        duration: formatLocallyDistanceToNowStrict(expirationDate)
+      })}
+    </Typography>
+  )
+}
+
+ExpirationAnnotation.propTypes = {
+  file: PropTypes.object.isRequired
+}
+
+export default withListItemLocales(ExpirationAnnotation)
