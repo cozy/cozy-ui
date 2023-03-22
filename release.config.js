d8f17d8315a58cc13a03a4336f97ee1b3fe286f6
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-03-22 11:11:32 +0100

    feat: Use muiv5 keyword for deployment instead of next
    to avoid version problem between next and master, because master
    must have a version lower than next version

--- release.config.js
+++ release.config.js
@@ -1,17 +1,24 @@
 module.exports = {
-  "branch": "next",
-  "tagFormat": "v${version}-next",
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/changelog", {
-      "changelogFile": "CHANGELOG.md"
-    }],
-    ["@semantic-release/git", {
-      "assets": ["CHANGELOG.md"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    "@semantic-release/npm",
-    "@semantic-release/github"
+  branches: ['next', { name: 'muiv5', prerelease: true }],
+  tagFormat: 'v${version}-muiv5',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github'
   ]
 }
