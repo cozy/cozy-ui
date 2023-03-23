9c8a48ab611af139d53add6dad1e4089ea65c95d
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-03-23 11:45:05 +0100

    fix(ActionMenuWithClose): Props wasn't propagated to ActionMenu

--- react/ActionMenu/ActionMenuWithClose.jsx
+++ react/ActionMenu/ActionMenuWithClose.jsx
@@ -10,18 +10,20 @@ import ActionMenu from '.'
 /**
  * Add onClose method on every children, to close the menu when clicking them
  */
-const ActionMenuWithClose = forwardRef(({ onClose, children }, ref) => {
-  return (
-    <ActionMenu anchorElRef={ref} onClose={onClose}>
-      {Children.map(children, child => {
-        if (isValidElement(child)) {
-          return cloneElement(child, { onClose })
-        }
-        return null
-      })}
-    </ActionMenu>
-  )
-})
+const ActionMenuWithClose = forwardRef(
+  ({ onClose, children, ...props }, ref) => {
+    return (
+      <ActionMenu anchorElRef={ref} onClose={onClose} {...props}>
+        {Children.map(children, child => {
+          if (isValidElement(child)) {
+            return cloneElement(child, { onClose })
+          }
+          return null
+        })}
+      </ActionMenu>
+    )
+  }
+)
 
 ActionMenuWithClose.displayName = 'ActionMenuWithClose'
 
