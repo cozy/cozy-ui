8c06496bb3f8976c918d3872d930bc546a3cbb13
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-04-03 13:28:15 +0200

    fix(ListItemContact): UseActions wasn't executed correctly in prod env
    action names were not retrieved once the app was built, maybe due to anonymous functions although I tried without success also with named functions

--- react/MuiCozyTheme/ListItem/ListItemContact/useActions.jsx
+++ react/MuiCozyTheme/ListItem/ListItemContact/useActions.jsx
@@ -1,29 +1,9 @@
 import { useMemo } from 'react'
 
 import { makeActions } from '../../../ActionMenu/Actions/helpers'
-import { hr } from '../../../ActionMenu/Actions/hr'
-import { emailTo } from '../../../ActionMenu/Actions/emailTo'
-import { smsTo } from '../../../ActionMenu/Actions/smsTo'
-import { call } from '../../../ActionMenu/Actions/call'
 import { modify } from '../../../ActionMenu/Actions/modify'
 import { viewInContacts } from '../../../ActionMenu/Actions/viewInContacts'
-
-const makeOptionalActions = contact => {
-  const optionalActions = [hr, call, smsTo, emailTo]
-
-  const hasPhoneAction = contact.phone?.length > 0
-  const hasEmailAction = contact.email?.length > 0
-  const hasMessageActions = hasPhoneAction || hasEmailAction
-
-  const conditions = {
-    hr: hasMessageActions,
-    call: hasPhoneAction,
-    smsTo: hasPhoneAction,
-    emailTo: hasEmailAction
-  }
-
-  return optionalActions.filter(action => conditions[action.name])
-}
+import { makeOptionalActions } from './helpers'
 
 const useActions = contact => {
   const optionalActions = useMemo(() => makeOptionalActions(contact), [contact])
