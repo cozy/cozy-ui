1d183524a8e17767920619242898f87e307c10b0
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-04-25 13:48:21 +0200

    feat(List): Update List, ListItem, ListItemText, ListSubheader, ListItemIcon

--- react/MuiCozyTheme/ListItem/index.js
+++ react/MuiCozyTheme/ListItem/index.js
@@ -1,3 +1,124 @@
-import ListItem from '@material-ui/core/ListItem'
+import React, { Fragment, forwardRef } from 'react'
+import cx from 'classnames'
+import PropTypes from 'prop-types'
+import merge from 'lodash/merge'
+import MuiListItem from '@material-ui/core/ListItem'
+import isMuiElement from '@material-ui/core/utils/isMuiElement'
+
+import { makeStyles } from '../../styles'
+
+const useStyles = makeStyles({
+  gutters: {
+    paddingLeft: ({ guttersValue }) => guttersValue.left,
+    paddingRight: ({ guttersValue }) => guttersValue.right
+  },
+  secondaryAction: {
+    paddingRight: ({ secondaryActionPaddingRight }) =>
+      secondaryActionPaddingRight
+  }
+})
+
+const getComponentName = component => {
+  if (!component) return null
+  return component.type.displayName || component.type.name || 'Component'
+}
+
+const getChildren = props => {
+  return React.Children.toArray(props.children)
+}
+
+const getChildrenCount = props => {
+  const children = getChildren(props)
+  return children.length
+}
+
+const getLastChild = props => {
+  const children = getChildren(props)
+  const lastChild = children[children.length - 1]
+
+  return lastChild.type === Fragment
+    ? getChildren(lastChild.props)[0]
+    : lastChild
+}
+
+// Return secondaryAction padding right
+const useSecondaryAction = ({ gutters, ...props }) => {
+  const BUTTON_SIZE = 48
+  const margin = 8
+  const offset = gutters === 'double' ? margin + 16 : margin
+
+  const lastChild = getLastChild(props)
+  const hasSecondaryAction =
+    (lastChild && isMuiElement(lastChild, ['ListItemSecondaryAction'])) || false
+
+  return hasSecondaryAction
+    ? getChildrenCount(lastChild.props) * BUTTON_SIZE + offset
+    : 0
+}
+
+// Return gutters size
+const useGutters = ({ gutters, ...props }) => {
+  const guttersValue = gutters === 'double' ? 32 : 16
+
+  const lastChild = getLastChild(props)
+  const isLastChildListItemIcon = getComponentName(lastChild).includes(
+    'ListItemIcon'
+  )
+
+  const gutterRight = isLastChildListItemIcon ? guttersValue - 8 : guttersValue
+
+  return { left: guttersValue, right: gutterRight }
+}
+
+// Add margin on ListItemSecondaryAction when gutters are double
+const useOverridenChildren = ({ gutters, ...props }) => {
+  const children = getChildren(props)
+  const childrenCount = getChildrenCount(props)
+
+  return React.Children.map(children, (child, index) => {
+    const isLastChild = index === childrenCount - 1
+    const isSecondaryAction =
+      (isLastChild && isMuiElement(child, ['ListItemSecondaryAction'])) || false
+
+    if (isSecondaryAction && gutters === 'double') {
+      return React.cloneElement(child, {
+        className: `${child.props.className} u-mr-1`
+      })
+    }
+    return child
+  })
+}
+
+const ListItem = forwardRef(({ className, ...props }, ref) => {
+  const secondaryActionPaddingRight = useSecondaryAction(props)
+  const guttersValue = useGutters(props)
+
+  const styles = useStyles({ secondaryActionPaddingRight, guttersValue })
+  const overridenChildren = useOverridenChildren(props)
+
+  return (
+    <MuiListItem
+      {...props}
+      ref={ref}
+      classes={merge(props.classes, styles)}
+      className={cx(className, props.size)}
+      disableGutters={props.disableGutters || props.gutters === 'disabled'}
+    >
+      {overridenChildren}
+    </MuiListItem>
+  )
+})
+
+ListItem.displayName = 'ListItem'
+
+ListItem.defaultProps = {
+  gutters: 'default',
+  size: 'medium'
+}
+
+ListItem.propTypes = {
+  gutters: PropTypes.oneOf(['disabled', 'double', 'default']),
+  size: PropTypes.oneOf(['small', 'medium', 'large'])
+}
 
 export default ListItem
