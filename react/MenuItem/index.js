607315f40572b9db82725ffcb1b1857607fc12a2
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-04-26 17:58:55 +0200

    feat(MenuItem): Use ListItem from cozy-ui and not from Mui

--- react/MenuItem/index.js
+++ react/MenuItem/index.js
@@ -1,3 +1,16 @@
+import React, { forwardRef } from 'react'
 import MuiMenuItem from '@material-ui/core/MenuItem'
 
-export default MuiMenuItem
+import ListItem, { LitItemPropTypes } from '../MuiCozyTheme/ListItem'
+
+const MenuItem = forwardRef((props, ref) => {
+  return (
+    <MuiMenuItem ref={ref} component={ListItem} gutters="disabled" {...props} />
+  )
+})
+
+MenuItem.displayName = 'MenuItem'
+
+MenuItem.propTypes = LitItemPropTypes
+
+export default MenuItem
