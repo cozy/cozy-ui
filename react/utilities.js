2c269578fc3036b10f732121015e86d8a62cb0ca
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-06-15 15:35:17 +0200

    fix(css): Avoid importing palette.styl and utils.styl in each css of
    legacy component. Prefer to import palette.styl in utils.styl to avoid
    duplication, and to use not hashed css.

--- react/utilities.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-// Special file for utilities to be included in the transpiled stylesheet
-import palette from '../stylus/utilities/palette.styl' // 130k
-import utilies from '../stylus/cozy-ui/utils.styl'
