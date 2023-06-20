b0c3711332be3b8379354db15ef816019a72ecb9
Antonin Cezard <acezard@gmail.com>
2023-06-20 09:07:40 +0200

    feat: Add "ArrowUp" UI Icon

---
+++ react/Icons/ArrowUp.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/arrow-up.svg` to regenerate;
+import React from 'react'
+
+function SvgArrowUp(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 3.414v11.58C9 15.548 8.556 16 8 16c-.552 0-1-.45-1-1.007V3.414L1.707 8.707A1 1 0 01.293 7.293l7-7a1 1 0 011.414 0l7 7a1 1 0 01-1.414 1.414L9 3.414z"
+      />
+    </svg>
+  )
+}
+
+export default SvgArrowUp
