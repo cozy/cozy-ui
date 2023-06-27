2c36bfe131e481885e165768254d135a13609904
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-06-27 16:11:37 +0200

    refactor(Paywall): Removes maxAccount non-intuitive notion

--- react/Paywall/MaxAccountsByKonnectorPaywall.jsx
+++ react/Paywall/MaxAccountsByKonnectorPaywall.jsx
@@ -2,20 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Paywall from './Paywall'
-import { useI18n } from '../I18n'
 import withPaywallLocales from './locales/withPaywallLocales'
 
 /**
  * Paywall displayed when the user reach the maximum accounts allowed for a konnector
  */
 const MaxAccountsByKonnectorPaywall = ({ max, konnectorName, onClose }) => {
-  const { t } = useI18n()
-
   return (
     <Paywall
       variant="maxAccountsByKonnector"
       contentInterpolation={{
-        maxAccount: t('maxAccount', { smart_count: max }),
+        smart_count: max,
         konnectorName
       }}
       onClose={onClose}
--- react/Paywall/MaxAccountsPaywall.jsx
+++ react/Paywall/MaxAccountsPaywall.jsx
@@ -3,20 +3,15 @@ import PropTypes from 'prop-types'
 
 import Paywall from './Paywall'
 import withPaywallLocales from './locales/withPaywallLocales'
-import { useI18n } from '../I18n'
 
 /**
  * Paywall displayed when the user reach the maximum accounts allowed for all konnectors
  */
 const MaxAccountsPaywall = ({ max, onClose }) => {
-  const { t } = useI18n()
-
   return (
     <Paywall
       variant="maxAccounts"
-      contentInterpolation={{
-        maxAccount: t('maxAccount', { smart_count: max })
-      }}
+      contentInterpolation={{ smart_count: max }}
       onClose={onClose}
     />
   )
