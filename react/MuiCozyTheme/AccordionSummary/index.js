6e95f9d00cba0ff8313ae40d3e7bb78c473fad92
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-06-28 14:37:20 +0200

    feat(AccordionExpandIcon): Moved to `./AccordionSummary` folder
    no BC here, we will make one later on the parent folder

--- react/MuiCozyTheme/AccordionSummary/index.js
+++ react/MuiCozyTheme/AccordionSummary/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 
-import AccordionExpandIcon from '../AccordionExpandIcon'
+import AccordionExpandIcon from './AccordionExpandIcon'
 
 AccordionSummary.defaultProps = {
   expandIcon: <AccordionExpandIcon />
