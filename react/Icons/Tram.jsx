b7bf028ea38b8d7fe21cb3b264f37117e3208d14
AlexisG <merkkur39@gmail.com>
2023-09-07 11:21:32 +0200

    feat: Add multiple icons
    For needs in CoachCO2 app

---
+++ react/Icons/Tram.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/tram.svg` to regenerate;
+import React from 'react'
+
+function SvgTram(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 .75A.75.75 0 014.75 0h6.5a.75.75 0 010 1.5H9.589l-.764 1.526C11.527 3.201 14 4.276 14 7v5a3 3 0 01-1.765 2.735L13 15.5v.5h-2l-1-1H6l-1 1H3v-.5l.765-.765A3 3 0 012 12V7c0-2.78 1.718-3.843 5.153-3.984L7.911 1.5H4.75A.75.75 0 014 .75zM4 10V6h8v4H4zm4 4a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+      />
+    </svg>
+  )
+}
+
+export default SvgTram
