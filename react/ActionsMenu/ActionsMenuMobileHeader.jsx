805a030b5b5f788fe0514593c12a64eb42a5498f
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-10-19 11:22:57 +0200

    fix(ActionsMenu): ActionsMenuMobileHeader was unnecessarily in DOM on desktop

--- react/ActionsMenu/ActionsMenuMobileHeader.jsx
+++ react/ActionsMenu/ActionsMenuMobileHeader.jsx
@@ -8,12 +8,8 @@ import useBreakpoints from '../providers/Breakpoints'
 const ActionsMenuMobileHeader = forwardRef(({ children, ...props }, ref) => {
   const { isMobile } = useBreakpoints()
 
-  // To make accessibility work, we need to return a displayed item.
-  // The trick is to return an empty one with no padding to simulate a display none.
-  // Otherwise it will be impossible to use keyboard navigation for example
-  // probably due to the inner workings of Mui
   if (!isMobile) {
-    return <ActionsMenuItem {...props} ref={ref} className="u-p-0" />
+    return null
   }
 
   return (
@@ -25,6 +21,8 @@ const ActionsMenuMobileHeader = forwardRef(({ children, ...props }, ref) => {
     </>
   )
 })
+
+ActionsMenuMobileHeader.displayName = 'ActionsMenuMobileHeader'
 
 ActionsMenuMobileHeader.propTypes = {
   children: PropTypes.node
