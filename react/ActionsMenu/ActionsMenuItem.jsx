888a5b86d3603d4de7baa2b38b98f434d0a64400
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-10-19 11:29:56 +0200

    feat(ActionsMenuItem): Add `cozyActionsMenuItem` class to be able to target it

--- react/ActionsMenu/ActionsMenuItem.jsx
+++ react/ActionsMenu/ActionsMenuItem.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import omit from 'lodash/omit'
+import cx from 'classnames'
 
 import MenuItem from '../MenuItem'
 import ListItem from '../ListItem'
@@ -11,16 +12,27 @@ const cleanPropsForDOM = props => {
   return omit(props, nonDOMProps)
 }
 
-const ActionsMenuItem = forwardRef(({ isListItem, ...props }, ref) => {
-  const Component = isListItem ? ListItem : MenuItem
-  const compProps = cleanPropsForDOM(props)
+const ActionsMenuItem = forwardRef(
+  ({ isListItem, className, ...props }, ref) => {
+    const Component = isListItem ? ListItem : MenuItem
+    const compProps = cleanPropsForDOM(props)
 
-  return <Component {...compProps} ref={ref} button ellipsis={false} />
-})
+    return (
+      <Component
+        {...compProps}
+        className={cx(className, { ['cozyActionsMenuItem']: !isListItem })}
+        ref={ref}
+        button
+        ellipsis={false}
+      />
+    )
+  }
+)
 
 ActionsMenuItem.displayName = 'ActionsMenuItem'
 
 ActionsMenuItem.propTypes = {
+  className: PropTypes.string,
   /** Whether the ActionsMenuItem will return a ListItem or MenuItem */
   isListItem: PropTypes.bool
 }
