f9057ed9364f83d901dd240c8881b8da03ab51f8
cballevre <celestin.ballevre@protonmail.com>
2023-11-23 09:24:53 +0100

    feat: Add TrashDuotone icon

---
+++ react/Icons/TrashDuotone.jsx
@@ -0,0 +1,21 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/illus/trash-duotone.svg` to regenerate;
+import React from 'react'
+
+function SvgTrashDuotone(props) {
+  return (
+    <svg viewBox="0 0 32 32" fill="none" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.586 3L10.707.879A3 3 0 0112.828 0h6.344a3 3 0 012.121.879L23.414 3H29a2 2 0 110 4H3a2 2 0 110-4h5.586zm3.535-.707A1 1 0 0112.828 2h6.344a1 1 0 01.707.293l.707.707h-9.172l.707-.707z"
+        fill="#95999D"
+      />
+      <path
+        d="M4 8a1 1 0 011-1h22a1 1 0 011 1v22a2 2 0 01-2 2H6a2 2 0 01-2-2V8z"
+        fill="#D6D8DA"
+      />
+    </svg>
+  )
+}
+
+export default SvgTrashDuotone
