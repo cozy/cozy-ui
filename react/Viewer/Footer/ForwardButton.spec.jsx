a35d3b11dd03d5d89a7fcea5c31d1a11c90bfcb7
Alexis G <merkkur39@gmail.com>
2023-12-01 13:52:00 +0100

    feat(ForwardButton): Add `onClick` prop
    So that apps can pass a custom function

--- react/Viewer/Footer/ForwardButton.spec.jsx
+++ react/Viewer/Footer/ForwardButton.spec.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 
+import { getSharingLink } from 'cozy-client/dist/models/sharing'
 import { isMobileApp } from 'cozy-device-helper'
-import { models } from 'cozy-client'
-const {
-  sharing: { getSharingLink }
-} = models
 
 import DemoProvider from '../docs/DemoProvider'
 import ForwardButton from './ForwardButton'
 import { exportFilesNative } from './helpers'
 
 jest.mock('cozy-device-helper')
-jest.mock('cozy-client', () => ({
-  ...jest.requireActual('cozy-client'),
-  models: {
-    sharing: {
-      getSharingLink: jest.fn()
-    }
-  }
+jest.mock('cozy-client/dist/models/sharing', () => ({
+  ...jest.requireActual('cozy-client/dist/models/sharing'),
+  getSharingLink: jest.fn()
 }))
 jest.mock('./helpers')
 
@@ -27,11 +20,11 @@ const file = {
   name: 'filename.pdf'
 }
 
-const setup = ({ isMobileApplication }) => {
+const setup = ({ isMobileApplication, onClick }) => {
   isMobileApp.mockReturnValue(isMobileApplication)
   return render(
     <DemoProvider>
-      <ForwardButton file={file} />
+      <ForwardButton file={file} onClick={onClick} />
     </DemoProvider>
   )
 }
@@ -69,6 +62,17 @@ describe('ForwardButton', () => {
       fireEvent.click(btn)
 
       expect(getSharingLink).toHaveBeenCalledTimes(0)
+    })
+
+    it('should not call it if the "onClick" prop is passed', async () => {
+      const onClick = jest.fn()
+      const { findByTestId } = setup({ isMobileApplication: true, onClick })
+
+      const btn = await findByTestId('openFileButton')
+      fireEvent.click(btn)
+
+      expect(getSharingLink).toHaveBeenCalledTimes(0)
+      expect(onClick).toHaveBeenCalledTimes(1)
     })
 
     it('should call it if is on web app', async () => {
