2c51cf672a194629b693fc322aeb88e58110976d
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-12-06 15:43:24 +0100

    fix(DeprecatedDivider): Use responsive theme instead of light theme

--- react/Divider/index.jsx
+++ react/Divider/index.jsx
@@ -2,7 +2,6 @@ import React, { forwardRef } from 'react'
 import MuiDivider from '@material-ui/core/Divider'
 
 import { withStyles } from '../styles'
-import { normalTheme } from '../MuiCozyTheme/theme'
 
 import TextDivider from './TextDivider'
 
@@ -38,20 +37,20 @@ const defaultValues = {
   }
 }
 
-export const DeprecatedDivider = withStyles({
+export const DeprecatedDivider = withStyles(theme => ({
   /**
    * calcs are made since we have defaultMargin on the Dialog so
    * we need to remove the left margin and add the width of 2 margins
    * in order to have the divider takes the full width of the Modal
    */
   root: {
-    [normalTheme.breakpoints.down('md')]: {
+    [theme.breakpoints.down('md')]: {
       width: `calc(100% + ${defaultValues.dialog.sm.padding}*2px)`,
       marginLeft: `-${defaultValues.dialog.sm.padding}px`
     },
-    [normalTheme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('md')]: {
       width: `calc(100% + ${defaultValues.dialog.md.padding}*2px)`,
       marginLeft: `-${defaultValues.dialog.md.padding}px`
     }
   }
-})(Divider)
+}))(Divider)
