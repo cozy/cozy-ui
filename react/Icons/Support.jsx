95eb861956b7a4eeb1aa9c4f20be6ccc4c7eb351
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-12-18 13:45:43 +0100

    feat(Icon): Add support icon

---
+++ react/Icons/Support.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/support.svg` to regenerate;
+import React from 'react'
+
+function SvgSupport(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5.723 13.553l.899-1.797a4.012 4.012 0 01-2.378-2.378l-1.797.899a6.02 6.02 0 003.276 3.276zm.899-9.31a4.012 4.012 0 00-2.378 2.379l-1.797-.899a6.02 6.02 0 013.276-3.276l.899 1.797zm6.931 6.034a6.021 6.021 0 01-3.277 3.276l-.898-1.797a4.012 4.012 0 002.378-2.378l1.797.899zm0-4.554a6.02 6.02 0 00-3.277-3.276l-.898 1.797a4.012 4.012 0 012.378 2.378l1.797-.899zM16 8A8 8 0 110 8a8 8 0 0116 0zm-6 0a2 2 0 11-4 0 2 2 0 014 0z"
+      />
+    </svg>
+  )
+}
+
+export default SvgSupport
