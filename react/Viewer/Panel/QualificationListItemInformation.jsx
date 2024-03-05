1d106ef9b4f97c7cd0cd78e34a4ac5e58d737b75
Théo Poizat <hello@zatteo.com>
2024-03-05 17:52:12 +0100

    feat(Viewer): Use information metadata qualification formatting from cozy-client

--- react/Viewer/Panel/QualificationListItemInformation.jsx
+++ react/Viewer/Panel/QualificationListItemInformation.jsx
@@ -1,7 +1,10 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 
-import { getBoundT } from 'cozy-client/dist/models/document/locales'
+import {
+  getTranslatedNameForInformationMetadata,
+  formatInformationMetadataValue
+} from 'cozy-client/dist/models/paper'
 
 import ListItem from '../../ListItem'
 import ListItemSecondaryAction from '../../ListItemSecondaryAction'
@@ -12,62 +15,30 @@ import QualificationListItemText from './QualificationListItemText'
 import { useI18n } from '../../providers/I18n'
 import MidEllipsis from '../../MidEllipsis'
 
-export const makeInformationValue = ({
-  name,
-  value,
-  qualificationLabel,
-  t,
-  scannerT
-}) => {
-  if (typeof value !== 'number' && !value) {
-    return t('Viewer.panel.qualification.noInfo')
-  }
-
-  if (name === 'noticePeriod') {
-    return `${value} ${t('Viewer.panel.qualification.information.day', {
-      smart_count: value
-    })}`
-  }
-  if (name === 'contractType') {
-    return scannerT(`Scan.attributes.contractType.${value}`, { _: value })
-  }
-  if (
-    name === 'refTaxIncome' ||
-    name === 'netSocialAmount' ||
-    (name === 'number' && qualificationLabel === 'pay_sheet')
-  ) {
-    return `${value} €`
-  }
-
-  return <MidEllipsis text={`${value}`} />
-}
-
 const QualificationListItemInformation = forwardRef(
   ({ formatedMetadataQualification, file, toggleActionsMenu }, ref) => {
-    const { t, lang } = useI18n()
-    const scannerT = getBoundT(lang)
+    const { lang } = useI18n()
     const { name, value } = formatedMetadataQualification
     const qualificationLabel = file.metadata.qualification.label
 
-    const currentValue = makeInformationValue({
-      name,
-      value,
-      qualificationLabel,
-      t,
-      scannerT
+    const formattedTitle = getTranslatedNameForInformationMetadata(name, {
+      lang,
+      qualificationLabel
     })
-    const title =
-      name === 'number'
-        ? t(
-            `Viewer.panel.qualification.information.title.${qualificationLabel}.${name}`
-          )
-        : t(`Viewer.panel.qualification.information.title.${name}`)
+    const formattedValue = formatInformationMetadataValue(value, {
+      lang,
+      name,
+      qualificationLabel
+    })
+
+    const titleComponent =
+      formattedTitle === name ? <MidEllipsis text={name} /> : formattedTitle
 
     return (
       <ListItem className={'u-pl-2 u-pr-3'}>
         <QualificationListItemText
-          primary={title}
-          secondary={currentValue}
+          primary={titleComponent}
+          secondary={formattedValue}
           disabled={!value}
         />
         <ListItemSecondaryAction>
--- react/Viewer/Panel/QualificationListItemInformation.spec.jsx
+++ react/Viewer/Panel/QualificationListItemInformation.spec.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { fireEvent, render } from '@testing-library/react'
 
-import QualificationListItemInformation, {
-  makeInformationValue
-} from './QualificationListItemInformation'
-import MidEllipsis from '../../MidEllipsis'
+import QualificationListItemInformation from './QualificationListItemInformation'
 
 jest.mock('../../providers/I18n', () => ({
   useI18n: jest.fn(() => ({ t: x => x }))
@@ -31,16 +28,16 @@ describe('QualificationListItemInformation', () => {
       const formatedMetadataQualification = { name: 'country', value: '' }
       const { getByText } = setup({ formatedMetadataQualification })
 
-      expect(getByText('Viewer.panel.qualification.noInfo'))
+      expect(getByText('No information'))
     })
-    it('should display current value if it is truthy', () => {
+    it.only('should display current value if it is truthy', () => {
       const formatedMetadataQualification = { name: 'country', value: 'Italie' }
       const { queryByText } = setup({
         formatedMetadataQualification
       })
 
-      expect(queryByText('Viewer.panel.qualification.noInfo')).toBeNull()
-      expect(queryByText('Ita')).toBeInTheDocument()
+      expect(queryByText('No information')).toBeNull()
+      expect(queryByText('Italie')).toBeInTheDocument()
     })
     it('should display current value if it number type', () => {
       const formatedMetadataQualification = { name: 'country', value: 0 }
@@ -48,7 +45,7 @@ describe('QualificationListItemInformation', () => {
         formatedMetadataQualification
       })
 
-      expect(queryByText('Viewer.panel.qualification.noInfo')).toBeNull()
+      expect(queryByText('No information')).toBeNull()
       expect(queryByText('0')).toBeInTheDocument()
     })
   })
@@ -64,56 +61,6 @@ describe('QualificationListItemInformation', () => {
       fireEvent.click(toggleActionsMenuBtn)
 
       expect(toggleActionsMenu).toBeCalledWith('Italie')
-    })
-  })
-  describe('makeInformationValue', () => {
-    let mockT
-    beforeEach(() => {
-      mockT = jest.fn(key => key)
-    })
-    afterEach(() => {
-      mockT = jest.fn(key => key)
-    })
-
-    it('should return "MidEllipsis" component with the value', () => {
-      const res = makeInformationValue({
-        name: 'metadataName',
-        value: 'metadataValue',
-        t: mockT,
-        scannerT: mockT
-      })
-
-      expect(res).toEqual(<MidEllipsis text="metadataValue" />)
-    })
-    it('should return value with suffix locale', () => {
-      const res = makeInformationValue({
-        name: 'noticePeriod',
-        value: '88',
-        t: mockT,
-        scannerT: mockT
-      })
-
-      expect(res).toEqual('88 Viewer.panel.qualification.information.day')
-    })
-    it('should return "noInfo" value', () => {
-      const res = makeInformationValue({
-        name: 'metadataName',
-        value: '',
-        t: mockT,
-        scannerT: mockT
-      })
-
-      expect(res).toEqual('Viewer.panel.qualification.noInfo')
-    })
-    it('should return noInfo value with "noticePeriod" metadata name', () => {
-      const res = makeInformationValue({
-        name: 'noticePeriod',
-        value: '',
-        t: mockT,
-        scannerT: mockT
-      })
-
-      expect(res).toEqual('Viewer.panel.qualification.noInfo')
     })
   })
 })
