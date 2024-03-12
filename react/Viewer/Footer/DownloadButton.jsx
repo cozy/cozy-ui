594b79a6b3bb089d303aa744c3c1cbc21de83922
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-03-12 14:06:12 +0100

    feat(Viewer): Add variants on Download/Forward/Sharing buttons
    to be able to choose returned style

--- react/Viewer/Footer/DownloadButton.jsx
+++ react/Viewer/Footer/DownloadButton.jsx
@@ -3,16 +3,21 @@ import PropTypes from 'prop-types'
 
 import { useClient } from 'cozy-client'
 
+import { useI18n } from '../../providers/I18n'
 import Icon from '../../Icon'
+import IconButton from '../../IconButton'
 import DownloadIcon from '../../Icons/Download'
 import Button from '../../Buttons'
 import Alerter from '../../deprecated/Alerter'
-import { withViewerLocales } from '../hoc/withViewerLocales'
 
-const DownloadButton = ({ file, t }) => {
+const DownloadButton = ({ file, variant }) => {
   const client = useClient()
+  const { t } = useI18n()
 
-  const handleClick = async file => {
+  const icon = <Icon icon={DownloadIcon} />
+  const label = t('Viewer.download')
+
+  const handleClick = async () => {
     try {
       await client.collection('io.cozy.files').download(file)
     } catch (error) {
@@ -20,19 +25,43 @@ const DownloadButton = ({ file, t }) => {
     }
   }
 
+  if (variant === 'iconButton') {
+    return (
+      <IconButton className="u-white" aria-label={label} onClick={handleClick}>
+        {icon}
+      </IconButton>
+    )
+  }
+
+  if (variant === 'buttonIcon') {
+    return (
+      <Button
+        variant="secondary"
+        label={icon}
+        aria-label={label}
+        onClick={handleClick}
+      />
+    )
+  }
+
   return (
     <Button
       fullWidth
       variant="secondary"
-      startIcon={<Icon icon={DownloadIcon} />}
-      label={t('Viewer.download')}
-      onClick={() => handleClick(file)}
+      startIcon={icon}
+      label={label}
+      onClick={handleClick}
     />
   )
 }
 
 DownloadButton.propTypes = {
-  file: PropTypes.object
+  file: PropTypes.object,
+  variant: PropTypes.oneOf(['default', 'iconButton', 'buttonIcon'])
 }
 
-export default withViewerLocales(DownloadButton)
+DownloadButton.defaultProptypes = {
+  variant: 'default'
+}
+
+export default DownloadButton
