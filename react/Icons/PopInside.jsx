69cd1316b502db4b0ea417a9dfeb2e78585418aa
Théo Poizat <hello@zatteo.com>
2024-03-12 18:46:27 +0100

    feat: Add PopInside icon

---
+++ react/Icons/PopInside.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/pop-inside.svg` to regenerate;
+import React from 'react'
+
+function SvgPopInside(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14 0a2 2 0 012 2v8a2 2 0 01-2 2V2H4a2 2 0 012-2h8zm-2 10v4a2 2 0 01-2 2H2a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v4zM4 6a1 1 0 000 2h2.586l-4.293 4.293a1 1 0 101.414 1.414L8 9.414V12a1 1 0 102 0V7a1 1 0 00-1-1H4z"
+      />
+    </svg>
+  )
+}
+
+export default SvgPopInside
