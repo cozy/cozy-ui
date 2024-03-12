586ed283e424d9483e05ca0dd62bd52ebaa6df09
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-03-12 16:11:23 +0100

    feat(Viewer): Remove HOC for locals on no viewer DownloadButton

--- react/Viewer/NoViewer/DownloadButton.jsx
+++ react/Viewer/NoViewer/DownloadButton.jsx
@@ -1,30 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import flow from 'lodash/flow'
 
 import { withClient } from 'cozy-client'
 
+import { useI18n } from '../../providers/I18n'
 import { FileDoctype } from '../../proptypes'
 import Button from '../../deprecated/Button'
 
-import { withViewerLocales } from '../hoc/withViewerLocales'
 import { downloadFile } from '../helpers'
 
-const DownloadButton = ({ t, client, file, url }) => (
-  <Button
-    onClick={() => downloadFile({ client, file, url })}
-    label={t('Viewer.download')}
-  />
-)
+const DownloadButton = ({ client, file, url }) => {
+  const { t } = useI18n()
+
+  return (
+    <Button
+      onClick={() => downloadFile({ client, file, url })}
+      label={t('Viewer.download')}
+    />
+  )
+}
 
 DownloadButton.propTypes = {
-  t: PropTypes.func.isRequired,
   client: PropTypes.object.isRequired,
   file: FileDoctype,
   url: PropTypes.string
 }
 
-export default flow(
-  withClient,
-  withViewerLocales
-)(DownloadButton)
+export default withClient(DownloadButton)
