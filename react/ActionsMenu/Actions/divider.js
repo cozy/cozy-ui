d9f188674cdcbed52680f8c92d402ed9bc2b54a7
Alexis G <merkkur39@gmail.com>
2024-03-28 15:13:44 +0100

    feat(Actions): The divider action should not appear in the SelectionBar

--- react/ActionsMenu/Actions/divider.js
+++ react/ActionsMenu/Actions/divider.js
@@ -5,6 +5,7 @@ import Divider from '../../Divider'
 export const divider = () => {
   return {
     name: 'divider',
+    displayInSelectionBar: false,
     Component: forwardRef((props, ref) => {
       return <Divider className="u-mv-half" ref={ref} />
     })
