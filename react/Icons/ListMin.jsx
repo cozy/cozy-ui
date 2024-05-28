0418a8f44c54ba8c435831de49ffed45020270ef
Antonin Cezard <acezard@gmail.com>
2024-05-28 15:52:50 +0200

    feat: Add new icons ListMin and MosaicMin

---
+++ react/Icons/ListMin.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/list-min.svg` to regenerate;
+import React from 'react'
+
+function SvgListMin(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3 11a1 1 0 110 2 1 1 0 010-2zm10 0a1 1 0 110 2H7a1 1 0 110-2h6zM3 7a1 1 0 110 2 1 1 0 010-2zm10 0a1 1 0 110 2H7a1 1 0 010-2h6zM3 3a1 1 0 110 2 1 1 0 010-2zm10 0a1 1 0 110 2H7a1 1 0 010-2h6z"
+      />
+    </svg>
+  )
+}
+
+export default SvgListMin
