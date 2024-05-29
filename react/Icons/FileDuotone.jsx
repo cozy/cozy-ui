76fe3d5bd4f0004ea8bf8031e8c44f42be7130a8
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-05-29 11:19:55 +0200

    fix(Icon): FileDuotone icon is now theme responsive

--- react/Icons/FileDuotone.jsx
+++ react/Icons/FileDuotone.jsx
@@ -1,18 +1,10 @@
-// Automatically created, please run `scripts/generate-svg-icon.sh assets/icons/ui/file-duotone.svg` to regenerate;
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/file-duotone.svg` to regenerate;
 import React from 'react'
 
 function SvgFileDuotone(props) {
   return (
     <svg viewBox="0 0 26 32" {...props}>
       <g fillRule="evenodd">
-        <path
-          d="M0 2.002C0 .896.89 0 1.997 0H19l7 7v22.996A2 2 0 0124.003 32H1.997A1.995 1.995 0 010 29.998C.048 16 0 16 0 2.002z"
-          fill="#FFF"
-        />
-        <path
-          d="M18.5 0c-.276 0-.5.23-.5.5V8h7.5c.276 0 .5-.232.5-.5V7l-7-7h-.5z"
-          fill="#FFF"
-        />
         <path
           d="M0 2.002C0 .896.89 0 1.997 0H19l7 7v22.996A2 2 0 0124.003 32H1.997A1.995 1.995 0 010 29.998C.048 16 0 16 0 2.002z"
           opacity={0.242}
