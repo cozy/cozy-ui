0934f5c1d8743f503d8f8b9bd12424d7804d761c
Paul Tran-Van <paul.tranvan@gmail.com>
2024-06-04 18:25:34 +0200

    feat: Improve contacts list performances
    When dealing with thousands of contacts, the default 100 pagination will
    make dozens of requests, thus making it quite slow.

--- react/ContactsListModal/queries.js
+++ react/ContactsListModal/queries.js
@@ -4,7 +4,7 @@ import { CONTACTS_DOCTYPE } from 'cozy-client/dist/models/contact'
 const defaultFetchPolicy = fetchPolicies.olderThan(30 * 1000)
 
 export const buildContactsQuery = () => ({
-  definition: () => Q(CONTACTS_DOCTYPE),
+  definition: () => Q(CONTACTS_DOCTYPE).limitBy(1000),
   options: {
     as: `${CONTACTS_DOCTYPE}`,
     fetchPolicy: defaultFetchPolicy
