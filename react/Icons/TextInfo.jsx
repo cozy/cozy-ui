25488eb135f72bc4ba1a0aad3a162354195b449d
AlexisG <merkkur39@gmail.com>
2024-07-03 16:36:08 +0200

    feat: Add TextInfo icon

---
+++ react/Icons/TextInfo.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/text-info.svg` to regenerate;
+import React from 'react'
+
+function SvgTextInfo(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 3h7.172l2-2H2a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2V6.828l-2 2V14H2V3zm3 7v2h2l7.146-7.146-2-2L5 10zm10.5-6.5l-.646.646-2-2L13.5 1.5a1.414 1.414 0 112 2z"
+      />
+    </svg>
+  )
+}
+
+export default SvgTextInfo
