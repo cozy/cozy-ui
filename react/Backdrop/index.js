db5e93c0941f990fb27cff49368465bc52ee816b
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-07-04 15:05:07 +0200

    feat(Backdrop): Add `isOver` props

--- react/Backdrop/index.js
+++ react/Backdrop/index.js
@@ -1,3 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import MuiBackdrop from '@material-ui/core/Backdrop'
+import cx from 'classnames'
 
-export default MuiBackdrop
+import { makeStyles } from '../styles'
+
+const useStyles = makeStyles({
+  isOver: {
+    zIndex: 'calc(var(--zIndex-modal-toolbar) + 1)' // to be over a modal if used inside it
+  }
+})
+
+const Backdrop = ({ isOver, ...props }) => {
+  const styles = useStyles()
+
+  return (
+    <MuiBackdrop
+      {...props}
+      className={cx({ [styles.isOver]: isOver }, props.className)}
+    />
+  )
+}
+
+Backdrop.propTypes = {
+  /** Whether the Backdrop is over modal */
+  isOver: PropTypes.bool
+}
+
+export default Backdrop
