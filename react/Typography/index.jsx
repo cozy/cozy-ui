94c9ae6d5708d58b6d1d86042824e219827ce399
cballevre <celestin.ballevre@protonmail.com>
2024-08-22 09:07:57 +0200

    feat(Typography): Add types

--- react/Typography/index.jsx
+++ react/Typography/index.jsx
@@ -1,6 +1,16 @@
 import MuiTypography from '@material-ui/core/Typography'
 import React, { forwardRef } from 'react'
 
+/**
+ * @typedef TypographyPropTypes
+ * @property {string} [color] - The color of the text.
+ * @property {string} [variant] - The variant of the text.
+ * @property {React.ReactNode} children - The content of the component.
+ */
+
+/**
+ * @type React.ForwardRefRenderFunction<HTMLDivElement, TypographyPropTypes & MuiTypography>
+ */
 const Typography = forwardRef(({ color, variant, children, ...props }, ref) => {
   const madeColor =
     color || (variant === 'caption' ? 'textSecondary' : 'textPrimary')
