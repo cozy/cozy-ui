b58109d9fb58c4a28adbfecef79494cd0f1c413b
cballevre <celestin.ballevre@protonmail.com>
2024-09-17 16:49:16 +0200

    refactor(I18n): Extract useExtendI18n into its own file

--- react/providers/I18n/translation.jsx
+++ react/providers/I18n/translation.jsx
@@ -1,6 +1,6 @@
 import Polyglot from 'node-polyglot'
 
-import { DEFAULT_LANG, useI18n } from '.'
+import { DEFAULT_LANG } from '.'
 
 export let _polyglot
 
@@ -40,34 +40,3 @@ export const initTranslation = (
 }
 
 export const extend = (dict, polyglot) => (polyglot || _polyglot)?.extend(dict)
-
-// Use to determine if we need to merge locales again, and to avoid useless calls
-let useExtendI18nLang = ''
-
-/**
- * Hook to merge app locales with cozy-ui locales
- * @param {object} locales - Locales sorted by lang `{ fr: {...}, en: {...} }`
- * @returns {void}
- */
-export const useExtendI18n = locales => {
-  const { lang, polyglot } = useI18n()
-
-  if (!locales || !lang || !polyglot) return
-
-  // To simplify code we use Polyglot.extend to merge
-  // locales from object and from polyglot.phrases
-  // rather than native JS or lodash. this is why we have two extend.
-  if (useExtendI18nLang !== lang) {
-    const _polyglot = new Polyglot({
-      phrases: locales[lang],
-      locale: lang
-    })
-
-    // merge locales from app and cozy-ui, without replacing existing one in app
-    extend(polyglot.phrases, _polyglot)
-    // use merged locales in app
-    extend(_polyglot.phrases, polyglot)
-    // set the sitch to avoid useless merge
-    useExtendI18nLang = lang
-  }
-}
