d76e2dea720af433f30073deefbaa1d8575b1d4c
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-10-09 15:27:02 +0200

    feat(Icon): Add `stop`

---
+++ react/Icons/Stop.jsx
@@ -0,0 +1,12 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/stop.svg` to regenerate;
+import React from 'react'
+
+function SvgStop(props) {
+  return (
+    <svg viewBox="0 0 12 12" {...props}>
+      <path d="M.75 1.5A.75.75 0 011.5.75h9a.75.75 0 01.75.75v9a.75.75 0 01-.75.75h-9a.75.75 0 01-.75-.75v-9z" />
+    </svg>
+  )
+}
+
+export default SvgStop
