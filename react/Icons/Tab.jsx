4431b2228c51a4bcb1173afbc454ff6e48b7a654
Théo Poizat <hello@zatteo.com>
2024-10-17 14:30:01 +0200

    feat: Add tab icon

---
+++ react/Icons/Tab.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/tab.svg` to regenerate;
+import React from 'react'
+
+function SvgTab(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13 12v1a2 2 0 01-2 2H2a2 2 0 01-2-2V6a2 2 0 012-2h1V3a2 2 0 012-2h9a2 2 0 012 2v7a2 2 0 01-2 2h-1zm1-9H5v1h6a2 2 0 012 2v4h1V3zM2 6h9v7H2V6z"
+      />
+    </svg>
+  )
+}
+
+export default SvgTab
