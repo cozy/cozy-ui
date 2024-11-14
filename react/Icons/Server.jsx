88b8161a1dac12d659d18306eb53c2485ab35219
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-11-14 14:49:16 +0100

    feat: Add server icon

---
+++ react/Icons/Server.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/server.svg` to regenerate;
+import React from 'react'
+
+function SvgServer(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 2a2 2 0 012-2h12a2 2 0 012 2v3a2 2 0 01-2 2H2a2 2 0 01-2-2V2zm5 1.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM0 11a2 2 0 012-2h12a2 2 0 012 2v3a2 2 0 01-2 2H2a2 2 0 01-2-2v-3zm5 1.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
+      />
+    </svg>
+  )
+}
+
+export default SvgServer
