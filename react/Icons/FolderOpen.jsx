0a568950d411008ec36fea4f155c3b2b8c7ed100
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-11-25 12:11:50 +0100

    feat(Icon): Add folderOpen

---
+++ react/Icons/FolderOpen.jsx
@@ -0,0 +1,12 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/folder-open.svg` to regenerate;
+import React from 'react'
+
+function SvgFolderOpen(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path d="M0 13V3a2 2 0 012-2h3.172a2 2 0 011.414.586l.828.828A2 2 0 008.828 3H12a2 2 0 012 2H3.5a.5.5 0 00-.474.342l-2 6a.5.5 0 10.948.316L3.86 6h11.142a1 1 0 01.952 1.304l-2.01 6.304A2 2 0 0112.039 15H2a2 2 0 01-2-2z" />
+    </svg>
+  )
+}
+
+export default SvgFolderOpen
