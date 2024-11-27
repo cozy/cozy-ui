ea64239cbbd99ff3d5c4099247d93c12a841c9b5
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-11-27 10:43:28 +0100

    feat(Icon): Add Text

---
+++ react/Icons/Text.jsx
@@ -0,0 +1,16 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/text.svg` to regenerate;
+import React from 'react'
+
+function SvgText(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 13a1 1 0 110 2H1a1 1 0 110-2h8zm6-4a1 1 0 110 2H1a1 1 0 110-2h14zm0-4a1 1 0 110 2H1a1 1 0 010-2h14zm0-4a1 1 0 110 2H1a1 1 0 010-2h14z"
+      />
+    </svg>
+  )
+}
+
+export default SvgText
