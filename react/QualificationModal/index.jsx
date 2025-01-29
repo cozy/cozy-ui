ba9a2824d30882f5b74e5093c54ec8510ff5af5e
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-01-29 10:44:15 +0100

    refactor(QualificationModal): Move makeOptions in helpers
    no code change

--- react/QualificationModal/index.jsx
+++ react/QualificationModal/index.jsx
@@ -2,46 +2,12 @@ import PropTypes from 'prop-types'
 import React, { useMemo } from 'react'
 
 import { useClient } from 'cozy-client'
-import { themesList } from 'cozy-client/dist/models/document/documentTypeData'
-import { isQualificationNote } from 'cozy-client/dist/models/document/documentTypeDataHelpers'
-import { getBoundT } from 'cozy-client/dist/models/document/locales'
 import { isSupportedQualification } from 'cozy-client/dist/models/document/qualification'
 
+import { makeOptions } from './helpers'
 import { locales } from './locales'
-import Icon from '../Icon'
-import FileTypeNoteIcon from '../Icons/FileTypeNote'
 import NestedSelectResponsive from '../NestedSelect/NestedSelectResponsive'
-import QualificationIconStack from '../QualificationIconStack'
 import { useI18n, useExtendI18n } from '../providers/I18n'
-
-const makeOptions = lang => {
-  const qualifT = getBoundT(lang)
-
-  return {
-    children: [
-      {
-        id: 'none',
-        title: qualifT('Scan.themes.none'),
-        icon: <QualificationIconStack />
-      },
-      ...themesList.map(theme => ({
-        id: theme.id,
-        title: qualifT(`Scan.themes.${theme.label}`),
-        icon: <QualificationIconStack theme={theme.label} />,
-        children: theme.items.map(item => ({
-          id: item.label,
-          item,
-          title: qualifT(`Scan.items.${item.label}`),
-          icon: isQualificationNote(item) ? (
-            <Icon icon={FileTypeNoteIcon} size={64} />
-          ) : (
-            <QualificationIconStack qualification={item.label} />
-          )
-        }))
-      }))
-    ]
-  }
-}
 
 const QualificationModal = ({ file, title, onClose }) => {
   useExtendI18n(locales)
