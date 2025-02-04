4985e1a26d81dc6870ec9a22583b3456132ced4a
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-02-04 15:22:35 +0100

    fix(QualificationModal): Now show initial list if search value is empty

--- react/QualificationModal/helpers.spec.js
+++ react/QualificationModal/helpers.spec.js
@@ -29,4 +29,33 @@ describe('searchOptionsFn', () => {
 
     expect(res).toStrictEqual([{ id: '01', title: 'Identity Photo' }])
   })
+
+  it('should return the initial list and not all children', () => {
+    const options = {
+      children: [
+        { id: 'none', title: 'None' },
+        {
+          id: 'theme1',
+          title: 'Identity',
+          children: [
+            { id: '01', title: 'Identity Photo' },
+            { id: '02', title: 'ID card' }
+          ]
+        },
+        {
+          id: 'theme2',
+          title: 'Family',
+          children: [
+            { id: '03', title: 'Family record book' },
+            { id: '04', title: 'Birth certificate' },
+            { id: '01', title: 'Identity Photo' }
+          ]
+        }
+      ]
+    }
+
+    const res = searchOptionsFn(options, '')
+
+    expect(res).toStrictEqual(options.children)
+  })
 })
