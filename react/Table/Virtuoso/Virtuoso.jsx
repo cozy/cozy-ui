f819531465a7be447b80093ac3532f7cc8358036
AlexisG <merkkur39@gmail.com>
2025-02-17 16:55:33 +0100

    feat(VirtualizedTable): Add selection props
    To manage the selection context by app

--- react/Table/Virtuoso/Virtuoso.jsx
+++ react/Table/Virtuoso/Virtuoso.jsx
@@ -1,90 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, forwardRef } from 'react'
 import { TableVirtuoso } from 'react-virtuoso'
 
 import FixedHeaderContent from './FixedHeaderContent'
 import RowContent from './RowContent'
-import VirtuosoComponents from './VirtuosoComponents'
 import { stableSort, getComparator } from './helpers'
+import virtuosoComponents from './virtuosoComponents'
 
-const VirtualizedTable = ({
-  rows,
-  columns,
-  defaultOrder,
-  secondarySort,
-  componentsProps
-}) => {
-  const [order, setOrder] = useState('asc')
-  const [orderBy, setOrderBy] = useState(defaultOrder)
-  const [selected, setSelected] = useState([])
+const VirtualizedTable = forwardRef(
+  (
+    {
+      rows,
+      columns,
+      defaultOrder,
+      secondarySort,
+      selectedItems = [],
+      onSelect = () => {},
+      onSelectAll = () => {},
+      isSelectedItem = () => {},
+      componentsProps,
+      ...props
+    },
+    ref
+  ) => {
+    const [order, setOrder] = useState('asc')
+    const [orderBy, setOrderBy] = useState(defaultOrder)
 
-  const sortedData = stableSort(rows, getComparator(order, orderBy))
-  const data = secondarySort ? secondarySort(sortedData) : sortedData
+    const sortedData = stableSort(rows, getComparator(order, orderBy))
+    const data = secondarySort ? secondarySort(sortedData) : sortedData
 
-  const handleSort = property => {
-    const isAsc = orderBy === property && order === 'asc'
-    setOrder(isAsc ? 'desc' : 'asc')
-    setOrderBy(property)
-  }
-
-  const handleSelectAll = event => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map(n => n.name)
-      setSelected(newSelecteds)
-      return
-    }
-    setSelected([])
-  }
-
-  const handleSelect = name => {
-    const selectedIndex = selected.indexOf(name)
-    let newSelected = []
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name)
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1))
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1))
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      )
+    const handleSort = property => {
+      const isAsc = orderBy === property && order === 'asc'
+      setOrder(isAsc ? 'desc' : 'asc')
+      setOrderBy(property)
     }
 
-    setSelected(newSelected)
+    const handleSelectAll = event => {
+      if (event.target.checked) {
+        onSelectAll(rows)
+        return
+      }
+      onSelectAll([])
+    }
+
+    return (
+      <TableVirtuoso
+        {...props}
+        ref={ref}
+        data={data}
+        context={{ isSelectedItem }}
+        components={virtuosoComponents}
+        fixedHeaderContent={() => (
+          <FixedHeaderContent
+            columns={columns}
+            rowCount={rows.length}
+            selectedCount={selectedItems.length}
+            order={order}
+            orderBy={orderBy}
+            onClick={handleSort}
+            onSelectAllClick={handleSelectAll}
+          />
+        )}
+        itemContent={(_index, row) => (
+          <RowContent
+            index={_index}
+            row={row}
+            columns={columns}
+            isSelectedItem={isSelectedItem}
+            onSelectClick={onSelect}
+          >
+            {componentsProps?.rowContent?.children}
+          </RowContent>
+        )}
+        rowSpan={2}
+        // overscan={{ main: 500, reverse: 500 }}
+        // increaseViewportBy={500}
+      />
+    )
   }
+)
 
-  return (
-    <TableVirtuoso
-      data={data}
-      components={VirtuosoComponents}
-      fixedHeaderContent={() => (
-        <FixedHeaderContent
-          columns={columns}
-          rowCount={rows.length}
-          selectedCount={selected.length}
-          order={order}
-          orderBy={orderBy}
-          onClick={handleSort}
-          onSelectAllClick={handleSelectAll}
-        />
-      )}
-      itemContent={(_index, row) => (
-        <RowContent
-          index={_index}
-          row={row}
-          columns={columns}
-          selected={selected}
-          onSelectClick={handleSelect}
-        >
-          {componentsProps?.rowContent?.children}
-        </RowContent>
-      )}
-      rowSpan={2}
-      overscan={{ main: 5, reverse: 5 }}
-    />
-  )
-}
-
-export default VirtualizedTable
+export default React.memo(VirtualizedTable)
