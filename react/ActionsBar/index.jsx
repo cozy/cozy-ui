ada54038dcc7e0bd07e7d0794ec559ca46c67f00
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-04 16:56:48 +0100

    feat: Adjust ActionsBar

--- react/ActionsBar/index.jsx
+++ react/ActionsBar/index.jsx
@@ -11,20 +11,31 @@ import AppBar from '../AppBar'
 import Box from '../Box'
 import Icon from '../Icon'
 import IconButton from '../IconButton'
+import CheckCircleIcon from '../Icons/CheckCircle'
 import CrossIcon from '../Icons/Cross'
 import CrossCircleIcon from '../Icons/CrossCircle'
 import DotsIcon from '../Icons/Dots'
 import Toolbar from '../Toolbar'
+import Typography from '../Typography'
+import { isTwakeTheme } from '../helpers/isTwakeTheme'
 import useBreakpoints from '../providers/Breakpoints'
+import CozyTheme from '../providers/CozyTheme'
 import { useI18n } from '../providers/I18n'
 import { makeStyles } from '../styles'
 
 const useStyles = makeStyles({
   appBar: ({ isMobile }) => ({
+    width: isTwakeTheme() && !isMobile ? 'calc(100% - 44px)' : '100%',
+    right: isTwakeTheme() && !isMobile ? '22px' : '0',
     height: isMobile ? '4rem' : '3rem',
     top: isMobile ? 'auto' : 0,
     bottom: isMobile ? 0 : undefined,
     zIndex: 'var(--zIndex-selection)',
+    borderRadius: isTwakeTheme()
+      ? isMobile
+        ? '16px 16px 0 0'
+        : '0 0 16px 16px'
+      : 'initial',
     boxShadow: isMobile ? 'var(--shadow8)' : 'var(--shadow1)'
   }),
   toolbar: {
@@ -54,14 +65,68 @@ const useStyles = makeStyles({
     position: 'absolute',
     right: 0
   },
-  actionsContainer: {
+  actionsContainer: ({ isMobile }) => ({
     display: 'flex',
     height: '100%',
     flexGrow: 1,
+    paddingRight: isTwakeTheme() && !isMobile ? 48 : 0,
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: isTwakeTheme() && !isMobile ? 'end' : 'center'
+  })
 })
+
+const SelectedCount = ({ styles, docs, onClose }) => {
+  const { isMobile } = useBreakpoints()
+  const { t } = useI18n()
+
+  const showMobileCloseButton = !!onClose && isMobile
+
+  if (isTwakeTheme()) {
+    if (isMobile)
+      return (
+        <Box
+          className="u-flex u-flex-items-center u-h-100 u-pr-1"
+          borderRadius="15px 0 0 0"
+          bgcolor="primary.main"
+          color="primary.contrastText"
+        >
+          <IconButton color="inherit" onClick={onClose}>
+            <Icon icon={CrossCircleIcon} />
+          </IconButton>
+          {docs.length}
+        </Box>
+      )
+
+    return (
+      <div className="u-flex u-flex-items-center u-h-100 u-ph-1">
+        <Icon
+          className="u-mr-1"
+          icon={CheckCircleIcon}
+          color="var(--iconTextColor)"
+        />
+        <Typography variant="body1" component="span">
+          {t('selected_light', docs.length)}
+        </Typography>
+      </div>
+    )
+  }
+
+  return (
+    <Box
+      className={styles.selectedCount}
+      padding={showMobileCloseButton ? '0 1rem 0 0' : '0 1rem'}
+      color="primary.contrastText"
+      bgcolor="primary.main"
+    >
+      {showMobileCloseButton && (
+        <IconButton color="inherit" onClick={onClose}>
+          <Icon icon={CrossCircleIcon} />
+        </IconButton>
+      )}
+      <div>{isMobile ? docs.length : t('selected', docs.length)}</div>
+    </Box>
+  )
+}
 
 const ActionsBar = ({
   actions,
@@ -71,12 +136,10 @@ const ActionsBar = ({
   onClose
 }) => {
   const { isMobile } = useBreakpoints()
-  const { t } = useI18n()
   const anchorRef = useRef()
   const [showMenu, setShowMenu] = useState(false)
   const styles = useStyles({ isMobile })
 
-  const showMobileCloseButton = !!onClose && isMobile
   const showDesktopCloseButton = !!onClose && !isMobile
   const maxActionsDisplayed = isMobile ? 4 : maxDesktopActions
 
@@ -99,75 +162,65 @@ const ActionsBar = ({
   }
 
   return (
-    <AppBar className={styles.appBar} position="fixed" color="inherit">
-      <Toolbar
-        className={styles.toolbar}
-        variant={isMobile ? 'regular' : 'dense'}
-        disableGutters
-      >
-        {showDesktopCloseButton && (
-          <IconButton className={styles.desktopCloseButton} onClick={onClose}>
-            <Icon icon={CrossIcon} />
-          </IconButton>
-        )}
-        <Box
-          className={styles.selectedCount}
-          padding={showMobileCloseButton ? '0 1rem 0 0' : '0 1rem'}
-          color="primary.contrastText"
-          bgcolor="primary.main"
+    <CozyTheme variant={isTwakeTheme() ? 'inverted' : 'normal'}>
+      <AppBar className={styles.appBar} position="fixed" color="inherit">
+        <Toolbar
+          className={styles.toolbar}
+          variant={isMobile ? 'regular' : 'dense'}
+          disableGutters
         >
-          {showMobileCloseButton && (
-            <IconButton color="inherit" onClick={onClose}>
-              <Icon icon={CrossCircleIcon} />
+          {showDesktopCloseButton && (
+            <IconButton className={styles.desktopCloseButton} onClick={onClose}>
+              <Icon icon={CrossIcon} />
             </IconButton>
           )}
-          <div>{isMobile ? docs.length : t('selected', docs.length)}</div>
-        </Box>
-        <div className={styles.actionsContainer}>
-          {/* actions displayed in the bar itself */}
-          <ActionsItems
-            docs={docs}
-            component={ResponsiveAction}
-            actions={barActions}
-            onClick={handleClick}
-          />
+          <SelectedCount styles={styles} docs={docs} onClose={onClose} />
+          <div className={styles.actionsContainer}>
+            {/* actions displayed in the bar itself */}
+            <ActionsItems
+              docs={docs}
+              component={ResponsiveAction}
+              actions={barActions}
+              onClick={handleClick}
+            />
 
-          {/* actions displayed in ActionsMenu */}
-          {menuActions.length > 0 && (
-            <>
-              {isMobile ? (
-                <ActionsItems
+            {/* actions displayed in ActionsMenu */}
+            {menuActions.length > 0 && (
+              <>
+                {isMobile ? (
+                  <ActionsItems
+                    docs={docs}
+                    component={ResponsiveAction}
+                    actions={otherAction}
+                    onClick={() => setShowMenu(true)}
+                  />
+                ) : (
+                  <IconButton ref={anchorRef} onClick={() => setShowMenu(true)}>
+                    <Icon icon={DotsIcon} />
+                  </IconButton>
+                )}
+                <ActionsMenu
+                  ref={anchorRef}
+                  open={showMenu}
                   docs={docs}
-                  component={ResponsiveAction}
-                  actions={otherAction}
-                  onClick={() => setShowMenu(true)}
+                  actions={menuActions}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left'
+                  }}
+                  componentsProps={{
+                    actionsItems: {
+                      onClick: handleClick
+                    }
+                  }}
+                  onClose={() => setShowMenu(false)}
                 />
-              ) : (
-                <IconButton ref={anchorRef} onClick={() => setShowMenu(true)}>
-                  <Icon icon={DotsIcon} />
-                </IconButton>
-              )}
-              <ActionsMenu
-                ref={anchorRef}
-                open={showMenu}
-                docs={docs}
-                actions={menuActions}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left'
-                }}
-                componentsProps={{
-                  actionsItems: {
-                    onClick: handleClick
-                  }
-                }}
-                onClose={() => setShowMenu(false)}
-              />
-            </>
-          )}
-        </div>
-      </Toolbar>
-    </AppBar>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </CozyTheme>
   )
 }
 
