e8e1fa08e1fa4567d33d88f0f8fcda57f3452779
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-04 11:52:11 +0100

    feat: Modify Circle and LegacyAvatar

--- react/legacy/Avatar/helpers.js
+++ react/legacy/Avatar/helpers.js
@@ -1,27 +1,35 @@
+import { colorMapping } from '../../Avatar/helpers'
+import { isTwakeTheme } from '../../helpers/isTwakeTheme'
 import palette from '../../palette'
 
+const makeKey = (colors, name) =>
+  Array.from(name.toUpperCase())
+    .map(letter => letter.charCodeAt(0))
+    .reduce((sum, number) => sum + number, 0) % colors.length
+
+const colors = [
+  palette['azure'],
+  palette['melon'],
+  palette['blazeOrange'],
+  palette['pomegranate'],
+  palette['mango'],
+  palette['pumpkinOrange'],
+  palette['darkPeriwinkle'],
+  palette['purpley'],
+  palette['lightishPurple'],
+  palette['weirdGreen'],
+  palette['puertoRico'],
+  palette['emerald'],
+  palette['seafoamGreen'],
+  palette['lavender'],
+  palette['brightSun'],
+  palette['fuchsia']
+]
+
+const twakeColors = Object.values(colorMapping).filter(Boolean)
+
 export const nameToColor = (name = '') => {
-  const colors = [
-    palette['azure'],
-    palette['melon'],
-    palette['blazeOrange'],
-    palette['pomegranate'],
-    palette['mango'],
-    palette['pumpkinOrange'],
-    palette['darkPeriwinkle'],
-    palette['purpley'],
-    palette['lightishPurple'],
-    palette['weirdGreen'],
-    palette['puertoRico'],
-    palette['emerald'],
-    palette['seafoamGreen'],
-    palette['lavender'],
-    palette['brightSun'],
-    palette['fuchsia']
-  ]
-  const key =
-    Array.from(name.toUpperCase())
-      .map(letter => letter.charCodeAt(0))
-      .reduce((sum, number) => sum + number, 0) % colors.length
-  return colors[key]
+  const _colors = isTwakeTheme() ? twakeColors : colors
+  const key = makeKey(_colors, name)
+  return _colors[key]
 }
