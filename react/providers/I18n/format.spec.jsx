9d4ab11f4a3763a6d2db58bdcee3bf99489bbf4c
Théo Poizat <hello@zatteo.com>
2025-03-05 14:44:11 +0100

    feat: Migrate date formatting to es module
    To allow efficient tree shaking by bundlers we need to use only es
    module.
    
    Here we migrate the require used to import dynamically the
    appropriate date formatting from date-fns. We lose the support of
    date formatting in a lot of languages, but we currently support only
    en, fr and es languages so :
    - it was not used
    - it has no senses to display everything in a default language and
    just format the date in another language

--- react/providers/I18n/format.spec.jsx
+++ react/providers/I18n/format.spec.jsx
@@ -4,9 +4,11 @@ describe('initFormat', () => {
   beforeEach(() => {
     jest.spyOn(console, 'warn').mockImplementation(() => {})
   })
+
   afterEach(() => {
     console.warn.mockRestore()
   })
+
   it('should not throw if a date-fns locale can not be found', () => {
     expect(() => initFormat('unknown-lang', 'unknown-default')).not.toThrow()
   })
@@ -17,7 +19,13 @@ describe('initFormat', () => {
     expect(date).toBe('1970-janvier-01')
   })
 
-  it('should fallback english if a date-fns locale can not be found', () => {
+  it('should use the correct default locale if user locale can not be found', () => {
+    const f = initFormat('it', 'fr')
+    const date = f(0, 'yyyy-LLLL-dd')
+    expect(date).toBe('1970-janvier-01')
+  })
+
+  it('should fallback english if user locale and default locale can not be found', () => {
     const f = initFormat('unknown-lang', 'unknown-default')
     const date = f(0, 'yyyy-LLLL-dd')
     expect(date).toBe('1970-January-01')
@@ -25,6 +33,12 @@ describe('initFormat', () => {
 })
 
 describe('formatLocallyDistanceToNow', () => {
+  beforeEach(() => {
+    // To avoid currentLocale set to 'fr' because of previous tests
+    const f = initFormat('en')
+    f(0, 'yyyy-LLLL-dd')
+  })
+
   it('should formatDistanceToNow with small value', () => {
     const date = Date.now() + 29 * 1000 // 29s
 
