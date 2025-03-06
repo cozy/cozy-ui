133d58c8216050d212bde0072410cb9f775aa1ef
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-06 12:01:15 +0100

    feat: Add Twake app icons

---
+++ react/Icons/Pass_twake.jsx
@@ -0,0 +1,69 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/twake/illus/pass_twake.svg` to regenerate;
+import React from 'react'
+
+function SvgPassTwake(props) {
+  return (
+    <svg viewBox="0 0 16 17" fill="none" {...props}>
+      <path
+        d="M0 5.58A5.284 5.284 0 015.284.296h5.432A5.284 5.284 0 0116 5.58v5.432a5.284 5.284 0 01-5.284 5.284H5.284A5.284 5.284 0 010 11.012V5.58z"
+        fill="#fff"
+      />
+      <path
+        d="M0 5.58A5.284 5.284 0 015.284.296h5.432A5.284 5.284 0 0116 5.58v5.432a5.284 5.284 0 01-5.284 5.284H5.284A5.284 5.284 0 010 11.012V5.58z"
+        fill="url(#pass_twake_svg__paint0_linear_11359_12605)"
+      />
+      <g filter="url(#pass_twake_svg__filter0_d_11359_12605)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4.664 12.232a.311.311 0 01-.31-.315l.012-.912 2.589-2.588a2.659 2.659 0 01-.229-1.538 2.49 2.49 0 01.74-1.453 2.537 2.537 0 011.86-.775c.723 0 1.343.258 1.86.775.517.516.775 1.136.775 1.86 0 .723-.258 1.343-.775 1.86a2.57 2.57 0 01-1.418.736 2.547 2.547 0 01-1.573-.225l-.109.109v.928c0 .172-.14.311-.31.311h-.619a.311.311 0 00-.31.312v.615a.31.31 0 01-.314.311l-1.87-.011zm5.128-4.647a.778.778 0 100-1.556.778.778 0 000 1.556z"
+          fill="#fff"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="pass_twake_svg__paint0_linear_11359_12605"
+          x1={2.774}
+          y1={14.859}
+          x2={15.094}
+          y2={1.275}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.23} stopColor="#B6C4DF" />
+          <stop offset={1} stopColor="#D3DDF5" />
+        </linearGradient>
+        <filter
+          id="pass_twake_svg__filter0_d_11359_12605"
+          x={2.995}
+          y={3.563}
+          width={10.326}
+          height={10.312}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={0.272} />
+          <feGaussianBlur stdDeviation={0.68} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_11359_12605"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_11359_12605"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
+
+export default SvgPassTwake
