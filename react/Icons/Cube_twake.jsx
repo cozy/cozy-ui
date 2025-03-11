d7c4010eb08b362e8b10ab637ff1c48af63a9a15
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-11 10:01:05 +0100

    fix(Cube): Missed `ref` props

--- react/Icons/Cube_twake.jsx
+++ react/Icons/Cube_twake.jsx
@@ -1,9 +1,9 @@
 // Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/twake/cube_twake.svg` to regenerate;
 import React from 'react'
 
-function SvgCubeTwake(props) {
+function SvgCubeTwake({ iconRef, ...props }) {
   return (
-    <svg viewBox="0 0 15 16" {...props}>
+    <svg viewBox="0 0 15 16" {...props} ref={iconRef}>
       <path d="M14.4 4L7.2 0 0 4v8l7.2 4 7.2-4V4zM7.2 1.832l4.728 2.624L9.52 5.792a3.213 3.213 0 00-4.64 0L2.472 4.456 7.2 1.832zm-.8 11.896l-4.8-2.664V5.808L4.104 7.2A3.193 3.193 0 006.4 11.096v2.632zM5.6 8c0-.88.72-1.6 1.6-1.6.88 0 1.6.72 1.6 1.6 0 .88-.72 1.6-1.6 1.6-.88 0-1.6-.72-1.6-1.6zM8 13.728v-2.624a3.193 3.193 0 002.4-3.096c0-.28-.032-.552-.104-.808L12.8 5.808v5.256L8 13.728z" />
     </svg>
   )
