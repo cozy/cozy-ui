0eb2b26cac2f58e27cefd0b65526733b2680bd7c
Théo Poizat <hello@zatteo.com>
2025-03-21 11:31:12 +0100

    feat: Add Twake apps text icons
    Twake apps text icons use linear gradients and a new font called Cal
    Sans. Font is 100KB so it's lighter to use svg icons that adds only
    23KB.
    
    To manage dark mode, we can change the fill attribute of TwakeText.
    Other icons are dark mode friendly.

---
+++ react/Icons/NotesText.jsx
@@ -0,0 +1,88 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/twake/illus/notes-text.svg` to regenerate;
+import React from 'react'
+
+function SvgNotesText(props) {
+  return (
+    <svg viewBox="0 0 479 136" fill="none" {...props}>
+      <path
+        d="M0 132.881V0h20.122l64.732 76.881c3.037 3.417 6.075 7.024 8.732 11.01-.759-5.695-.949-13.857-.949-21.64V0h24.298v132.881H99.661l-67.2-79.918c-3.037-3.417-6.075-7.024-8.732-11.01.76 5.694.949 13.857.949 21.64v69.288H0z"
+        fill="url(#notes-text_svg__paint0_linear_59620_144)"
+      />
+      <path
+        d="M182.772 135.349c-30.562 0-52.583-23.159-52.583-51.634 0-28.474 22.021-51.824 52.583-51.824 30.563 0 52.583 23.35 52.583 51.824 0 28.475-22.02 51.634-52.583 51.634zm-26.956-51.634c0 15.376 10.631 28.285 26.956 28.285 16.326 0 26.956-12.909 26.956-28.285s-10.63-28.474-26.956-28.474c-16.325 0-26.956 13.098-26.956 28.474z"
+        fill="url(#notes-text_svg__paint1_linear_59620_144)"
+      />
+      <path
+        d="M278.718 132.881c-19.552 0-30.183-9.112-30.183-30.563V56h-13.857V34.17h13.857V14.236l25.627-2.657v22.59h20.882V56h-20.882v44.99c0 6.264 3.038 9.112 7.973 9.112h10.631v22.779h-14.048z"
+        fill="url(#notes-text_svg__paint2_linear_59620_144)"
+      />
+      <path
+        d="M348.282 135.349c-30.563 0-51.064-23.159-51.064-51.634 0-28.474 21.451-52.013 51.064-52.013 29.614 0 50.115 23.539 50.115 52.013 0 2.848-.19 5.885-.759 9.112h-73.844c2.847 11.58 11.58 19.742 24.488 19.742 11.01 0 19.553-5.695 24.108-13.288l19.933 14.997c-7.973 12.339-24.488 21.071-44.041 21.071zm-24.488-61.695h48.786c-2.847-11.01-12.149-19.932-24.678-19.932-12.149 0-21.261 8.163-24.108 19.932z"
+        fill="url(#notes-text_svg__paint3_linear_59620_144)"
+      />
+      <path
+        d="M440.657 135.349c-16.705 0-30.183-5.695-41.004-17.275l17.275-15.756c7.213 8.163 14.807 12.15 23.159 12.15 8.732 0 13.668-4.366 13.668-10.251 0-5.126-2.468-7.973-18.034-11.58-26.386-6.264-30.373-18.224-30.373-30.752 0-17.275 13.668-30.183 36.827-30.183 15.946 0 26.577 3.986 36.638 17.084l-18.604 14.238c-4.746-7.594-10.82-10.63-17.464-10.63-7.024 0-12.339 2.657-12.339 8.731 0 3.417 1.519 6.455 13.288 9.492 28.664 7.403 35.308 18.034 35.308 32.84 0 18.224-16.515 31.892-38.345 31.892z"
+        fill="url(#notes-text_svg__paint4_linear_59620_144)"
+      />
+      <defs>
+        <linearGradient
+          id="notes-text_svg__paint0_linear_59620_144"
+          x1={236.012}
+          y1={7.974}
+          x2={236.012}
+          y2={132.881}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC953" />
+          <stop offset={1} stopColor="#FFB81E" />
+        </linearGradient>
+        <linearGradient
+          id="notes-text_svg__paint1_linear_59620_144"
+          x1={236.012}
+          y1={7.974}
+          x2={236.012}
+          y2={132.881}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC953" />
+          <stop offset={1} stopColor="#FFB81E" />
+        </linearGradient>
+        <linearGradient
+          id="notes-text_svg__paint2_linear_59620_144"
+          x1={236.012}
+          y1={7.974}
+          x2={236.012}
+          y2={132.881}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC953" />
+          <stop offset={1} stopColor="#FFB81E" />
+        </linearGradient>
+        <linearGradient
+          id="notes-text_svg__paint3_linear_59620_144"
+          x1={236.012}
+          y1={7.974}
+          x2={236.012}
+          y2={132.881}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC953" />
+          <stop offset={1} stopColor="#FFB81E" />
+        </linearGradient>
+        <linearGradient
+          id="notes-text_svg__paint4_linear_59620_144"
+          x1={236.012}
+          y1={7.974}
+          x2={236.012}
+          y2={132.881}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFC953" />
+          <stop offset={1} stopColor="#FFB81E" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default SvgNotesText
