305b67d03eead395ec21f3e9c1ee77a355ecce60
Théo Poizat <hello@zatteo.com>
2025-03-24 14:29:47 +0100

    feat: Rewritten AppTitle component
    Display the full title of an app with icon, brand name and app name.
    Support Twake theme only.
    
    It can fallback on a text passed as a children if app is not
    supported. This fallback corresponds to previous AppTitle which was
    unused.

--- react/AppTitle/Readme.md
+++ react/AppTitle/Readme.md
@@ -1,9 +1,22 @@
-Displays the App title in the Cozy Bar
+
+Display the app title of an app with icon, brand name and app name. Support Twake theme only.
+
+You can pass a child that will be used as default value is app is not supported.
 
 ### Default
 
 ```jsx
 import AppTitle from 'cozy-ui/transpiled/react/AppTitle';
 
-<AppTitle>Drive</AppTitle>
+const slugs = ["chat", "drive", "mail", "notes", "pass", "store", "home"];
+
+<div class="u-flex u-flex-column">
+  {
+    isTwakeTheme() && slugs.map(slug => <AppTitle slug={slug} />)
+  }
+  <AppTitle slug="coachco2" />
+  <AppTitle slug="coachco2">CoachCO2</AppTitle>
+  <AppTitle>Drive</AppTitle>
+</div>
+
 ```
--- react/AppTitle/index.jsx
+++ react/AppTitle/index.jsx
@@ -1,24 +1,105 @@
-import Typography from '@material-ui/core/Typography'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import styles from './styles.styl'
+import Icon from '../Icon'
+import ChatIcon from '../Icons/Chat'
+import ChatTextIcon from '../Icons/ChatText'
+import DriveIcon from '../Icons/Drive'
+import DriveTextIcon from '../Icons/DriveText'
+import MailIcon from '../Icons/Mail'
+import MailTextIcon from '../Icons/MailText'
+import NotesIcon from '../Icons/Notes'
+import NotesTextIcon from '../Icons/NotesText'
+import PassIcon from '../Icons/Pass'
+import PassTextIcon from '../Icons/PassText'
+import StoreIcon from '../Icons/Store'
+import StoreTextIcon from '../Icons/StoreText'
+import TwakeTextIcon from '../Icons/TwakeText'
+import TwakeWorkplaceIcon from '../Icons/TwakeWorkplace'
+import WorkplaceTextIcon from '../Icons/WorkplaceText'
+import Typography from '../Typography'
+import { useCozyTheme } from '../providers/CozyTheme'
 
-export const AppTitle = ({ children, tag, className, ...restProps }) => {
+const SLUG_TO_ICONS = {
+  chat: {
+    icon: ChatIcon,
+    text: ChatTextIcon
+  },
+  drive: {
+    icon: DriveIcon,
+    text: DriveTextIcon
+  },
+  mail: {
+    icon: MailIcon,
+    text: MailTextIcon
+  },
+  notes: {
+    icon: NotesIcon,
+    text: NotesTextIcon
+  },
+  pass: {
+    icon: PassIcon,
+    text: PassTextIcon
+  },
+  store: {
+    icon: StoreIcon,
+    text: StoreTextIcon
+  },
+  home: {
+    icon: TwakeWorkplaceIcon,
+    text: WorkplaceTextIcon
+  }
+}
+
+const AppTitle = ({ slug, tag, className, children, ...otherProps }) => {
+  const { isLight } = useCozyTheme()
+
+  const titleData = SLUG_TO_ICONS[slug]
+
+  if (!titleData)
+    return (
+      <Typography
+        component={tag}
+        variant="h4"
+        className={cx(styles['c-apptitle'], className)}
+        {...otherProps}
+      >
+        {children}
+      </Typography>
+    )
+
   return (
-    <Typography
-      component={tag}
-      variant="h4"
-      className={cx(styles['c-apptitle'], className)}
-      {...restProps}
-    >
-      {children}
-    </Typography>
+    <div className="u-flex u-flex-items-center">
+      <Icon
+        icon={titleData.icon}
+        size="32"
+        className={styles['c-apptitle-app-icon']}
+      />
+      <Icon
+        icon={TwakeTextIcon}
+        width="auto"
+        height="22"
+        className={cx(
+          'u-mr-half',
+          'u-w-auto',
+          isLight ? styles['c-app-title-light'] : styles['c-app-title-dark']
+        )}
+      />
+      <Icon
+        icon={titleData.text}
+        width="auto"
+        height="22"
+        className={cx('u-w-auto', slug === 'home' ? 'u-mt-half' : undefined)}
+      />
+    </div>
   )
 }
 
 AppTitle.propTypes = {
+  slug: PropTypes.string,
+  tag: PropTypes.string,
   className: PropTypes.string
 }
 
