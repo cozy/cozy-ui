dc8b4b0931ac868a1cca4118fecf06ec688d6102
Théo Poizat <hello@zatteo.com>
2025-03-26 12:16:01 +0100

    feat: Add CloudRainbow, Mosaic and PersonAdd icons

---
+++ react/Icons/CloudRainbow.jsx
@@ -0,0 +1,63 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/cloud-rainbow.svg` to regenerate;
+import React from 'react'
+
+function SvgCloudRainbow(props) {
+  return (
+    <svg viewBox="0 0 16 11" {...props}>
+      <path
+        d="M8 1.333a3.669 3.669 0 013.593 2.954l.2 1 1.02.073c1.04.067 1.854.94 1.854 1.973 0 1.1-.9 2-2 2H4a2.666 2.666 0 01-2.667-2.666A2.649 2.649 0 013.707 4.02l.713-.073.333-.634A3.646 3.646 0 018 1.333zM8 0a4.997 4.997 0 00-4.433 2.693A3.996 3.996 0 000 6.667c0 2.206 1.793 4 4 4h8.667A3.335 3.335 0 0016 7.333a3.317 3.317 0 00-3.1-3.306A4.993 4.993 0 008 0z"
+        fill="#fff"
+      />
+      <path
+        d="M8 1.333a3.669 3.669 0 013.593 2.954l.2 1 1.02.073c1.04.067 1.854.94 1.854 1.973 0 1.1-.9 2-2 2H4a2.666 2.666 0 01-2.667-2.666A2.649 2.649 0 013.707 4.02l.713-.073.333-.634A3.646 3.646 0 018 1.333zM8 0a4.997 4.997 0 00-4.433 2.693A3.996 3.996 0 000 6.667c0 2.206 1.793 4 4 4h8.667A3.335 3.335 0 0016 7.333a3.317 3.317 0 00-3.1-3.306A4.993 4.993 0 008 0z"
+        fill="url(#cloud-rainbow_svg__paint0_linear_11512_6354)"
+      />
+      <path
+        d="M8 1.333a3.669 3.669 0 013.593 2.954l.2 1 1.02.073c1.04.067 1.854.94 1.854 1.973 0 1.1-.9 2-2 2H4a2.666 2.666 0 01-2.667-2.666A2.649 2.649 0 013.707 4.02l.713-.073.333-.634A3.646 3.646 0 018 1.333zM8 0a4.997 4.997 0 00-4.433 2.693A3.996 3.996 0 000 6.667c0 2.206 1.793 4 4 4h8.667A3.335 3.335 0 0016 7.333a3.317 3.317 0 00-3.1-3.306A4.993 4.993 0 008 0z"
+        fill="url(#cloud-rainbow_svg__paint1_linear_11512_6354)"
+      />
+      <path
+        d="M8 1.333a3.669 3.669 0 013.593 2.954l.2 1 1.02.073c1.04.067 1.854.94 1.854 1.973 0 1.1-.9 2-2 2H4a2.666 2.666 0 01-2.667-2.666A2.649 2.649 0 013.707 4.02l.713-.073.333-.634A3.646 3.646 0 018 1.333zM8 0a4.997 4.997 0 00-4.433 2.693A3.996 3.996 0 000 6.667c0 2.206 1.793 4 4 4h8.667A3.335 3.335 0 0016 7.333a3.317 3.317 0 00-3.1-3.306A4.993 4.993 0 008 0z"
+        fill="url(#cloud-rainbow_svg__paint2_linear_11512_6354)"
+      />
+      <defs>
+        <linearGradient
+          id="cloud-rainbow_svg__paint0_linear_11512_6354"
+          x1={16}
+          y1={6.222}
+          x2={0}
+          y2={6.222}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.13} stopColor="#A033FF" />
+          <stop offset={0.61} stopColor="#0094FF" />
+          <stop offset={1} stopColor="#4FB500" />
+        </linearGradient>
+        <linearGradient
+          id="cloud-rainbow_svg__paint1_linear_11512_6354"
+          x1={7.333}
+          y1={3.333}
+          x2={4.833}
+          y2={10.833}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FFD600" stopOpacity={0} />
+          <stop offset={0.563} stopColor="#FFC700" />
+        </linearGradient>
+        <linearGradient
+          id="cloud-rainbow_svg__paint2_linear_11512_6354"
+          x1={8.667}
+          y1={6.222}
+          x2={12.079}
+          y2={11.642}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.03} stopColor="#FF3B30" stopOpacity={0} />
+          <stop offset={0.843} stopColor="#E73B2D" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default SvgCloudRainbow
