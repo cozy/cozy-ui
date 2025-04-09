77acb40ee796f7058a1a93a240cdd988bd2115ad
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-04-09 11:33:48 +0200

    fix(Icons): Remove forced color on illus and permission icons

--- react/Icons/Sinister_twake.jsx
+++ react/Icons/Sinister_twake.jsx
@@ -3,12 +3,8 @@ import React from 'react'
 
 function SvgSinisterTwake(props) {
   return (
-    <svg viewBox="0 0 16 15" fill="none" {...props}>
-      <path
-        d="M8 3.493l5.476 9.462H2.524L8 3.492zM8 .59L0 14.409h16L8 .591zm.727 10.182H7.273v1.454h1.454v-1.454zm0-4.364H7.273v2.91h1.454v-2.91z"
-        fill="#1D212A"
-        fillOpacity={0.9}
-      />
+    <svg viewBox="0 0 16 15" {...props}>
+      <path d="M8 3.493l5.476 9.462H2.524L8 3.492zM8 .59L0 14.409h16L8 .591zm.727 10.182H7.273v1.454h1.454v-1.454zm0-4.364H7.273v2.91h1.454v-2.91z" />
     </svg>
   )
 }
