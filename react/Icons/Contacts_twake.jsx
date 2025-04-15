b7f8d880e5897c21f9bead596b810a9f41620ee6
Théo Poizat <hello@zatteo.com>
2025-04-15 08:57:06 +0200

    feat: Update Contacts Twake icon

--- react/Icons/Contacts_twake.jsx
+++ react/Icons/Contacts_twake.jsx
@@ -3,40 +3,31 @@ import React from 'react'
 
 function SvgContactsTwake(props) {
   return (
-    <svg viewBox="0 0 16 16" fill="none" {...props}>
-      <rect width={16} height={16} rx={5.284} fill="#fff" />
-      <rect
-        width={16}
-        height={16}
-        rx={5.284}
-        fill="url(#contacts_twake_svg__paint0_linear_11294_1829)"
-      />
-      <g filter="url(#contacts_twake_svg__filter0_d_11294_1829)">
+    <svg viewBox="0 0 32 32" fill="none" {...props}>
+      <g filter="url(#contacts_twake_svg__filter0_d_3096_936)">
         <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M3.107 4.113c-.251.492-.251 1.137-.251 2.427v2.993c0 1.29 0 1.934.25 2.427.221.433.574.786 1.007 1.006.492.251 1.137.251 2.427.251h2.993c1.29 0 1.935 0 2.427-.25a2.304 2.304 0 001.006-1.007c.251-.493.251-1.137.251-2.427V6.54c0-1.29 0-1.935-.25-2.427a2.302 2.302 0 00-1.007-1.007c-.492-.25-1.137-.25-2.427-.25H6.54c-1.29 0-1.935 0-2.427.25a2.299 2.299 0 00-1.006 1.007zm4.93 1.045a1.439 1.439 0 110 2.878 1.439 1.439 0 010-2.878zm-2.879 5.395v-.347c0-1.24 1.918-1.594 2.878-1.594.961 0 2.879.354 2.879 1.594v.347c0 .2-.162.362-.362.362H5.52a.362.362 0 01-.362-.362z"
+          d="M15.992 5.874c5.857 0 10.606 4.102 10.606 9.16 0 5.06-4.749 9.16-10.606 9.16a12.12 12.12 0 01-3.432-.49c-1.203.936-3.069 1.722-5.596 2.356a.485.485 0 01-.541-.706l.18-.329c.84-1.544 1.364-2.811 1.572-3.8-1.733-1.63-2.79-3.804-2.79-6.19 0-5.06 4.749-9.16 10.607-9.16z"
           fill="#fff"
         />
       </g>
+      <g clipPath="url(#contacts_twake_svg__clip0_3096_936)">
+        <rect
+          width={32}
+          height={32}
+          rx={10}
+          fill="url(#contacts_twake_svg__paint0_linear_3096_936)"
+        />
+        <g filter="url(#contacts_twake_svg__filter1_d_3096_936)" fill="#fff">
+          <path d="M15.99 16.016a4.5 4.5 0 100-9 4.5 4.5 0 000 9zm8.85 6.028c-.68-1.907-2.795-4.028-8.85-4.028s-8.171 2.122-8.85 4.029c-.599 1.682 1.081 2.971 2.867 2.971h11.965c1.786 0 3.466-1.289 2.867-2.971z" />
+        </g>
+      </g>
       <defs>
-        <linearGradient
-          id="contacts_twake_svg__paint0_linear_11294_1829"
-          x1={2.774}
-          y1={14.564}
-          x2={15.094}
-          y2={0.979}
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset={0.23} stopColor="#F74EA9" />
-          <stop offset={1} stopColor="#FFB2A8" />
-        </linearGradient>
         <filter
-          id="contacts_twake_svg__filter0_d_11294_1829"
-          x={2.04}
-          y={2.203}
-          width={11.994}
-          height={11.994}
+          id="contacts_twake_svg__filter0_d_3096_936"
+          x={3.753}
+          y={4.569}
+          width={24.477}
+          height={23.463}
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
@@ -46,20 +37,63 @@ function SvgContactsTwake(props) {
             values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
             result="hardAlpha"
           />
-          <feOffset dy={0.163} />
-          <feGaussianBlur stdDeviation={0.408} />
+          <feOffset dy={0.326} />
+          <feGaussianBlur stdDeviation={0.816} />
           <feComposite in2="hardAlpha" operator="out" />
           <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
           <feBlend
             in2="BackgroundImageFix"
-            result="effect1_dropShadow_11294_1829"
+            result="effect1_dropShadow_3096_936"
           />
           <feBlend
             in="SourceGraphic"
-            in2="effect1_dropShadow_11294_1829"
+            in2="effect1_dropShadow_3096_936"
             result="shape"
           />
         </filter>
+        <filter
+          id="contacts_twake_svg__filter1_d_3096_936"
+          x={4.16}
+          y={4.731}
+          width={23.66}
+          height={23.714}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={0.571} />
+          <feGaussianBlur stdDeviation={1.429} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_3096_936"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_3096_936"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="contacts_twake_svg__paint0_linear_3096_936"
+          x1={5.548}
+          y1={29.127}
+          x2={30.188}
+          y2={1.958}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.23} stopColor="#F74EA9" />
+          <stop offset={1} stopColor="#FFB2A8" />
+        </linearGradient>
+        <clipPath id="contacts_twake_svg__clip0_3096_936">
+          <path fill="#fff" d="M0 0h31.983v31.983H0z" />
+        </clipPath>
       </defs>
     </svg>
   )
