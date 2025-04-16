8caa5083b234f3fd51e00f1c611898651a095de3
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-04-16 15:48:12 +0200

    feat(Sprite): Add Twake icons

--- react/Icon/Sprite.jsx
+++ react/Icon/Sprite.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
-import IconSprintContent from './icons-sprite'
+import IconSpriteContent from './icons-sprite'
+import IconSpriteContentTwake from './icons-sprite-twake'
+import { isTwakeTheme } from '../helpers/isTwakeTheme'
 
 const displayNone = { display: 'none' }
 
 const Sprite = () => {
+  const SpriteContent = isTwakeTheme()
+    ? IconSpriteContentTwake
+    : IconSpriteContent
+
   return (
     <div
       style={displayNone}
-      dangerouslySetInnerHTML={{ __html: IconSprintContent }}
+      dangerouslySetInnerHTML={{ __html: SpriteContent }}
     />
   )
 }
