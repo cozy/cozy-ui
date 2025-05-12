88efbb253cec81ac9d88f1703dc9383a4a2cf134
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-12 16:45:29 +0200

    feat: Remove `isTwakeTheme` function
    BREAKING CHANGE: `isTwakeTheme` has been removed. You no longer need to import it. You can safely remove it and keep the truthy result. Besides, you probably have to update your test snapshot, and maybe test itselves too.

--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -210,7 +210,6 @@ module.exports = {
   context: {
     utils: path.resolve(__dirname, 'utils'),
     isTesting: path.resolve(__dirname, '../react/helpers/isTesting'),
-    isTwakeTheme: path.resolve(__dirname, '../react/helpers/isTwakeTheme'),
     content: path.resolve(__dirname, 'fixtures/content')
   },
   styles: {
