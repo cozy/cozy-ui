2a0d6262b609e754580a8b362b8b290eff6cfb47
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-21 16:34:54 +0200

    feat(TableRow): Add disabled style

--- react/TableRow/index.js
+++ react/TableRow/index.js
@@ -1,3 +1,18 @@
-import TableRow from '@material-ui/core/TableRow'
+import MuiTableRow from '@material-ui/core/TableRow'
+import cx from 'classnames'
+import React, { forwardRef } from 'react'
+
+const TableRow = forwardRef(({ disabled, className, ...props }, ref) => {
+  return (
+    <MuiTableRow
+      {...props}
+      className={cx(className, { disabled })}
+      disabled={disabled}
+      ref={ref}
+    />
+  )
+})
+
+TableRow.displayName = 'TableRow'
 
 export default TableRow
