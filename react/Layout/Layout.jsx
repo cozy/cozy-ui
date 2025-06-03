50726af57ab9e8bff245e08f784bc7474afb85f2
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-03 13:22:56 +0200

    feat(Layout): Move style into sidebar and isolate layout styles
    BREAKING CHANGE: Layout doesn't apply style on Main and Content anymore, they have their own styles now. So if you didn't use these components but, for example, `<main>` instead of `<Main>` and `<div role="main">` instead of `<Content>`, you could have a style problem. So prefer `Main` and `Content` components.

--- react/Layout/Layout.jsx
+++ react/Layout/Layout.jsx
@@ -1,44 +1,105 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { createContext, useContext, useMemo } from 'react'
 
 import styles from './styles.styl'
+
+export const LayoutContext = createContext()
+
+export const useLayout = () => {
+  const context = useContext(LayoutContext)
+
+  if (!context) {
+    throw new Error('useLayout must be used within a LayoutContext')
+  }
+  return context
+}
+
+const LayoutProvider = React.memo(({ monoColumn, withTopBar, children }) => {
+  const value = useMemo(
+    () => ({
+      monoColumn,
+      withTopBar
+    }),
+    [monoColumn, withTopBar]
+  )
+
+  return (
+    <LayoutContext.Provider value={value}>{children}</LayoutContext.Provider>
+  )
+})
+
+LayoutProvider.displayName = 'LayoutProvider'
 
 export const Layout = ({
   children,
   className,
   monoColumn,
   withTopBar,
-  ...rest
+  ...props
 }) => {
+  const variant = monoColumn ? 'monocolumn' : '2panes'
+
   return (
-    <div
+    <LayoutProvider monoColumn={monoColumn} withTopBar={withTopBar}>
+      <div
+        className={cx(
+          className,
+          styles['o-layout'],
+          styles[`o-layout-${variant}`],
+          {
+            [styles[`o-layout-${variant}--topbar`]]: withTopBar
+          }
+        )}
+        {...props}
+      >
+        {children}
+      </div>
+    </LayoutProvider>
+  )
+}
+
+export const Main = ({ className, children, ...props }) => {
+  const { monoColumn, withTopBar } = useLayout()
+  const variant = monoColumn ? 'monocolumn' : '2panes'
+
+  return (
+    <main
       className={cx(
-        monoColumn ? styles['o-layout'] : styles['o-layout-2panes'],
         className,
+        styles['o-layout-main'],
+        styles[`o-layout-main-${variant}`],
         {
-          [styles[`o-layout${monoColumn ? '' : '-2panes'}--withTopBar`]]:
-            withTopBar
+          [styles[`o-layout-main-${variant}--topbar`]]: withTopBar
         }
       )}
-      {...rest}
+      {...props}
+    >
+      {children}
+    </main>
+  )
+}
+
+export const Content = ({ className, children, ...props }) => {
+  const { monoColumn, withTopBar } = useLayout()
+  const variant = monoColumn ? 'monocolumn' : '2panes'
+
+  return (
+    <div
+      role="main"
+      className={cx(
+        className,
+        styles['o-layout-content'],
+        styles[`o-layout-content-${variant}`],
+        {
+          [styles[`o-layout-content-${variant}--topbar`]]: withTopBar
+        }
+      )}
+      {...props}
     >
       {children}
     </div>
   )
-}
-
-export const Main = ({ children, ...rest }) => <main {...rest}>{children}</main>
-
-export class Content extends Component {
-  render() {
-    const { children, ...rest } = this.props
-    return (
-      <div role="main" {...rest}>
-        {children}
-      </div>
-    )
-  }
 }
 
 Layout.propTypes = {
