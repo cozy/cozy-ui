86e0b9368bdea50535cfe6603611c5c3ced30082
Théo Poizat <hello@zatteo.com>
2025-06-16 16:13:36 +0200

    feat(ExtendableFab): Forward ref in ExtendableFab component

--- react/Fab/ExtendableFab.jsx
+++ react/Fab/ExtendableFab.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
 import Fab from '.'
 import Icon from '../Icon'
 import useScroll from '../hooks/useScroll'
 
-const ExtendableFab = ({
-  label,
-  icon,
-  follow,
-  topLimit = 50,
-  scrollOptions,
-  ...rest
-}) => {
-  const { scrollTop } = useScroll(follow, scrollOptions)
-  const isBelowTopLimit = scrollTop < topLimit
+const ExtendableFab = forwardRef(
+  ({ label, icon, follow, topLimit = 50, scrollOptions, ...rest }, ref) => {
+    const { scrollTop } = useScroll(follow, scrollOptions)
+    const isBelowTopLimit = scrollTop < topLimit
 
-  return (
-    <Fab
-      aria-label={label}
-      variant={isBelowTopLimit ? 'extended' : 'circular'}
-      {...rest}
-    >
-      <Icon icon={icon} {...(isBelowTopLimit && { className: 'u-mr-half' })} />
-      {isBelowTopLimit && label}
-    </Fab>
-  )
-}
+    return (
+      <Fab
+        ref={ref}
+        aria-label={label}
+        variant={isBelowTopLimit ? 'extended' : 'circular'}
+        {...rest}
+      >
+        <Icon
+          icon={icon}
+          {...(isBelowTopLimit && { className: 'u-mr-half' })}
+        />
+        {isBelowTopLimit && label}
+      </Fab>
+    )
+  }
+)
+
+ExtendableFab.displayName = 'ExtendableFab'
 
 export default ExtendableFab
