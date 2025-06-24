f009f34b1fab67a4cb06a2dafb7993a6bd5249af
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-24 15:11:06 +0200

    fix(ContactIdentity): Now show `me` correctly

--- react/ContactsList/locales/withContactsListLocales.jsx
+++ react/ContactsList/locales/withContactsListLocales.jsx
@@ -2,7 +2,7 @@ import en from './en.json'
 import fr from './fr.json'
 import withOnlyLocales from '../../providers/I18n/withOnlyLocales'
 
-const locales = {
+export const locales = {
   en,
   fr
 }
