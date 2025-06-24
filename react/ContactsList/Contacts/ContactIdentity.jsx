947770c9954bd199ef736a43586be30acefac73c
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-24 11:48:57 +0200

    feat(ContactIdentity): Now use `contact` as prop
    BREAKING CHANGE: When using `cozy-ui/transpiled/react/ContactsList/Contacts/ContactName` you have to pass a `contact` as prop and no longer `displayName` and `familyName`

--- react/ContactsList/Contacts/ContactIdentity.jsx
+++ react/ContactsList/Contacts/ContactIdentity.jsx
@@ -1,22 +1,18 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { models } from 'cozy-client'
+import { getInitials } from 'cozy-client/dist/models/contact'
 
 import ContactName from './ContactName'
 import { TableCell } from '../../deprecated/Table'
 import { Avatar } from '../../legacy/Avatar'
 import styles from '../styles.styl'
 
-const { getDisplayName, getInitials } = models.contact
-
 const MyselfMarker = (props, { t }) => (
   <span className={`${styles['contact-myself']}`}>({t('me')})</span>
 )
 
 const ContactIdentity = ({ contact }) => {
-  const name = contact.name || {}
-  const displayName = getDisplayName(contact) || undefined
   const isMyself = contact.metadata ? !!contact.metadata.me : false
 
   return (
@@ -25,7 +21,7 @@ const ContactIdentity = ({ contact }) => {
       className={`${styles['contact-identity']} u-flex u-flex-items-center u-ellipsis u-p-0`}
     >
       <Avatar text={getInitials(contact)} size="small" />
-      <ContactName displayName={displayName} familyName={name.familyName} />
+      <ContactName contact={contact} />
       {isMyself && <MyselfMarker />}
     </TableCell>
   )
