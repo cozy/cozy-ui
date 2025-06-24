f33ccb3e739c06425cbc38f18609eada5b377d9a
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-24 15:11:06 +0200

    fix(ContactIdentity): Now show `me` correctly

--- react/ContactsList/Contacts/ContactIdentity.jsx
+++ react/ContactsList/Contacts/ContactIdentity.jsx
@@ -6,14 +6,19 @@ import { getInitials } from 'cozy-client/dist/models/contact'
 import ContactName from './ContactName'
 import Avatar from '../../Avatar'
 import { TableCell } from '../../deprecated/Table'
+import { useI18n, useExtendI18n } from '../../providers/I18n'
+import { locales } from '../locales/withContactsListLocales'
 import styles from '../styles.styl'
 
-const MyselfMarker = (props, { t }) => (
-  <span className={`${styles['contact-myself']}`}>({t('me')})</span>
-)
+const MyselfMarker = () => {
+  useExtendI18n(locales)
+  const { t } = useI18n()
+
+  return <span className={`${styles['contact-myself']}`}>({t('me')})</span>
+}
 
 const ContactIdentity = ({ contact }) => {
-  const isMyself = contact.metadata ? !!contact.metadata.me : false
+  const isMyself = !!contact.me
 
   return (
     <TableCell
