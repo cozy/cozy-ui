16bae533fad306c53f6856cf5ba3d29b63e5a61d
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-06-25 14:08:15 +0200

    refactor(VirtualizedTable): Extract virtuosoComponentsDnd code
    no code change

---
+++ react/Table/Virtualized/Dnd/virtuosoComponents.jsx
@@ -0,0 +1,38 @@
+import React, { forwardRef } from 'react'
+
+import DnDConfigWrapper from './DnDConfigWrapper'
+import TableRowDnD from './TableRow'
+import TableContainer from '../../../TableContainer'
+import virtuosoComponents from '../virtuosoComponents'
+
+/**
+ Be aware that context is spread to every components but should not be spread to Table components
+ so we desctrure it from props, but don't spread to child to avoid its presence in DOM
+*/
+const virtuosoComponentsDnd = {
+  ...virtuosoComponents,
+  // eslint-disable-next-line no-unused-vars
+  Scroller: forwardRef(({ context, ...props }, ref) => (
+    <DnDConfigWrapper ref={ref}>
+      <TableContainer {...props} ref={ref} />
+    </DnDConfigWrapper>
+  )),
+  TableRow: forwardRef(({ item, context, ...props }, ref) => {
+    const isSelected = context?.isSelectedItem(item)
+    const isDisabled = context?.itemsInDropProcess.includes(item._id)
+
+    return (
+      <TableRowDnD
+        {...props}
+        ref={ref}
+        item={item}
+        context={context}
+        selected={isSelected}
+        disabled={isDisabled}
+        hover
+      />
+    )
+  })
+}
+
+export default virtuosoComponentsDnd
