1723ba114b9950fa7c4b6620f76dc32a7af419be
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-08-18 15:21:28 +0200

    feat(ContactsList): Add makeGroupLabelsAndCounts function

--- react/ContactsList/helpers.spec.js
+++ react/ContactsList/helpers.spec.js
@@ -1,4 +1,11 @@
-import { sortLastNameFirst, sortHeaders } from './helpers'
+import {
+  sortLastNameFirst,
+  sortHeaders,
+  makeGroupLabelsAndCounts
+} from './helpers'
+
+const t = x => x
+
 describe('Sort contacts', () => {
   describe('By Last Name', () => {
     it('should sort contact by last name', () => {
@@ -97,5 +104,47 @@ describe('sortHeaders', () => {
     const sortedHeaders = sortHeaders(contacts, t)
 
     expect(sortedHeaders).toEqual(['me', 'empty', 'B', 'F', 'H'])
+  })
+})
+
+describe('makeGroupLabelsAndCounts', () => {
+  it('should returns labels and counts', () => {
+    const contacts = [
+      { name: 'Alex', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'A' } },
+      { name: 'Alan', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'A' } },
+      { name: 'Cleo', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'C' } },
+      { name: 'Cloé', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'C' } },
+      { name: 'Clotilde', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'C' } },
+      { name: 'Constant', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'C' } },
+      {
+        name: 'Christophe',
+        indexes: { byFamilyNameGivenNameEmailCozyUrl: 'C' }
+      },
+      { name: 'Bernard', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'B' } },
+      { name: 'Baptiste', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'B' } },
+      { name: 'Xavier', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'X' } },
+      { name: 'Zorro', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'Z' } },
+      { name: '', indexes: { byFamilyNameGivenNameEmailCozyUrl: '' } },
+      { name: {}, indexes: { byFamilyNameGivenNameEmailCozyUrl: {} } },
+      { name: 'John', indexes: { byFamilyNameGivenNameEmailCozyUrl: null } },
+      {
+        name: 'Connor',
+        indexes: { byFamilyNameGivenNameEmailCozyUrl: undefined }
+      },
+      { name: 'Àlbert', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'À' } },
+      {
+        name: 'Alice',
+        me: true,
+        indexes: { byFamilyNameGivenNameEmailCozyUrl: 'Alice' }
+      },
+      { name: 'Èllen', indexes: { byFamilyNameGivenNameEmailCozyUrl: 'È' } }
+    ]
+
+    const res = makeGroupLabelsAndCounts(contacts, t)
+
+    expect(res).toStrictEqual({
+      groupLabels: ['A', 'C', 'B', 'X', 'Z', 'empty', 'me', 'E'],
+      groupCounts: [3, 5, 2, 1, 1, 4, 1, 1]
+    })
   })
 })
