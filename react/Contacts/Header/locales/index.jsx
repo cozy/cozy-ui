9a940cc0093ab225e923da5c509484f11f045340
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-08-20 16:55:07 +0200

    refactor: Remove useless code commented

--- react/Contacts/Header/locales/index.jsx
+++ react/Contacts/Header/locales/index.jsx
@@ -1,12 +1,7 @@
 import en from './en.json'
 import fr from './fr.json'
-// import { getI18n } from '../../../providers/I18n/helpers'
-// import withOnlyLocales from '../../../providers/I18n/withOnlyLocales'
 
 export const locales = {
   en,
   fr
 }
-
-// export const getOwnI18n = () => getI18n(undefined, lang => locales[lang])
-// export default withOnlyLocales(locales)
