68962be5846a00e6e827a39030866cd8d63ea1cf
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-10-16 12:10:12 +0200

    refactor(EditBadge): Use `AddPropsToChildren`

--- react/EditBadge/StatusWrapper.jsx
+++ react/EditBadge/StatusWrapper.jsx
@@ -2,42 +2,31 @@ import cx from 'classnames'
 import React from 'react'
 
 import Spinner from '../Spinner'
+import { AddPropsToChildren } from '../utils/react'
 
 const StatusWrapper = ({ src, status, setStatus, timestamp, children }) => {
   if (status === 'LOADING') {
     return (
       <>
-        {React.Children.map(children, child =>
-          React.isValidElement(child)
-            ? React.cloneElement(child, {
-                className: cx(child.props.className, 'u-o-50')
-              })
-            : null
-        )}
+        {AddPropsToChildren(children, childProps => ({
+          className: cx(childProps.className, 'u-o-50')
+        }))}
         <Spinner className="u-m-0" middle size="large" />
       </>
     )
   }
 
   if (status === 'PRESENT') {
-    return React.Children.map(children, child =>
-      React.isValidElement(child)
-        ? React.cloneElement(child, {
-            key: timestamp,
-            src,
-            onError: () => setStatus('ABSENT')
-          })
-        : null
-    )
+    return AddPropsToChildren(children, () => ({
+      key: timestamp,
+      src,
+      onError: () => setStatus('ABSENT')
+    }))
   }
 
-  return React.Children.map(children, child =>
-    React.isValidElement(child)
-      ? React.cloneElement(child, {
-          key: timestamp
-        })
-      : null
-  )
+  return AddPropsToChildren(children, () => ({
+    key: timestamp
+  }))
 }
 
 export default StatusWrapper
