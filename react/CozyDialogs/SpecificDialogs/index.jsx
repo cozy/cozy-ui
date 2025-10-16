72ee6a413efe141c8f530a3fb03019762e447ab5
Théo Poizat <hello@zatteo.com>
2025-10-16 11:28:00 +0200

    feat: Add ShortcutDialog
    A "dumb" modal that centralizes UI, translations and basic error management.
    
    It can take an io.cozy.files.shortcut object in prop to prefill fields.
    
    On save, it calls the callback with the values entered by the user.
    
    I decided to not build an io.cozy.files.shortcuts object for the output as there are multiple ways to create them. See documentation :
    - https://docs.cozy.io/en/cozy-doctypes/docs/io.cozy.files/#shortcuts
    - https://docs.cozy.io/en/cozy-stack/shortcuts/#shortcuts

--- react/CozyDialogs/SpecificDialogs/index.jsx
+++ react/CozyDialogs/SpecificDialogs/index.jsx
@@ -1,3 +1,4 @@
 export { default as AllowLocationDialog } from './AllowLocationDialog'
 export { default as InstallFlagshipAppDialog } from './InstallFlagshipAppDialog'
 export { default as AuthentificationDialog } from './AuthentificationDialog'
+export { default as ShortcutDialog } from './ShortcutDialog'
