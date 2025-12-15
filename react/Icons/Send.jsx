9dc2177fd350bc4e7f32e412f9faa7f790f496e5
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-12-15 14:50:58 +0100

    feat(Icon): Add Send

---
+++ react/Icons/Send.jsx
@@ -0,0 +1,12 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/send.svg` to regenerate;
+import React from 'react'
+
+function SvgSend(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path d="M15.594 1.252L9.75 14.927a.88.88 0 01-.66.53.821.821 0 01-.799-.253L.252 6.88a.821.821 0 01-.224-.807.88.88 0 01.553-.64L14.45.067c.374-.137.696-.067.965.212.27.279.329.603.178.972zM8.74 13.55L13.7 1.938 1.92 6.488l2.52 2.607 4.722-2.774-2.938 4.621 2.518 2.608z" />
+    </svg>
+  )
+}
+
+export default SvgSend
