5fcaf9fe256c462189adc65322aa19a9e9abbc08
doubleface <christophe@cozycloud.cc>
2026-01-14 10:48:36 +0100

    feat: Add antivirus icons

---
+++ react/Icons/ShieldInfected.jsx
@@ -0,0 +1,13 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/shield-infected.svg` to regenerate;
+import React from 'react'
+
+function SvgShieldInfected(props) {
+  return (
+    <svg viewBox="0 0 13 16" {...props}>
+      <path d="M6.333 0L0 2.375v4.821c0 3.998 2.7 7.727 6.333 8.637 3.634-.91 6.334-4.639 6.334-8.637V2.375L6.333 0zm4.75 7.196c0 3.167-2.018 6.096-4.75 6.99-2.73-.894-4.75-3.815-4.75-6.99v-3.72l4.75-1.782 4.75 1.781v3.721z" />
+      <path d="M7.125 9.5H5.542v1.583h1.583V9.5zm0-5.542H5.542v3.959h1.583V3.958z" />
+    </svg>
+  )
+}
+
+export default SvgShieldInfected
