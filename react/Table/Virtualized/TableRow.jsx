f972ce87243e1a873fba2403a9510eef7afbedac
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-02-04 11:01:31 +0100

    fix(Table): Remove implicit selection on click when `withCheckbox` false
    BREAKING CHANGE: You must now use `componentsProps.rowContent.onClick` prop to use selection on click

--- react/Table/Virtualized/TableRow.jsx
+++ react/Table/Virtualized/TableRow.jsx
@@ -7,20 +7,12 @@ const TableRow = ({ item, context, className, ...props }) => {
   const _item = item || context.data[props['data-item-index']]
   const isSelected = context.isSelectedItem(_item)
 
-  const handleClick = () => {
-    if (!context.withCheckbox && _item) {
-      const index = props['data-item-index']
-      context.onSelect?.(_item, null, index)
-    }
-  }
-
   return (
     <TableRowClassic
       {...props}
       className={cx(className, 'virtualized')}
       selected={isSelected}
       hover
-      onClick={!context.withCheckbox ? handleClick : props.onClick}
     />
   )
 }
