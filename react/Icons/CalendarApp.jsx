2ff858974cf6200cae97b2937f8ed95b1324fbab
Théo Poizat <hello@zatteo.com>
2026-02-18 11:49:05 +0100

    feat: Add CalendarApp and CalendarText icons
    CalendarApp has been named "CalendarApp" and not just "Calendar" because we already have a "Calendar" icon.
    
    CalendarText has not been added to Icons documentation like other text icons.

---
+++ react/Icons/CalendarApp.jsx
@@ -0,0 +1,67 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/illus/calendar-app.svg` to regenerate;
+import React from 'react'
+
+function SvgCalendarApp(props) {
+  return (
+    <svg viewBox="0 0 197 197" fill="none" {...props}>
+      <rect
+        width={197}
+        height={197}
+        rx={64.764}
+        fill="url(#calendar-app_svg__paint0_linear_60858_90)"
+      />
+      <g filter="url(#calendar-app_svg__filter0_d_60858_90)">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M66.58 38.931c-1.135-.607-2.478-.931-4.277-.931h-.57c-1.8 0-3.142.324-4.277.931a6.327 6.327 0 00-2.633 2.636c-.607 1.136-.93 2.481-.93 4.283l.005 2.637c-2.66.616-4.65 1.466-6.647 2.535-4.54 2.43-8.102 5.998-10.53 10.543C34.293 66.11 33 70.619 33 83.051v40.054c0 12.433 1.293 16.941 3.72 21.486 2.429 4.546 5.992 8.113 10.531 10.543 4.54 2.431 9.042 3.725 21.46 3.725h58.578c12.417 0 16.92-1.294 21.46-3.725 4.539-2.43 8.102-5.997 10.53-10.543 2.428-4.545 3.721-9.053 3.721-21.486V83.051c0-12.432-1.293-16.94-3.721-21.486-2.428-4.545-5.991-8.112-10.53-10.543-1.995-1.068-3.983-1.917-6.639-2.533l-.003-2.64c0-1.801-.323-3.146-.93-4.282a6.33 6.33 0 00-2.633-2.636c-1.135-.607-2.478-.931-4.277-.931h-.57c-1.799 0-3.142.324-4.277.931a6.33 6.33 0 00-2.633 2.636c-.607 1.136-.93 2.481-.93 4.283v1.447l-55.714-.01V45.85c0-1.802-.323-3.147-.93-4.283a6.327 6.327 0 00-2.633-2.636zM56.987 82.16h82.025c2.691 0 3.667.28 4.65.807a5.486 5.486 0 012.282 2.285c.526.985.806 1.961.806 4.655v41.968l-.063 1.165a10.692 10.692 0 01-10.639 9.527l-73.135-.161-1.476-.022c-3.66-.105-5.194-.591-6.738-1.42a9.707 9.707 0 01-4.026-4.041c-.93-1.741-1.423-3.467-1.423-8.227V89.907l.017-1.054c.068-1.919.338-2.757.79-3.601a5.483 5.483 0 012.28-2.285c.984-.526 1.96-.807 4.65-.807zm69.228 11.621h-5.359c-3.104 0-4.23.324-5.365.932a6.33 6.33 0 00-2.632 2.635c-.607 1.136-.93 2.264-.93 5.372v5.365c0 3.108.323 4.235.93 5.371a6.329 6.329 0 002.632 2.636c1.135.608 2.261.931 5.365.931h5.359c3.105 0 4.23-.323 5.365-.931a6.33 6.33 0 002.633-2.636c.606-1.136.93-2.263.93-5.371v-5.365c0-3.108-.324-4.236-.93-5.372a6.333 6.333 0 00-2.633-2.635c-1.135-.608-2.26-.932-5.365-.932z"
+          fill="#fff"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="calendar-app_svg__paint0_linear_60858_90"
+          x1={18.5}
+          y1={196}
+          x2={235}
+          y2={-27.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset={0.287} stopColor="#FD813C" />
+          <stop offset={1} stopColor="#FFF500" />
+        </linearGradient>
+        <filter
+          id="calendar-app_svg__filter0_d_60858_90"
+          x={23}
+          y={30}
+          width={150}
+          height={140.859}
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity={0} result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy={2} />
+          <feGaussianBlur stdDeviation={5} />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_60858_90"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_60858_90"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
+
+export default SvgCalendarApp
