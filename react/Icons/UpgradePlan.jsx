539978decc8cb2ba41554cc23bff215579ab32d1
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-03-02 14:50:14 +0100

    feat: Add upgrade-plan icon

---
+++ react/Icons/UpgradePlan.jsx
@@ -0,0 +1,12 @@
+// Automatically created, please run `scripts/generate-svgr-icon.sh assets/icons/ui/upgrade-plan.svg` to regenerate;
+import React from 'react'
+
+function SvgUpgradePlan(props) {
+  return (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path d="M6.328 8.907l1.767-1.341 1.76 1.34-.67-2.17 1.767-1.403H8.79l-.694-2.14-.693 2.14H5.238l1.76 1.402-.67 2.172zm7.862-2.812A6.094 6.094 0 008.096 0 6.094 6.094 0 002 6.095c0 1.547.58 2.949 1.524 4.023V16l4.571-1.524L12.667 16v-5.882a6.066 6.066 0 001.524-4.023zM8.096 1.524a4.575 4.575 0 014.572 4.571 4.575 4.575 0 01-4.572 4.572 4.575 4.575 0 01-4.571-4.572 4.575 4.575 0 014.571-4.571zm0 11.428l-3.047.777v-2.361a6.08 6.08 0 003.047.822 6.08 6.08 0 003.048-.822v2.361l-3.048-.777z" />
+    </svg>
+  )
+}
+
+export default SvgUpgradePlan
