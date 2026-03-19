e91bd27e20f3390f3bb2f23b78fe0827796c24c5
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-03-19 11:21:54 +0100

    feat: Remove CircularChart component
    BREAKING CHANGE: CircularChart has been removed with no replacement

--- docs/styleguide.config.js
+++ docs/styleguide.config.js
@@ -22,7 +22,6 @@ module.exports = {
         '../react/Buttons',
         '../react/Checkbox',
         '../react/Chips',
-        '../react/CircularChart',
         '../react/Counter',
         '../react/Dialog',
         '../react/Divider',
