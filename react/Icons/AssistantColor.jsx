40f678101ab844905bf740f9e93f688bfdcad874
lethemanh <lethemanh@lethemanhs-MacBook-Pro.local>
2026-03-20 12:58:46 +0700

    fix: Adjust assistant color icon :bug:

--- react/Icons/AssistantColor.jsx
+++ react/Icons/AssistantColor.jsx
@@ -3,37 +3,30 @@ import React from 'react'
 
 function SvgAssistantColor(props) {
   return (
-    <svg viewBox="0 0 16 16" fill="none" {...props}>
-      <g clipPath="url(#assistant-color_svg__clip0_19251_33606)">
+    <svg viewBox="0 0 16 16" fill="transparent" color="transparent" {...props}>
+      <g clipPath="url(#assistant-color_svg__clip0_25355_124591)">
         <path
           d="M6.626 10.332a1.333 1.333 0 00-.958-.958L1.578 8.32a.333.333 0 010-.642l4.09-1.055a1.333 1.333 0 00.958-.957l1.055-4.09a.333.333 0 01.642 0l1.054 4.09a1.333 1.333 0 00.958.958l4.09 1.054a.333.333 0 010 .642l-4.09 1.054a1.333 1.333 0 00-.958.958l-1.055 4.09a.333.333 0 01-.642 0l-1.054-4.09z"
-          stroke="#868687"
-          strokeWidth={1.333}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M6.626 10.332a1.333 1.333 0 00-.958-.958L1.578 8.32a.333.333 0 010-.642l4.09-1.055a1.333 1.333 0 00.958-.957l1.055-4.09a.333.333 0 01.642 0l1.054 4.09a1.333 1.333 0 00.958.958l4.09 1.054a.333.333 0 010 .642l-4.09 1.054a1.333 1.333 0 00-.958.958l-1.055 4.09a.333.333 0 01-.642 0l-1.054-4.09z"
-          stroke="url(#assistant-color_svg__paint0_linear_19251_33606)"
+          stroke="url(#assistant-color_svg__paint0_linear_25355_124591)"
           strokeOpacity={0.9}
           strokeWidth={1.333}
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
-          d="M13.335 1.334c.368 0 .667.3.667.667v.666h.668a.667.667 0 010 1.334h-.668v.667a.667.667 0 01-1.334 0v-.667h-.665a.667.667 0 010-1.334h.665v-.666c0-.368.299-.667.667-.667z"
-          fill="url(#assistant-color_svg__paint1_linear_19251_33606)"
+          d="M13.335 1.336c.368 0 .667.299.667.667v.665h.668a.667.667 0 010 1.334h-.668v.668a.667.667 0 01-1.334 0v-.668h-.665a.667.667 0 010-1.334h.665v-.665c0-.368.299-.667.667-.667z"
+          fill="url(#assistant-color_svg__paint1_linear_25355_124591)"
           fillOpacity={0.9}
         />
         <path
-          d="M2.667 10.667c.368 0 .665.298.666.665a.667.667 0 01.001 1.333v.002a.667.667 0 01-1.334 0v-.001h-.001a.667.667 0 010-1.334H2a.667.667 0 01.667-.665z"
-          fill="url(#assistant-color_svg__paint2_linear_19251_33606)"
+          d="M2.667 10.668c.367 0 .664.297.666.664a.667.667 0 01.001 1.333v.003a.667.667 0 01-1.334 0v-.002h-.001a.667.667 0 010-1.334H2a.667.667 0 01.667-.664z"
+          fill="url(#assistant-color_svg__paint2_linear_25355_124591)"
           fillOpacity={0.9}
         />
       </g>
       <defs>
         <linearGradient
-          id="assistant-color_svg__paint0_linear_19251_33606"
+          id="assistant-color_svg__paint0_linear_25355_124591"
           x1={1.336}
           y1={7.999}
           x2={19.625}
@@ -45,11 +38,11 @@ function SvgAssistantColor(props) {
           <stop offset={1} stopColor="#FF0" />
         </linearGradient>
         <linearGradient
-          id="assistant-color_svg__paint1_linear_19251_33606"
+          id="assistant-color_svg__paint1_linear_25355_124591"
           x1={11.336}
-          y1={3.334}
+          y1={3.336}
           x2={16.822}
-          y2={3.334}
+          y2={3.336}
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#00B7FF" />
@@ -57,18 +50,18 @@ function SvgAssistantColor(props) {
           <stop offset={1} stopColor="#FF0" />
         </linearGradient>
         <linearGradient
-          id="assistant-color_svg__paint2_linear_19251_33606"
+          id="assistant-color_svg__paint2_linear_25355_124591"
           x1={1.332}
-          y1={12.001}
+          y1={12.002}
           x2={4.99}
-          y2={12.001}
+          y2={12.002}
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#00B7FF" />
           <stop offset={0.567} stopColor="#E06DD1" />
           <stop offset={1} stopColor="#FF0" />
         </linearGradient>
-        <clipPath id="assistant-color_svg__clip0_19251_33606">
+        <clipPath id="assistant-color_svg__clip0_25355_124591">
           <path fill="#fff" d="M0 0h16v16H0z" />
         </clipPath>
       </defs>
