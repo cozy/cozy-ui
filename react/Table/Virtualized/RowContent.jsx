2f9761c2e966d8b1e79ea78a4acee2118be7b49b
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-05-28 15:41:21 +0200

    feat(Table): Remove checkboxes
    BREAKING CHANGE: Props `onSelect`, `onSelectAll` and `withCheckbox` have been removed from Virtualized Tables (`import Table from 'cozy-ui/transpiled/react/Table/Virtualized'`. You must put the `onSelectAll` in something else outside of the Table, and `onSelect` func in `componentsProps.rowContent.onClick` prop.

--- react/Table/Virtualized/RowContent.jsx
+++ react/Table/Virtualized/RowContent.jsx
@@ -1,43 +1,17 @@
-import cx from 'classnames'
 import React from 'react'
 
 import Cell from './Cell'
-import Checkbox from '../../Checkbox'
-import TableCell from '../../TableCell'
 
 const RowContent = ({
-  index,
   row,
   columns,
-  context,
   children,
-  disableCheckbox,
-  onSelectClick,
   onLongPress,
   onDoubleClick,
   onClick
 }) => {
-  const selectedCount = context.selectedItems.length
-
   return (
     <>
-      {context.withCheckbox && (
-        <TableCell align="center" padding="checkbox">
-          <Checkbox
-            className={cx('virtualizedCheckbox', {
-              visible: selectedCount > 0,
-              checked: context.isSelectedItem(row)
-            })}
-            disabled={disableCheckbox?.(row)}
-            checked={context.isSelectedItem(row)}
-            inputProps={{
-              'aria-labelledby': `enhanced-table-checkbox-${index}`
-            }}
-            size="small"
-            onClick={event => onSelectClick(row, event, index)}
-          />
-        </TableCell>
-      )}
       {columns.map(column => (
         <Cell
           key={column.id}
