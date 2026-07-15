45aa727383e14164a07016ab6276a30feb62e59c
Théo Poizat <hello@zatteo.com>
2026-07-15 16:26:00 +0200

    feat: Reexport ImageList, ImageListItem and ImageListItemBar

---
+++ react/ImageList/index.js
@@ -0,0 +1,3 @@
+import MuiImageList from '@material-ui/core/ImageList'
+
+export default MuiImageList
---
+++ react/ImageListItem/index.js
@@ -0,0 +1,3 @@
+import MuiImageListItem from '@material-ui/core/ImageListItem'
+
+export default MuiImageListItem
