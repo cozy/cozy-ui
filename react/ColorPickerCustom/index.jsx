99cf242d8ea5c55b5450cc10398f79597356bd30
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-07-16 16:12:23 +0200

    feat(ColorPickerCustom): Change API and behavior
    Should be a BC but not used anywhere so can skip this one

--- react/ColorPickerCustom/index.jsx
+++ react/ColorPickerCustom/index.jsx
@@ -1,25 +1,65 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { forwardRef, useState } from 'react'
 import { HexColorPicker, HexColorInput } from 'react-colorful'
+import { useI18n, useExtendI18n } from 'twake-i18n'
 
+import { locales } from './locales'
 import styles from './styles.styl'
+import Button from '../../react/Buttons'
 import TextField from '../../react/TextField'
 import Typography from '../../react/Typography'
 
-const ColorPickerCustom = ({ color, setColor, className }) => {
+// Adapts HexColorInput (color/onChange(string)) to MUI's inputComponent contract (value/onChange(event)).
+// inputRef is stripped because HexColorInput doesn't handle it and would forward it to the DOM.
+const ColorInput = forwardRef(
+  // eslint-disable-next-line no-unused-vars
+  ({ value, onChange, inputRef, ...props }, ref) => (
+    <HexColorInput
+      {...props}
+      ref={ref}
+      color={value}
+      onChange={color => onChange?.({ target: { value: color } })}
+    />
+  )
+)
+
+ColorInput.displayName = 'ColorInput'
+
+const ColorPickerCustom = ({ className, color, onCancel, onSave }) => {
+  useExtendI18n(locales)
+  const [newColor, setNewColor] = useState(color)
+
+  const { t } = useI18n()
+
   return (
     <div className={cx('u-flex u-flex-column', styles.root, className)}>
-      <HexColorPicker color={color} onChange={setColor} />
+      <HexColorPicker
+        color={newColor}
+        onChange={c => setNewColor(c.toUpperCase())}
+      />
       <div className="u-flex u-flex-row u-flex-items-center u-mt-1">
         <Typography variant="h6" className="u-mr-half">
           Hex
         </Typography>
         <TextField
-          value={color}
-          onChange={e => setColor(e.target.value)}
-          inputComponent={HexColorInput}
           className={styles.input}
+          InputProps={{ inputComponent: ColorInput }}
+          inputProps={{ prefixed: true }}
+          value={newColor}
+          onChange={e => setNewColor(e.target.value.toUpperCase())}
+        />
+      </div>
+      <div className="u-mt-1 u-ta-right">
+        <Button
+          variant="text"
+          label={t('ColorPickerCustom.cancel')}
+          onClick={() => onCancel?.(color)}
+        />
+        <Button
+          variant="ghost"
+          label={t('ColorPickerCustom.submit')}
+          onClick={() => onSave?.(newColor)}
         />
       </div>
     </div>
@@ -27,9 +67,10 @@ const ColorPickerCustom = ({ color, setColor, className }) => {
 }
 
 ColorPickerCustom.propTypes = {
+  className: PropTypes.string,
   color: PropTypes.string,
-  setColor: PropTypes.func,
-  className: PropTypes.string
+  onSave: PropTypes.func,
+  onCancel: PropTypes.func
 }
 
 export default ColorPickerCustom
---
+++ react/ColorPickerCustom/locales/index.js
@@ -0,0 +1,11 @@
+import en from './en.json'
+import fr from './fr.json'
+import ru from './ru.json'
+import vi from './vi.json'
+
+export const locales = {
+  en,
+  fr,
+  ru,
+  vi
+}
