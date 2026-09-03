c9f3fc1e53748b08712a83e1c971eb29d056742d
Théo Poizat <hello@zatteo.com>
2026-09-03 14:17:47 +0200

    feat: Update react-markdown

--- react/Markdown/index.jsx
+++ react/Markdown/index.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 import Link from '../Link'
 import Typography from '../Typography'
 
+const heading =
+  level =>
+  ({ children }) =>
+    (
+      <Typography variant={`h${level}`} className="u-mb-1">
+        {children}
+      </Typography>
+    )
+
+const components = {
+  a: ({ children, href }) => (
+    <Link href={href} rel="noreferrer" target="_blank">
+      {children}
+    </Link>
+  ),
+  p: ({ children }) => (
+    <Typography variant="body1" className="u-mb-1">
+      {children}
+    </Typography>
+  ),
+  ...Object.fromEntries(
+    [1, 2, 3, 4, 5, 6].map(level => [`h${level}`, heading(level)])
+  )
+}
+
 const Markdown = ({ content }) => {
   return (
-    <ReactMarkdown
-      source={content}
-      renderers={{
-        link: ({ children, href }) => (
-          <Link href={href} rel="noreferrer" target="_blank">
-            {children}
-          </Link>
-        ),
-        heading: ({ children, level }) => (
-          <Typography variant={`h${level}`} className="u-mb-1">
-            {children}
-          </Typography>
-        ),
-        paragraph: ({ children }) => (
-          <Typography variant="body1" className="u-mb-1">
-            {children}
-          </Typography>
-        )
-      }}
-    />
+    <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
+      {content}
+    </ReactMarkdown>
   )
 }
 
